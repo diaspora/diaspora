d3313c836dc7f2f620c0b207df5ae97f087c56de
B Mathis <brandon@imathis.com>
2009-11-21 15:41:38 -0600

    updated layout, page styles, fixed config paths for assets

--- config.rb
+++ config.rb
@@ -1,11 +1,10 @@
 # Require any additional compass plugins here.
-#require 'compass-colors'
+require 'compass-colors'
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "site/stylesheets"
-sass_dir = "/stylesheets"
-images_dir = "images"
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+sass_dir = "stylesheets"
+images_dir = "source/images"
+images_dir = "source/images/"
+http_images_path = "/images"
