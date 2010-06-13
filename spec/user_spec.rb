583067b05c6ebe6aca1429492b6cda37c37a353e
danielvincent <danielgrippi@gmail.com>
2010-06-12 18:52:36 -0700

    cleaned up user specs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,24 +1,13 @@
 require 'spec_helper'
 
-describe 'a user should be able to log into his seed' do
+describe User do
   before do
     User.delete_all
   end
 
-  it 'should should have a name and password' do
+  it 'should should have a valid name and password' do
+    User.create
     User.count.should == 0
-    billy = User.new
-    User.count.should == 0
-    billy.save
-    User.count.should == 0
-    
-    
   end
-
-  it 'should be able to log into a page with a password' do
-    billy = User.create(:password => "foobar")
-    billy.password.should == "foobar"
-  end
-
   
 end
