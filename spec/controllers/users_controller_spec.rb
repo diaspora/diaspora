865d7d18cbf69f81372648c6ed8d7114de09f26a
maxwell <maxwell@joindiaspora.com>
2010-06-13 20:00:31 -0700

    user login now drops to a dashboard page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
  
 describe UsersController do
   render_views
+  
   #fixtures here?
   it 'should, after logging in redirect to the dashboard page' do
-    pending
+    puts "where is rafi"
   end
 end
