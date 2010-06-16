b8e2fd018f687f8437fb867858283f308b665b55
maxwell <maxwell@joindiaspora.com>
2010-06-15 21:10:59 -0700

    fixed dashboard test

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
  
 describe DashboardController do
-  fixtures :all
-  integrate_views
+ render_views
   
   it "index action should render index template" do
+    request.env['warden'] = mock_model(Warden, :authenticate? => @user, :authenticate! => @user)
     get :index
     response.should render_template(:index)
   end
