7bc165d7a0e9d8503ec3113e489c794d618c34e1
maxwell <maxwell@joindiaspora.com>
2010-06-20 22:11:37 -0700

    added js file

---
+++ public/javascripts/socket.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function debug(str){ $("#debug").append("<p>" +  str); };
+
+    ws = new WebSocket("ws://localhost:8080/");
+    ws.onmessage = function(evt) { $(".msg").prepend("<p>"+evt.data+"</p>"); };
+    ws.onclose = function() { debug("socket closed"); };
+    ws.onopen = function() {
+      debug("connected...");
+      ws.send("hello server");
+      ws.send("hello again");
+    };
+});
