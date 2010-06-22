cc89cd5cda8781bf6be01fb71bb110e3057b525a
maxwell <maxwell@joindiaspora.com>
2010-06-22 16:12:15 -0700

    reload object before send to websocket

--- config/initializers/socket.rb
+++ config/initializers/socket.rb
@@ -8,14 +8,19 @@ module WebSocket
   EM.next_tick {
     EM.add_timer(0.1) do
       @channel = EM::Channel.new
+      @view = ActionView::Base.new(ActionController::Base.view_paths, {})  
+      class << @view  
+        include ApplicationHelper 
+        include Rails.application.routes.url_helpers
+      end
     end
     
-    EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
+    EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
       ws.onopen {
         sid = @channel.subscribe { |msg| ws.send msg }
         
         ws.onmessage { |msg|
-          @channel.push "#{msg}"
+          @channel.push msg
         }
 
         ws.onclose {
@@ -27,12 +32,8 @@ module WebSocket
 }
   #this should get folded into message queue i think?
   def self.update_clients(object)
-     view = ActionView::Base.new(ActionController::Base.view_paths, {})  
-     class << view  
-       include ApplicationHelper 
-       include Rails.application.routes.url_helpers
-     end  
-      n = view.render(:partial =>view.type_partial(object), :locals => {:post  => object})  
+
+      n = @view.render(:partial => @view.type_partial(object), :locals => {:post  => object})  
       @channel.push(n) if @channel
   end
 
