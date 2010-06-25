db8970eb48a65b906b933042713254963652c450
ilya <ilya@laptop.(none)>
2010-06-24 20:44:32 -0400

    RS, IZ, reversing Rafi's stupidity

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
+require File.dirname(__FILE__) + "/../config/environment" #unless defined?(Rails)
 require 'rspec/rails'
 require 'database_cleaner'
 #require File.dirname(__FILE__) + "/factories"
@@ -21,7 +21,6 @@ Rspec.configure do |config|
   DatabaseCleaner.orm = "mongoid"
 
   config.before(:suite) do
-    #DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
@@ -39,5 +38,5 @@ Rspec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+#  config.use_transactional_fixtures = true
 end
