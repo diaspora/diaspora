08d6a46bd85f83b7770d3d4c5fa2cf2963c0ba80
danielvincent <danielgrippi@gmail.com>
2010-06-28 22:18:05 -0700

    login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery :except => :receive
   layout 'application'
   before_filter :set_friends
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "session_wall"
+    else
+      "application"
+    end
+  end
   
   def set_friends
     @friends = Friend.all
