fbd1195113d1012f295692dc54cddfd9900d9aae
danielvincent <danielgrippi@gmail.com>
2010-06-30 16:00:29 -0700

    DG IZ; rake task for backer, can now pass in arguments [backer_num, password]

---
+++ db/seeds/backer.rb
@@ -0,0 +1,38 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => citie
+
+require 'config/environment'
+
+
+
+def create(backer_number, password)
+  names = [ ["George", "Washington"],
+            ["John", "Adams"],
+            ["Thomas", "Jefferson"],
+            ["James", "Madison"],
+            ["James", "Monroe"],
+            ["John Quincy", "Adams"],
+            ["Andrew", "Jackson"],
+            ["Martin Van", "Buren"],
+            ["William Henry","Harrison"],
+            ["John", "Tyler"],
+            ["James K." , "Polk"],
+            ["Zachary", "Taylor"],
+            ["Millard", "Fillmore"],
+            ["Franklin", "Pierce"],
+            ["James", "Buchanan"],
+            ["Abraham", "Lincoln"]
+          ]
+
+  # Create seed user
+  user = User.create( :email => "b#{backer_number}@joindiaspora.com", :password => "#{password}", :profile => Profile.create( :first_name => names[backer_number][0], :last_name => names[backer_number][1] ))
+
+  # Make friends with Diaspora Tom
+  Friend.create( :email => "tom@joindiaspora.com", :url => "http://tom.joindiaspora.com/", :profile => Profile.create(:first_name => "Diaspora", :last_name => "Tom"))
+end
+
--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -1,17 +1,22 @@
 namespace :db do
   desc 'Seed the current RAILS_ENV database from db/seeds.rb'
-  task :seed do
-
-    puts "Seeding the database for #{RAILS_ENV}..."
-
-    require 'db/seeds'
+  namespace :seed do
+    task :tom do
+      puts "Seeding the database for #{Rails.env}..."
+      require 'db/seeds/tom'
+    end
+    task :backer, :num, :password do |t, args|
+      puts "Seeding the database for #{Rails.env}..."
+      require 'db/seeds/backer'
+      create( Integer(args.num), args.password )
+    end
   end
 
   desc 'Delete the collections in the current RAILS_ENV database'
   task :purge do
     require 'config/environment'
 
-    puts "Purging the database for #{RAILS_ENV}..."
+    puts "Purging the database for #{Rails.env}..."
 
     # Specifiy what models to remove
     Post.delete_all
@@ -22,9 +27,9 @@ namespace :db do
   desc 'Purge and seed the current RAILS_ENV database using information from db/seeds.rb'
   task :reset do
     
-    puts "Resetting the database for #{RAILS_ENV}".upcase
+    puts "Resetting the database for #{Rails.env}".upcase
     Rake::Task['db:purge'].invoke
-    Rake::Task['db:seed'].invoke
+    Rake::Task['db:seed:tom'].invoke
     puts "Success!"
   end
 end
