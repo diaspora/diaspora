ba984009f803f51c6d1e966e10b429b904b171dc
maxwell <maxwell@joindiaspora.com>
2010-07-01 16:38:32 -0700

    added chrome frame

--- public/javascripts/google.js
+++ public/javascripts/google.js
@@ -8,8 +8,3 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
-CFInstall.check({
-  mode: "overlay",
-  destination: "http://www.waikiki.com"
-});
