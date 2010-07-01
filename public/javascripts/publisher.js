03e7071aae0b68fb9053501c379766697fa3ba22
danielvincent <danielgrippi@gmail.com>
2010-07-01 16:24:51 -0700

    fixed nav list/link deal for firefox, html compliance

--- public/javascripts/publisher.js
+++ public/javascripts/publisher.js
@@ -2,30 +2,30 @@
 
     $("#publisher_content_pickers .status_message").click(function(){
       if( $("#new_status_message").css("display") == "none" ) {
-        $("#publisher_content_pickers").children("a").children("li").removeClass("selected");
+        $("#publisher_content_pickers").children("li").removeClass("selected");
         $("#publisher_form form").fadeOut(50);
 
-        $(this).children("a > li").toggleClass("selected");
+        $(this).toggleClass("selected");
         $("#new_status_message").delay(50).fadeIn(200);
       }
     });
 
-    $("#publisher_content_pickers  .bookmark").click(function(){
+    $("#publisher_content_pickers .bookmark").click(function(){
       if( $("#new_bookmark").css("display") == "none" ) {
-        $("#publisher_content_pickers").children("a").children("li").removeClass("selected");
+        $("#publisher_content_pickers").children("li").removeClass("selected");
         $("#publisher_form form").fadeOut(50);
 
-        $(this).children("li").toggleClass("selected");
+        $(this).toggleClass("selected");
         $("#new_bookmark").delay(50).fadeIn(200);
       }
     });
 
     $("#publisher_content_pickers  .blog").click(function(){
       if( $("#new_blog").css("display") == "none" ) {
-        $("#publisher_content_pickers").children("a").children("li").removeClass("selected");
+        $("#publisher_content_pickers").children("li").removeClass("selected");
         $("#publisher_form form").fadeOut(50);
 
-        $(this).children("li").toggleClass("selected");
+        $(this).toggleClass("selected");
         $("#new_blog").delay(50).fadeIn(200);
       }
     });
