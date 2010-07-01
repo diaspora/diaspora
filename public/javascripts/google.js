6f17e5f28ffa7db595315082d156fb1eda8bbea3
maxwell <maxwell@joindiaspora.com>
2010-07-01 16:17:32 -0700

    added chrome frame for ie lameos

---
+++ public/javascripts/google.js
@@ -0,0 +1,15 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17207587-1']);
+_gaq.push(['_setDomainName', '.joindiaspora.com']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+CFInstall.check({
+  mode: "overlay",
+  destination: "http://www.waikiki.com"
+});
