6ce0f4464f206331ae4cf7b1936d969f6c87de6a
danielvincent <danielgrippi@gmail.com>
2010-07-01 20:04:31 -0700

    reverted publisher javascript refactor.  working again in chrome.

--- public/javascripts/publisher.js
+++ public/javascripts/publisher.js
@@ -1,20 +1,33 @@
   $(document).ready( function() {
 
-    $("#publisher_content_pickers .status_message").click( selectPicker);
-
-    $("#publisher_content_pickers  .bookmark").click(selectPicker);
-
-    $("#publisher_content_pickers  .blog").click(selectPicker);
-    
-    function selectPicker(event){
-      event.preventDefault();
-      if( $("#new_" + $(this)[0].classList[0]).css("display") == "none" ) {
-        $("#publisher_content_pickers .selected").removeClass("selected");
+    $("#publisher_content_pickers .status_message").click(function(){
+      if( $("#new_status_message").css("display") == "none" ) {
+        $("#publisher_content_pickers").children("li").removeClass("selected");
         $("#publisher_form form").fadeOut(50);
 
         $(this).toggleClass("selected");
-        $("#new_" + $(this)[0].classList[0]).delay(50).fadeIn(200);
+        $("#new_status_message").delay(50).fadeIn(200);
       }
+    });
 
-    }
+    $("#publisher_content_pickers .bookmark").click(function(){
+      if( $("#new_bookmark").css("display") == "none" ) {
+        $("#publisher_content_pickers").children("li").removeClass("selected");
+        $("#publisher_form form").fadeOut(50);
+
+        $(this).toggleClass("selected");
+        $("#new_bookmark").delay(50).fadeIn(200);
+      }
+    });
+
+    $("#publisher_content_pickers  .blog").click(function(){
+      if( $("#new_blog").css("display") == "none" ) {
+        $("#publisher_content_pickers").children("li").removeClass("selected");
+        $("#publisher_form form").fadeOut(50);
+
+        $(this).toggleClass("selected");
+        $("#new_blog").delay(50).fadeIn(200);
+      }
+    });
+
   });
