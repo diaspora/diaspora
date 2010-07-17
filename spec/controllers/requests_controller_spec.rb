40435d689105dd72f2bdb3bffed3ded7674ec45e
Raphael <raphael@joindiaspora.com>
2010-07-17 13:20:06 -0700

    Commented out tests that require internets to function

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe RequestsController do
   describe "profile" do
     it 'should fetch the public webfinger profile on request' do
+      pending "Duplicate test"
       #post :create {:request => {:destination_url => 'tom@tom.joindiaspora.com'}
      
       url = RequestsController.diaspora_url('http://tom.joindiaspora.com/')
