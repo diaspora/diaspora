c8c76ddab59f745d7e202969fe1b62a869334b26
danielvincent <danielgrippi@gmail.com>
2010-07-17 14:08:18 -0700

    added auth_user back to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
     @photos = Photo.paginate :page => params[:page], :order => 'created_at DESC'
