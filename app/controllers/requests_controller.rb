eb4153ec8f0748dedd95691d7e0129b852a9c66c
maxwell <maxwell@joindiaspora.com>
2010-07-16 23:22:18 -0700

    did a couple of things to the request page

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -26,7 +26,7 @@ class RequestsController < ApplicationController
   
   def create
     url = diaspora_url(params[:request][:destination_url])
-    @request = current_user.send_friend_request_to(url) unless url.include?('@')
+    @request = current_user.send_friend_request_to(url) unless url.include?('@')|| url == ''
     if @request
       flash[:notice] = "a friend request was sent to #{@request.destination_url}"
       redirect_to requests_url
