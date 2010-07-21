9b4d92e1fa97aa08c874fc72b2d66542f793069a
Raphael <raphael@joindiaspora.com>
2010-07-21 15:36:14 -0700

    trying to patch relationship controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -25,7 +25,7 @@ class RequestsController < ApplicationController
   end
   
   def create
-    url = diaspora_url(params[:request][:destination_url])
+    url = relationship_flow(params[:request][:destination_url]).values.first
     @request = current_user.send_friend_request_to(url) unless url.include?('@')|| url == ''
     if @request
       flash[:notice] = "a friend request was sent to #{@request.destination_url}"
