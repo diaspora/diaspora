e754e97c7ecc7621ebd54455e515de0509f92325
ilya <ilya@laptop.(none)>
2010-07-23 15:32:27 -0700

    Logging for the differing id problem in remote photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,14 +21,18 @@ class Photo < Post
     photo.image.store! image_file
     photo
   end
+  
+  after_save {Rails.logger.info("After saving now the id is #{id}"
 
   def remote_photo
     @remote_photo ||= User.owner.url.chop + image.url
   end
 
   def remote_photo= remote_path
+    Rails.logger.info("Setting remote photo with id #{id}")
     @remote_photo = remote_path
     image.download! remote_path
     image.store!
+    Rails.logger.info("Setting remote photo with id #{id}")
   end
 end
