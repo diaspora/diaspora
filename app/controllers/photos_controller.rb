1b32afc78f625bd677a4fceb53ab852ad857951a
ilya <ilya@laptop.(none)>
2010-07-23 14:52:59 -0700

    Switching to Grid_FS, correctly

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
   before_filter :authenticate_user!
   
   def create
-    begin
+    #begin
       @photo = Photo.instantiate(params[:photo])
       @photo.person = current_user
 
@@ -16,10 +16,10 @@ class PhotosController < ApplicationController
         render :action => 'album#new'
       end
 
-    rescue
-      flash[:error] = "Photo upload failed.  Are you sure that was an image?"
-      redirect_to Album.first(:id => params[:photo][:album_id])
-    end
+    #rescue
+    #  flash[:error] = "Photo upload failed.  Are you sure that was an image?"
+    #  redirect_to Album.first(:id => params[:photo][:album_id])
+    #end
   end
   
   def new
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  #config.grid_fs_database = "#diaspora-#{Rails.env}"
-  #config.grid_fs_host = 'localhost'
-  #config.grid_fs_access_url = "/images"
+  config.grid_fs_database = "#diaspora-#{Rails.env}"
+  config.grid_fs_host = 'localhost'
+  config.grid_fs_access_url = "/images"
   config.storage = :grid_fs
   #config.storage = :file
 end
