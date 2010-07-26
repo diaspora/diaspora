88c83bb0283b54fabc7f025db28e67ba5a15a7c9
maxwell <maxwell@joindiaspora.com>
2010-07-26 15:15:42 -0700

    MS added pagination to the author show page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,7 @@ class AuthorsController < ApplicationController
   
   def show
     @author= Author.where(:id => params[:id]).first
-    @author_ostatus_posts = @author.ostatus_posts.sort(:created_at.desc)
+    @author_ostatus_posts = @author.ostatus_posts.paginate :page => params[:page], :order => 'created_at DESC'
   end
 
 
