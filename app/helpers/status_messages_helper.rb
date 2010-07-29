03cf61bd7c51cae34cdad6f7c88b294fe16186db
ilya <ilya@laptop.(none)>
2010-07-28 17:04:56 -0700

    new user top bar

--- app/helpers/status_messages_helper.rb
+++ app/helpers/status_messages_helper.rb
@@ -3,7 +3,7 @@ module StatusMessagesHelper
   def my_latest_message
     message = @latest_status_message
     unless message.nil?
-      return message.message + "   -   " + how_long_ago(message)
+      return message.message#+ "   -   " + how_long_ago(message)
     else
       return "No message to display."
     end
