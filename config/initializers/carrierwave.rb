e3c28c4fee0aed444cd095f4367f9c872ba8fafb
Raphael <raphael@joindiaspora.com>
2010-08-02 21:31:14 -0700

    Fixed Photos, and Carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
-  config.grid_fs_database = "#diaspora-#{Rails.env}"
-  config.grid_fs_host = 'localhost'
+  config.grid_fs_database = MongoMapper::database.name
+  config.grid_fs_host = MongoMapper::connection.host
   config.grid_fs_access_url = "/images"
   config.storage = :grid_fs
   #config.storage = :file
