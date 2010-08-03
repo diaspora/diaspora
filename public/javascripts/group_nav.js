3d5f27fe5a49dad74f18bbab576bfe5ad4977a80
danielvincent <danielgrippi@gmail.com>
2010-08-02 17:27:45 -0700

    javascript gets group param for temp testing... friend pictures on group nav displays all people and authors, also for testing

---
+++ public/javascripts/group_nav.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++)
+  {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+  }
+
+  $("."+vars['g']).addClass('selected');
+});
