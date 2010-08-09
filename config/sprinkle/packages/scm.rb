27b4c1933749d06a4f96d8fc216c68ead6531afd
ilya <ilya@laptop.(none)>
2010-08-08 21:24:34 -0700

    Getting the deployment changes from master

--- config/sprinkle/packages/scm.rb
+++ config/sprinkle/packages/scm.rb
@@ -1,20 +1,22 @@
 package :git, :provides => :scm do
   description 'Git Distributed Version Control'
-	apt %w( git-core ) do 
-    pre :install, "mkdir -p /root/.ssh/"
-  end
+	apt %w( git-core )
   requires :pubkey
-  requires :privkey
-  requires :known_hosts
+
 end
 
 package :privkey do
   description 'checkout from github with it'
-  transfer "#{File.dirname(__FILE__)}/../deploy_key/id_rsa", '/root/.ssh/id_rsa', :render => false
+  transfer "#{File.dirname(__FILE__)}/../deploy_key/id_rsa", '/root/.ssh/id_rsa', :render => false do 
+    pre :install, "rm -rf /root/.ssh/ && mkdir -p /root/.ssh/"
+    post :install, "chmod go-rwx /root/.ssh/id_rsa"
+  end
 end
 
 package :pubkey do
-  transfer "#{File.dirname(__FILE__)}/../deploy_key/id_rsa.pub", '/root/.ssh/id_rsa.pub', :render => false 
+  transfer "#{File.dirname(__FILE__)}/../deploy_key/id_rsa.pub", '/root/.ssh/id_rsa.pub', :render => false  
+  requires :privkey
+  requires :known_hosts
 end
 
 package :known_hosts do
