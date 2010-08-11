144e9ed439653eaf58365c291f6a28be96b49794
Raphael <raphael@joindiaspora.com>
2010-08-11 11:18:40 -0700

    Bookmarks now clean link in instantiate, posting them should work right again

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -12,8 +12,6 @@ class Bookmark < Post
   validates_format_of :link, :with =>
     /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
 
-  before_validation :clean_link
-
   def to_activity
         <<-XML
   <entry>
@@ -27,12 +25,18 @@ class Bookmark < Post
   </entry>
         XML
   end
+  
+  def self.instantiate params
+    params[:link] = clean_link(params[:link])
+    create params
+  end
 
   protected
-  def clean_link
-    if self.link
-      self.link = 'http://' + self.link unless self.link.match('https?://')
-      self.link = self.link + '/' if self.link[-1,1] != '/'
+  def self.clean_link link
+    if link
+      link = 'http://' + link unless link.match('https?://')
+      link = link + '/' if link[-1,1] != '/'
+      link
     end
   end
 end
