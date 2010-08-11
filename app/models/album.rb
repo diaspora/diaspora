3d46a49b970ad9e7d65b3afb08a9c676e1399270
Raphael <raphael@joindiaspora.com>
2010-08-11 16:50:22 -0700

    RS, DG; fixing Album.mine_or_friends

--- app/models/album.rb
+++ app/models/album.rb
@@ -26,9 +26,9 @@ class Album
 
   def self.mine_or_friends(friend_param, current_user)
     if friend_param
-      Album.where(:person_id.ne => current_user.person.id)
+      Album.where(:person_id.ne => current_user.person.id.to_s)
     else
-      Album.where(:person_id => current_user.person.id)
+      Album.where(:person_id => current_user.person.id.to_s)
     end
   end
   
