b3d8f59c7712a72f3f717c3bd09770650269b9df
ilya <ilya@laptop.(none)>
2010-08-16 15:51:31 -0700

    removed many posts association within group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,11 +5,9 @@ class Group
 
   key :person_ids, Array
   key :request_ids, Array
-  key :post_ids, Array
 
   many :people, :in => :person_ids, :class_name => 'Person'
   many :requests, :in => :request_ids, :class_name => 'Request'
-  many :posts, :in => :post_ids, :class_name => 'Post'
 
   belongs_to :user, :class_name => 'User'
 
