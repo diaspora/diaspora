16f53922c5d913670655cbddbb32ace95c278ba3
Raphael <raphael@joindiaspora.com>
2010-08-17 11:22:06 -0700

    nginx conf done, comments deleted

--- config/sprinkle/packages/server.rb
+++ config/sprinkle/packages/server.rb
@@ -18,35 +18,3 @@ package :nginx_dependencies do
   apt %w( libc6 libpcre3 libpcre3-dev libssl0.9.8)
   source "http://zlib.net/zlib-1.2.5.tar.gz"
 end
-
-=begin
-package :mongrel do
-  description 'Mongrel Application Server'
-  gem 'mongrel'
-  version '1.1.5'
-end
-
-package :mongrel_cluster, :provides => :appserver do
-  description 'Cluster Management for Mongrel'
-  gem 'mongrel_cluster'
-  version '1.0.5'
-  requires :mongrel
-end
-
-package :apache, :provides => :webserver do
-  description 'Apache 2 HTTP Server'
-  version '2.2.15'
-	
-  source "http://download.nextag.com/apache/httpd/httpd-#{version}.tar.bz2" do
-    enable %w( mods-shared=all proxy proxy-balancer proxy-http rewrite cache headers ssl deflate so )
-    prefix "/opt/local/apache2-#{version}"
-    post :install, 'install -m 755 support/apachectl /etc/init.d/apache2', 'update-rc.d -f apache2 defaults'
-  end
-  requires :apache_dependencies
-end
-
-package :apache_dependencies do
-  description 'Apache 2 HTTP Server Build Dependencies'
-  apt %w( openssl libtool mawk zlib1g-dev libssl-dev )
-end
-=end
--- config/sprinkle/provision.rb
+++ config/sprinkle/provision.rb
@@ -1,37 +1,11 @@
 #!/usr/bin/env sprinkle -s
 
-# Annotated Example Sprinkle Rails deployment script
-#
-# This is an example Sprinkle script configured to install Rails from gems, Apache, Ruby,
-# Sphinx and Git from source, and mysql and Git dependencies from apt on an Ubuntu system.
-#
-# Installation is configured to run via capistrano (and an accompanying deploy.rb recipe script).
-# Source based packages are downloaded and built into /usr/local on the remote system.
-#
-# A sprinkle script is separated into 3 different sections. Packages, policies and deployment:
-
-
-# Packages (separate files for brevity)
-#
-#  Defines the world of packages as we know it. Each package has a name and
-#  set of metadata including its installer type (eg. apt, source, gem, etc). Packages can have
-#  relationships to each other via dependencies.
 
 require "#{File.dirname(__FILE__)}/packages/essential"
 require "#{File.dirname(__FILE__)}/packages/database"
 require "#{File.dirname(__FILE__)}/packages/server"
 require "#{File.dirname(__FILE__)}/packages/scm"
 require "#{File.dirname(__FILE__)}/packages/ruby"
-#require "#{File.dirname(__FILE__)}/packages/unfortunately_essential"
-
-# Policies
-#
-#  Names a group of packages (optionally with versions) that apply to a particular set of roles:
-#
-#   Associates the rails policy to the application servers. Contains rails, and surrounding
-#   packages. Note, appserver, database, webserver and search are all virtual packages defined above.
-#   If there's only one implementation of a virtual package, it's selected automatically, otherwise
-#   the user is requested to select which one to use.
 
 policy :diaspora, :roles => [:pivots] do
 #  requires :clean_dreamhost
@@ -45,14 +19,6 @@ policy :diaspora, :roles => [:pivots] do
   requires :vim
 end
 
-# Deployment
-#
-#  Defines script wide settings such as a delivery mechanism for executing commands on the target
-#  system (eg. capistrano), and installer defaults (eg. build locations, etc):
-#
-#   Configures spinkle to use capistrano for delivery of commands to the remote machines (via
-#   the named 'deploy' recipe). Also configures 'source' installer defaults to put package gear
-#   in /usr/local
 
 deployment do
 
@@ -73,5 +39,3 @@ deployment do
   end 
 end
 
-# End of script, given the above information, Spinkle will apply the defined policy on all roles using the
-# deployment settings specified.
