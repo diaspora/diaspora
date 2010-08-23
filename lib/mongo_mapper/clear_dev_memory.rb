74be86395b9ec44a4cbbb5dec618dd314b6b046a
Raphael <raphael@joindiaspora.com>
2010-08-23 16:33:44 -0700

    Changing env back to development mode, the memory leak is likely fixed

---
+++ lib/mongo_mapper/clear_dev_memory.rb
@@ -0,0 +1,19 @@
+module MongoMapper
+  class PerRequestMapClear
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if Rails.configuration.cache_classes
+      else
+        MongoMapper::Document.descendants.each do |m|
+          m.descendants.clear if m.respond_to? :descendants
+        end
+        MongoMapper::Document.descendants.clear
+        MongoMapper::EmbeddedDocument.descendants.clear
+      end
+      @app.call(env)
+    end
+  end
+end
