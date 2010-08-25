17636f4fd26c9745806bc1bb39f810172bf5a6f6
Raphael <raphael@joindiaspora.com>
2010-08-25 13:51:16 -0700

    View modifications, refactored stream javascript to another file

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 	
-	$('.comment_set').each(function(index) {
-	    if($(this).children().length > 1) {
-			$(this).parent().show();
-			var show_comments_toggle = $(this).parent().prev().children(".show_post_comments");
-			show_comments_toggle.html("hide comments ("+ ($(this).children().length - 1) + ")");
-		};
-  });
 
 	$('#debug_info').click(function() {
 		$('#debug_more').toggle('fast', function() {
@@ -18,26 +11,6 @@ $(document).ready(function(){
 	
   $('#flash_notice, #flash_error, #flash_alert').delay(2500).slideUp(130);
   
-  $("#stream li").live('mouseover',function() {
-    $(this).children(".destroy_link").fadeIn(0);
-  });
-
-  $("#stream li").live('mouseout',function() {
-    $(this).children(".destroy_link").fadeOut(0);
-  });
-
-  $(".show_post_comments").live('click', function(event) {
-    event.preventDefault();
-    if( $(this).hasClass( "visible")) {
-      $(this).html($(this).html().replace("hide", "show"));
-      $(this).closest("li").children(".content").children(".comments").fadeOut(100);
-    } else {
-      $(this).html($(this).html().replace("show", "hide"));
-      $(this).closest("li").children(".content").children(".comments").fadeIn(100);
-    }
-    $(this).toggleClass( "visible" );
-  });
-
 //Called with $(selector).clearForm()
 	$.fn.clearForm = function() {
 		return this.each(function() {
@@ -59,27 +32,6 @@ $(document).ready(function(){
     random: 1,
     timeout: 2000,
     speed: 3000
-  });
-
-
-  //comments/////
-
-  $(".comment_box").live('focus', function(evt){
-    var $this = $(this);
-    $this.attr("rows", 2);
-    $this.parents("p").parents("form").children("p").children(".comment_submit").fadeIn(200);
-  });
-
-  $(".comment_box").live('blur', function(evt){
-    var $this = $(this);
-    if( $this.val() == '' ) {
-      $this.parents("p").parents("form").children("p").children(".comment_submit").fadeOut(0);
-      $this.attr("rows", 1);
-    }
-  });
-
-  $(".comment_submit").live('click', function(evt){
-    $this.parents("p").parents("form").children("p").children(".comment_box").attr("rows", 1);
   });
 
   //buttons//////
@@ -109,9 +61,7 @@ $(document).ready(function(){
     $(this).fadeIn("slow");
   });
 
-  $(".delete").hover(function(){
-    $(this).toggleClass("button");
-  });
+
 
 });//end document ready
 
