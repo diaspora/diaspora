19e8aa5c352bb200dfc29b536df0079c23e282bc
Raphael <raphael@joindiaspora.com>
2010-08-26 11:06:35 -0700

    Refactored view js

---
+++ public/javascripts/photos.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  reset_photo_fancybox();
+
+  $("#add_photo_button").fancybox({
+    'onClosed'   :   function(){
+      if($("#add_photo_button").hasClass("uploading_complete")){
+        $("#add_photo_button").removeClass("uploading_complete");
+        reset_photo_fancybox();
+      }
+    }
+  });
+
+  $(".image_thumb img").load( function() {
+    $(this).fadeIn("slow");
+  });
+
+});//end document ready
+
+function reset_photo_fancybox(){
+        album_id = $(".album_id")[0].id;
+        ajax = $.get("/photos/new?album_id=" + album_id, function(){
+          $("#new_photo_pane").html(ajax.responseText)
+        });
+}
