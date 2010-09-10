04883f914fc855119bdbca1c9199b4e27df7a755
Raphael <raphael@joindiaspora.com>
2010-09-10 16:43:31 -0700

    Remove unused webhooks methods

--- lib/diaspora/webhooks.rb
+++ lib/diaspora/webhooks.rb
@@ -2,27 +2,6 @@ module Diaspora
   module Webhooks
     def self.included(klass)
       klass.class_eval do
-        require 'message_handler'
-
-        @@queue = MessageHandler.new
-
-        def push_to(recipients)
-          unless recipients.empty?
-            recipients.map!{|x| x = x.receive_url }
-            xml = to_diaspora_xml
-            Rails.logger.debug("Adding xml for #{self} to message queue to #{recipients}")
-            @@queue.add_post_request( recipients, xml )
-          end
-          @@queue.process
-        end
-
-        def push_to_url(url)
-          hook_url = url 
-          xml = to_diaspora_xml
-          Rails.logger.debug("Adding xml for #{self} to message queue to #{url}")
-          @@queue.add_post_request( hook_url, xml )
-          @@queue.process
-        end
 
         def to_diaspora_xml
           xml = "<XML>"
@@ -30,17 +9,7 @@ module Diaspora
           xml += "</XML>"
         end
 
-        def people_with_permissions
-          begin
-          friends = self.person.owner.friends.all
-          Rails.logger.error("Dan is wrong!") if friends.nil?
-          friends ||= []
-          rescue
-            Rails.logger.fatal("Called people_with_permissions on a post from a remote user.  We need to implement this shit.")
-            []
-          end
-        end
-      end
+       end
     end
   end
 end
