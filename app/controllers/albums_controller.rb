4c15b1a01c6f02dccf5a78f2593003a0dc7ce4fb
Daniel Vincent Grippi <Dan@SPEEDRACER.local>
2010-09-13 14:23:50 -0700

    added license to every single goddamn file.  also, put one in the root dir.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,22 @@
+#    Copyright 2010 Diaspora Inc.
+#
+#    This file is part of Diaspora.
+#
+#    Diaspora is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Diaspora is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Diaspora.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 class AlbumsController < ApplicationController
   before_filter :authenticate_user!
 
