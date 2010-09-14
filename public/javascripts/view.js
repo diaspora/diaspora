15651a7bb6a2bf2b7eed54ac83a7d2e45314c719
maxwell <maxwell@joindiaspora.com>
2010-09-13 18:10:07 -0700

    IZ MS group manage page can now add friends to groups

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -37,6 +37,7 @@ $(document).ready(function(){
   //buttons//////
   $("#add_aspect_button").fancybox({ 'titleShow' : false });
   $("#add_request_button").fancybox({ 'titleShow': false });
+  $(".add_request_button").fancybox({ 'titleShow': false });
 
   $("input[type='submit']").addClass("button");
 
