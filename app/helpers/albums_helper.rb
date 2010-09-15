aa3ebe372cf05497d34a272c83a49498954e766f
Raphael <raphael@joindiaspora.com>
2010-09-15 10:58:48 -0700

    License notices replaced

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,20 +1,6 @@
-#    Copyright 2010 Diaspora Inc.
-#
-#    This file is part of Diaspora.
-#
-#    Diaspora is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Diaspora is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with Diaspora.  If not, see <http://www.gnu.org/licenses/>.
-#
+#   Copyright (c) 2010, Disapora Inc.  This file is
+#   licensed under the Affero General Public License version 3.  See
+#   the COPYRIGHT file.
 
 
 module AlbumsHelper
