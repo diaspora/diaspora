89b063a1a439f8d5dc4b518cef587e7e73a389bc
Jarin Udom <jarin@robotmo.de>
2010-09-16 02:39:18 -0700

    nil does not respond to to_sym

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     config.storage = :file
   end
 
-  if ENV['ATTACHMENT_STORAGE'].to_sym == :s3
+  if ENV['ATTACHMENT_STORAGE'] == "s3"
     config.s3_access_key_id = ENV['S3_KEY'] 
     config.s3_secret_access_key = ENV['S3_SECRET'] 
     config.s3_bucket = ENV['S3_BUCKET']
