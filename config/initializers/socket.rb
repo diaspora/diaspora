46a83324ab63b67fa3f85fcefa705aef9ab1b77b
Michael Nutt <michael@nuttnet.net>
2010-09-16 02:34:11 -0400

    Warn if can't attach to websocket port

--- config/initializers/socket.rb
+++ config/initializers/socket.rb
@@ -9,18 +9,26 @@ require "lib/diaspora/websocket"
   EM.next_tick {
     Diaspora::WebSocket.initialize_channels
 
-    EventMachine::WebSocket.start(
-                  :host => "0.0.0.0",
-                  :port => APP_CONFIG[:socket_port],
-                  :debug =>APP_CONFIG[:socket_debug]) do |ws|
-      ws.onopen {
+    begin
+      EventMachine::WebSocket.start(
+                    :host => "0.0.0.0",
+                    :port => APP_CONFIG[:socket_port],
+                    :debug =>APP_CONFIG[:socket_debug]) do |ws|
+        ws.onopen {
 
-        sid = Diaspora::WebSocket.subscribe(ws.request['Path'].gsub('/',''), ws)
+          sid = Diaspora::WebSocket.subscribe(ws.request['Path'].gsub('/',''), ws)
 
-        ws.onmessage { |msg| SocketsController.new.incoming(msg) }
+          ws.onmessage { |msg| SocketsController.new.incoming(msg) }
 
-        ws.onclose { Diaspora::WebSocket.unsubscribe(ws.request['Path'].gsub('/',''), sid) }
-      }
+          ws.onclose { Diaspora::WebSocket.unsubscribe(ws.request['Path'].gsub('/',''), sid) }
+        }
+      end
+    rescue RuntimeError => e
+      if e.message =~ /no acceptor/
+        raise RuntimeError, "Could not listen on port #{APP_CONFIG[:socket_port]}, perhaps another process is already using it?"
+      else
+        raise
+      end
     end
   }
 
