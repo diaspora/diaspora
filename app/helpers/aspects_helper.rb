7c03f320be223ead936f02030349c551f3da8d56
danielvincent <daniel@joindiaspora.com>
2010-09-17 07:03:18 +0800

    DG IZ; aspect deletion

--- app/helpers/aspects_helper.rb
+++ app/helpers/aspects_helper.rb
@@ -7,4 +7,12 @@ module AspectsHelper
   def link_for_aspect( aspect )
     link_to aspect.name, aspect
   end
+
+  def remove_link( aspect )
+    if aspect.people.size == 0
+      link_to "remove", aspect, :method => :delete
+    else
+      "<span class='grey' title='Aspect not empty'>remove</span>"
+    end
+  end
 end
