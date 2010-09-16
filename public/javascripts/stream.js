708e9f88a55cc37130f93cc47b820f62fbf48d2d
Raphael <raphael@joindiaspora.com>
2010-09-15 17:56:18 -0700

    Spell our own name right

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -1,4 +1,4 @@
-/*   Copyright (c) 2010, Disapora Inc.  This file is
+/*   Copyright (c) 2010, Diaspora Inc.  This file is
 *   licensed under the Affero General Public License version 3.  See
 *   the COPYRIGHT file.
 */
