dc6f584ace5b8e8ce74d37c58f45e28f2c25c234
Jarin Udom <jarin@robotmo.de>
2010-09-16 01:31:40 -0700

    Fix MongoMapper config for Heroku

--- config/initializers/_mongo.rb
+++ config/initializers/_mongo.rb
@@ -4,11 +4,11 @@
 
 if ENV['MONGOHQ_URL']
   MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "diaspora-#{Rails.env}"
 end
-
-MongoMapper.database = "diaspora-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
