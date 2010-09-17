c6cdf65fc8879022e7d0e70f39e1790bc4f84911
Ariel Zavala <arielo.zavala@gmail.com>
2010-09-17 10:26:57 -0400

    updated local configuration

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,5 +4,5 @@
 
 
 # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-I18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :en
