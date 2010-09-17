71ac61c01d8166b8f6354f65bb8c166d293d6c5b
Raphael <raphael@joindiaspora.com>
2010-09-17 12:49:05 -0700

    Fix websocket specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,9 +46,15 @@ RSpec.configure do |config|
   end
 end
   def stub_sockets
-    Diaspora::WebSocket.stub!(:push_to_user).and_return(true)
+    Diaspora::WebSocket.stub!(:queue_to_user).and_return(true)
     Diaspora::WebSocket.stub!(:subscribe).and_return(true)
     Diaspora::WebSocket.stub!(:unsubscribe).and_return(true)
+  end
+
+  def unstub_sockets
+    Diaspora::WebSocket.unstub!(:queue_to_user)
+    Diaspora::WebSocket.unstub!(:subscribe)
+    Diaspora::WebSocket.unstub!(:unsubscribe)
   end
 
   def stub_signature_verification
