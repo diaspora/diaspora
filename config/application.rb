70893101abebdebf8869f504ae71b6b1039e467f
sloser <sloser@emusoft.hr>
2010-09-18 16:05:19 +0200

    i18n for Croatian (hrvatski) language

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Diaspora
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
      config.generators do |g|
