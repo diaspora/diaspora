bcdc42270f76dfc84e4d0b12482b208750095dab
Daniel Thomas <drt24@srcf.ucam.org>
2010-09-19 21:03:16 +0100

    Standardise formatting in ubuntu-setup.bash
    Original author was using 4 spaces for indentation so standardise on
    that.
    Use only 80 characters per line to improve readability and make diffs
    more useful.
    
    Signed-off-by: Daniel Thomas <drt24@srcf.ucam.org>

--- ubuntu-setup.bash
+++ ubuntu-setup.bash
@@ -10,14 +10,17 @@ shopt -s extglob
 sudo -v >/dev/null 2>&1 || { echo $(whoami) has no sudo privileges ; exit 1; }
 
 # Check if universal repository is enabled 
-grep -i universe /etc/apt/sources.list > /dev/null || { echo "Please enable universe repository" ; exit 1 ; }
+grep -i universe /etc/apt/sources.list > /dev/null || \
+    { echo "Please enable universe repository" ; exit 1 ; }
 
 # Check if wget is installed 
-test wget || echo "Installing wget.." && sudo apt-get install wget && echo "Installed wget.."
+test wget || echo "Installing wget.." && sudo apt-get install wget \
+    && echo "Installed wget.."
 
 # Install build tools 
 echo "Installing build tools.."
-sudo apt-get -y --no-install-recommends install build-essential libxslt1.1 libxslt1-dev libxml2
+sudo apt-get -y --no-install-recommends install \
+    build-essential libxslt1.1 libxslt1-dev libxml2
 echo "..Done installing build tools"
 
 # Install Ruby 1.8.7 
@@ -61,29 +64,29 @@ echo "Fetching and installing ruby gems.."
 (
     RELEASE=$(lsb_release -c | cut -f2)
     if [ RELEASE == "maverick" ]
-	then
-    	sudo apt-get install --no-install-recommends -y rubygems
-		sudo ln -s /var/lib/gems/1.8/bin/bundle /usr/local/bin/bundle #for PATH
-	elif [ RELEASE == "lucid" ]
+    then
+        sudo apt-get install --no-install-recommends -y rubygems
+        sudo ln -s /var/lib/gems/1.8/bin/bundle /usr/local/bin/bundle #for PATH
+    elif [ RELEASE == "lucid" ]
     then
         sudo add-apt-repository ppa:maco.m/ruby
         sudo apt-get update
-    	sudo apt-get install --no-install-recommends -y rubygems
-		sudo ln -s /var/lib/gems/1.8/bin/bundle /usr/local/bin/bundle #for PATH
-	else
-		# Old version
-		echo "."
-		cd /tmp 
-		wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
-		echo "."
-		tar -xf rubygems-1.3.7.tgz
-		echo "."
-		cd rubygems-1.3.7
-		echo "."
-		sudo ruby setup.rb
-		echo "."
-		sudo ln -s /usr/bin/gem1.8 /usr/bin/gem 
-		echo "."    
+        sudo apt-get install --no-install-recommends -y rubygems
+        sudo ln -s /var/lib/gems/1.8/bin/bundle /usr/local/bin/bundle #for PATH
+    else
+        # Old version
+        echo "."
+        cd /tmp
+        wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
+        echo "."
+        tar -xf rubygems-1.3.7.tgz
+        echo "."
+        cd rubygems-1.3.7
+        echo "."
+        sudo ruby setup.rb
+        echo "."
+        sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
+        echo "."
     fi  
 ) 
 echo "Done installing the gems.."
@@ -95,22 +98,25 @@ echo "Installed bundler.."
 
 # Take a clone of Diaspora
 (
-# Check if the user is already in a cloned source if not clone the source 
-[[ $( basename $PWD ) == "diaspora" ]]  && echo "Already in diaspora directory" ||  git clone http://github.com/diaspora/diaspora.git ; cd diaspora 
-echo "Cloned the source.."
+    # Check if the user is already in a cloned source if not clone the source
+    [[ $( basename $PWD ) == "diaspora" ]]  && \
+        echo "Already in diaspora directory" ||  \
+        git clone http://github.com/diaspora/diaspora.git ; cd diaspora
+    echo "Cloned the source.."
 
-# Install extra gems 
-cd diaspora
-echo "Installing more gems.."
-sudo bundle install
-echo "Installed."
+    # Install extra gems
+    cd diaspora
+    echo "Installing more gems.."
+    sudo bundle install
+    echo "Installed."
 
-# Install DB setup 
-echo "Seting up DB.."
-rake db:seed:tom
-echo "DB ready. Login -> tom and password -> evankorth. More details ./diaspora/db/seeds/tom.rb."
+    # Install DB setup
+    echo "Seting up DB.."
+    rake db:seed:tom
+    echo "DB ready. Login -> tom and password -> evankorth.\n"\
+    "More details ./diaspora/db/seeds/tom.rb."
 
-# Run appserver 
-echo "Starting server"
-bundle exec thin start 
+    # Run appserver
+    echo "Starting server"
+    bundle exec thin start
 )
