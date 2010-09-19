714f3bd0ed2d1d04e71689af86825f7d3c0d1e2f
Guillermo <guilleiguaran@gmail.com>
2010-09-18 22:55:26 -0500

    Fixing connection with MONGOHQ_URL

--- config/initializers/_mongo.rb
+++ config/initializers/_mongo.rb
@@ -2,13 +2,14 @@
 #   licensed under the Affero General Public License version 3.  See
 #   the COPYRIGHT file.
 
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+if ENV['MONGOHQ_URL']	
+  MongoMapper.config = {Rails.env => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(Rails.env)
+  Magent.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "diaspora-#{Rails.env}"	
 end
-
-MongoMapper.database = "diaspora-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
