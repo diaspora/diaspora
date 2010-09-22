63ed6a3a9505850affef46544fd5cf3fbae5bb37
maxwell <maxwell@joindiaspora.com>
2010-09-21 21:31:53 -0700

    MS DG made async webfingering controller.  need to call it from javascript to prevent seeds from blocking.  remember to update your gems

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Diaspora::Application.routes.draw do
   match 'signup', :to => 'registrations#new', :as => "new_user_registration"
 
   match 'get_to_the_choppa', :to => redirect("/signup") 
+  match "/finger", :to => AsyncController
   #public routes
   #
   match 'webfinger', :to => 'publics#webfinger'
