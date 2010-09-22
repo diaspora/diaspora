b01928af4bdfb43201ec8abda59b4f4eb92869b7
ilya <ilya@laptop.(none)>
2010-09-21 17:52:11 -0700

    RS IZ if yaml fails to load (empty config) file load the example

--- config/initializers/_load_app_config.rb
+++ config/initializers/_load_app_config.rb
@@ -4,6 +4,12 @@
 
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
 all_envs = YAML.load(raw_config)
+ 
+unless all_envs
+  raw_config = File.read("#{Rails.root}/config/app_config_example.yml")
+  all_envs = YAML.load(raw_config)
+end
+
 if all_envs[Rails.env]
   APP_CONFIG = all_envs['default'].merge(all_envs[Rails.env]).symbolize_keys
 else
