894197522443eaa5b2989f1e2dbddb5ade9ba630
danielvincent <danielgrippi@gmail.com>
2010-09-22 10:59:23 -0700

    DG IZ; example extension convention for app_config

--- db/seeds/backer.rb
+++ db/seeds/backer.rb
@@ -42,7 +42,7 @@ def create
 end
 
 def set_app_config username
-  current_config = YAML.load(File.read(Rails.root.join('config', 'app_config_example.yml')))
+  current_config = YAML.load(File.read(Rails.root.join('config', 'app_config.yml.example')))
   current_config[Rails.env.to_s] ||= {}
   current_config[Rails.env.to_s]['pod_url'] = "#{username}.joindiaspora.com"
   current_config['default']['pod_url'] = "#{username}.joindiaspora.com"
