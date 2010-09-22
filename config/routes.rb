5ef32b7d55947f745256d98658f717da3f91297c
danielvincent <danielgrippi@gmail.com>
2010-09-22 12:03:25 -0700

    DG IZ; a user can repost.  aspects are not sent through the websocket.  post does not show where it curently is going in its partial

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@
 Diaspora::Application.routes.draw do
   resources :people, :only => [:index, :show, :destroy]
   resources :users, :except => [:create, :new, :show]
-  resources :status_messages, :only => [:create, :destroy, :show]
+  resources :status_messages
   resources :comments, :except => [:index]
   resources :requests, :except => [:edit, :update]
   resources :photos, :except => [:index]
   resources :albums
 
+  match 'repost', :to => 'application#repost', :as => 'repost'
+  
   match 'aspects/move_friends', :to => 'aspects#move_friends', :as => 'move_friends'
   match 'aspects/move_friend', :to => 'aspects#move_friend', :as => 'move_friend'
   match 'aspects/manage', :to => 'aspects#manage'
