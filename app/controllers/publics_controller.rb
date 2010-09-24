16b6fc6cdc613de614ed6fc8fe1a284abf1b3d1d
Raphael <raphael@joindiaspora.com>
2010-09-23 17:18:34 -0700

    Don't give exception on webfinger with no query

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -20,7 +20,7 @@ class PublicsController < ApplicationController
   end
 
   def webfinger
-    @person = Person.by_webfinger(params[:q], :local => true)
+    @person = Person.by_webfinger(params[:q], :local => true) if params[:q]
     unless @person.nil? || @person.owner.nil?
       render 'webfinger', :content_type => 'application/xrd+xml'
     else
