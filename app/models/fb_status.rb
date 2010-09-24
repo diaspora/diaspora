bb5867855695a8ad9dd69c6856f721129a00cbc9
ilya <ilya@laptop.(none)>
2010-09-24 14:41:31 -0700

    added a fb_status model & parser method

---
+++ app/models/fb_status.rb
@@ -0,0 +1,30 @@
+#   Copyright (c) 2010, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3.  See
+#   the COPYRIGHT file.
+
+
+class FbStatus 
+  include MongoMapper::Document
+
+  key :graph_id, String
+  key :author_id, String
+  key :author_name, String
+  key :message, String
+  key :updated_time, DateTime
+
+  timestamps!
+
+  validates_presence_of :graph_id,:author_id,:author_name,:message,:updated_time
+
+  def self.from_api(json)
+    hash = JSON.parse(json)
+    self.create( 
+                  :graph_id     => hash['id'],
+                  :author_id    => hash['from']['id']
+                  :author_name  => hash['from']['name'],
+                  :message      => hash['message']
+                  :updated_time => Time.parse(hash['updated_time']
+  end
+  
+end
+
