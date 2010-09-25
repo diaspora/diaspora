5f2d4ba07318866c6d95a2ad0cf69f030a0836e7
Stephen Caudill <scaudill@gmail.com>
2010-09-25 15:22:10 -0400

    Use the mongo_mapper and cucumber Cleaner strategy

--- features/support/env.rb
+++ features/support/env.rb
@@ -37,10 +37,9 @@ ActionController::Base.allow_rescue = false
 
 # How to clean your database when transactions are turned off. See
 # http://github.com/bmabey/database_cleaner for more info.
-if defined?(ActiveRecord::Base)
-  begin
-    require 'database_cleaner'
-    DatabaseCleaner.strategy = :truncation
-  rescue LoadError => ignore_if_database_cleaner_not_present
-  end
+begin
+ require 'database_cleaner'
+ require 'database_cleaner/cucumber'
+ DatabaseCleaner.strategy = :truncation
+ DatabaseCleaner.orm = "mongo_mapper"
 end
