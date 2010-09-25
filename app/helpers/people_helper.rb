6055e63e71f771054265e79ee0bb67006756e7e1
Dorian <pr.dorian@gmail.com>
2010-09-25 12:54:04 +0200

    Improve display of name

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -4,13 +4,15 @@
 
 
 module PeopleHelper
-
   def search_or_index
     if !params[:q].nil? && !params[:q].empty?
       I18n.t 'people.helper.results_for',:params => params[:q]
     else
       I18n.t "people.helper.people_on_pod_are_aware_of"
     end
+  end
 
+  def enhance_name handle
+    handle.gsub(/^([^@]+)/, '<strong>\1</strong>')
   end
 end
