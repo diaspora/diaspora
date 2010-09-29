046dbf063b437f037f2fc30226e13e7b16d046a0
Jeffry Degrande <jeffry@jeffrydegrande.com>
2010-09-29 01:49:35 -0300

    fix i18n => I18n

--- app/controllers/aspects_controller.rb
+++ app/controllers/aspects_controller.rb
@@ -29,7 +29,7 @@ class AspectsController < ApplicationController
 
     begin
       current_user.drop_aspect @aspect
-      flash[:notice] = i18n.t 'aspects.destroy.success',:name => @aspect.name
+      flash[:notice] = I18n.t 'aspects.destroy.success',:name => @aspect.name
     rescue RuntimeError => e 
       flash[:error] = e.message
     end
@@ -55,7 +55,7 @@ class AspectsController < ApplicationController
 
     data = clean_hash(params[:aspect])
     @aspect.update_attributes( data )
-    flash[:notice] = i18n.t 'aspects.update.success',:name => @aspect.name
+    flash[:notice] = I18n.t 'aspects.update.success',:name => @aspect.name
     respond_with @aspect
   end
 
@@ -63,13 +63,13 @@ class AspectsController < ApplicationController
     params[:moves].each{ |move|
       move = move[1]
       unless current_user.move_friend(move)
-        flash[:error] = i18n.t 'aspects.move_friends.failure', :real_name => Person.find_by_id( move[:friend_id] ).real_name
+        flash[:error] = I18n.t 'aspects.move_friends.failure', :real_name => Person.find_by_id( move[:friend_id] ).real_name
         redirect_to aspects_manage_path
         return
       end
     }
 
-    flash[:notice] = i18n.t 'aspects.move_friends.success'
+    flash[:notice] = I18n.t 'aspects.move_friends.success'
     redirect_to aspects_manage_path
   end
 
