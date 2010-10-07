2a104ea62240eab7b888a8b425be0436bffe0c15
ilya <ilya@laptop.(none)>
2010-10-07 14:26:59 -0700

    Revert "moved the SMTP setting into app_config.yml"
    This reverts commit 2451ac49575c58391cbafcc14b5fabb2e48a4266.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,14 +49,14 @@ Diaspora::Application.configure do
   config.threadsafe!
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => APP_CONFIG[:terse_pod_url]}
+  config.action_mailer.default_url_options = {:host => 'pivots.joindiaspora.com'}
   config.action_mailer.smtp_settings = {
-    :address => APP_CONFIG[:smtp_address],
-    :port => APP_CONFIG[:smtp_port],
-    :domain => APP_CONFIG[:smtp_domain],
-    :authentication => APP_CONFIG[:smtp_authentication],
-    :user_name => APP_CONFIG[:smtp_username],
-    :password => APP_CONFIG[:smtp_password],
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'mail.joindiaspora.com',
+    :authentication => 'plain',
+    :user_name => 'diaspora-pivots@joindiaspora.com',
+    :password => "xy289|]G+R*-kA",
     :enable_starttls_auto => true
   }
 end
