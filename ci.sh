546401cbecb81bd151a2f874833b717c3b4d6ef0
Sarah Mei <sarahmei@gmail.com>
2010-10-11 22:37:58 -0700

    Gemfile.lock is different in 1.9.2, so we need to remove it after running specs so the next pull succeeds.

--- ci.sh
+++ ci.sh
@@ -4,10 +4,17 @@ echo "**************************************************************************
 echo "*                                  ruby 1.8.7-p249 build                                        *" &&
 echo "*************************************************************************************************" &&
 echo "" &&
-source /usr/local/rvm/scripts/rvm && rvm use ruby-1.8.7-p249 && bundle install && bundle exec rake ci &&
+source /usr/local/rvm/scripts/rvm &&
+rvm use ruby-1.8.7-p249 &&
+bundle install &&
+bundle exec rake ci &&
 echo "" &&
 echo "*************************************************************************************************" &&
 echo "*                                   ruby 1.9.2-p0 build                                         *" &&
 echo "*************************************************************************************************" &&
 echo "" &&
-source /usr/local/rvm/scripts/rvm && rvm use ruby-1.9.2-p0 && bundle install && bundle exec rake ci
+source /usr/local/rvm/scripts/rvm &&
+rvm use ruby-1.9.2-p0 &&
+bundle install &&
+bundle exec rake ci &&
+rm Gemfile.lock
