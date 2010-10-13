96219b736093b8923c21273e44d9f2d76842da12
Robin Nilsson <robinnilsson@gmail.com>
2010-10-14 00:20:45 +0200

    Making fancybox modal

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -26,10 +26,10 @@ $(document).ready(function(){
   });
 
   //buttons//////
-  $("#add_aspect_button").fancybox({ 'titleShow' : false });
-  $("#add_request_button").fancybox({ 'titleShow': false });
-  $("#invite_user_button").fancybox({ 'titleShow': false });
-  $(".add_request_button").fancybox({ 'titleShow': false });
+  $("#add_aspect_button").fancybox({ 'titleShow' : false , 'hideOnOverlayClick' : false });
+  $("#add_request_button").fancybox({ 'titleShow': false , 'hideOnOverlayClick' : false });
+  $("#invite_user_button").fancybox({ 'titleShow': false , 'hideOnOverlayClick' : false });
+  $(".add_request_button").fancybox({ 'titleShow': false , 'hideOnOverlayClick' : false });
 
   $("input[type='submit']").addClass("button");
 
