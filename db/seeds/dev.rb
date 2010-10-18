74c6446c22cfe1caa432fa487a7b1b42046c1583
Raphael <raphael@joindiaspora.com>
2010-10-18 12:56:22 -0700

    Don't overwrite existing pod_url in db:seed:dev

--- db/seeds/dev.rb
+++ db/seeds/dev.rb
@@ -7,8 +7,8 @@ require File.join(File.dirname(__FILE__), "..", "..", "config", "environment")
 def set_app_config username
   current_config = YAML.load(File.read(Rails.root.join('config', 'app_config.yml.example')))
   current_config[Rails.env.to_s] ||= {}
-  current_config[Rails.env.to_s]['pod_url'] = "#{username}.joindiaspora.com"
-  current_config['default']['pod_url'] = "#{username}.joindiaspora.com"
+  current_config[Rails.env.to_s]['pod_url'] ||= "#{username}.joindiaspora.com"
+  current_config['default']['pod_url'] ||= "#{username}.joindiaspora.com"
   file = File.new(Rails.root.join('config','app_config.yml'),'w')
   file.write(current_config.to_yaml)
   file.close
