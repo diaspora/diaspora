82e947a74a57e9ca394a45f5fbe2fe1c8f80428a
Sarah Mei <sarahmei@gmail.com>
2010-10-20 22:44:18 -0700

    Disabling ruby 1.9 build until we can move off capybara

--- ci.sh
+++ ci.sh
@@ -8,14 +8,15 @@ rm -f Gemfile.lock &&
 source /usr/local/rvm/scripts/rvm &&
 rvm use ruby-1.8.7-p249@diaspora &&
 bundle install &&
-bundle exec rake cruise &&
-echo "" &&
-echo "*************************************************************************************************" &&
-echo "*                                   ruby 1.9.2-p0 build                                         *" &&
-echo "*************************************************************************************************" &&
-echo "" &&
-rm -f Gemfile.lock &&
-source /usr/local/rvm/scripts/rvm &&
-rvm use ruby-1.9.2-p0@diaspora &&
-bundle install &&
 bundle exec rake cruise
+# bundle exec rake cruise &&
+# echo "" &&
+# echo "*************************************************************************************************" &&
+# echo "*                                   ruby 1.9.2-p0 build                                         *" &&
+# echo "*************************************************************************************************" &&
+# echo "" &&
+# rm -f Gemfile.lock &&
+# source /usr/local/rvm/scripts/rvm &&
+# rvm use ruby-1.9.2-p0@diaspora &&
+# bundle install &&
+# bundle exec rake cruise
