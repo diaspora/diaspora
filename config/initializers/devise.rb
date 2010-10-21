5fa623552d30c28d17f0e9ee6047be24ba3f42e1
MrZYX <pr0fkill@gmail.com>
2010-10-21 19:47:30 +0200

    made smtp sender address configurable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = APP_CONFIG[:smtp_sender_address]
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default), :mongoid
