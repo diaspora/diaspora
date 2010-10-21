48c13d81c511399dfdb75503ab11307ee2b8298c
Sarah Mei <sarahmei@gmail.com>
2010-10-20 21:56:59 -0700

    rly rly remove Gemfile.lock. rly!

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
 
 Project.configure do |project|
-  project.build_command = 'source /usr/local/rvm/scripts/rvm && rvm use ruby-1.8.7-p249@diaspora && rm Gemfile.lock && gem update --system && ruby lib/cruise/build.rb'
+  project.build_command = 'source /usr/local/rvm/scripts/rvm && rvm use ruby-1.8.7-p249@diaspora && rm -f Gemfile.lock && gem update --system && ruby lib/cruise/build.rb'
 end
