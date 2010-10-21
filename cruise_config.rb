55914827db1b7d05797a337e80004d0078be33d5
Sarah Mei <sarahmei@gmail.com>
2010-10-20 21:06:33 -0700

    cruisecontrol uses rvm

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
 
 Project.configure do |project|
-  project.build_command = 'sudo gem update --system && ruby lib/cruise/build.rb'
+  project.build_command = 'cd .. && cd work && sudo gem update --system && ruby lib/cruise/build.rb'
 end
