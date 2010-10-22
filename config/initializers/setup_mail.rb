47b03c45b231ca507d8aa3c3432df7870abf3301
ilya <ilya@laptop.(none)>
2010-10-22 10:37:04 -0700

    commented out the email interceptor

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,1 +1,2 @@
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?  
+# if you wish to intercept emails to go to a particuar email address
+#ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?  
