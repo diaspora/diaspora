096a0308e1270f22cbde4a77b1468fd4b534f012
maxwell <maxwell@joindiaspora.com>
2010-10-22 17:12:28 -0700

    update attachment with emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,11 @@
 class Notifier < ActionMailer::Base
   default :from => "no-reply@joindiaspora.com"
-  
+  ATTACHMENT =  File.read("#{Rails.root}/public/images/diaspora_caps.png")  
+
   def new_request(recipient, sender)
     @receiver = recipient
     @sender = sender
-    attachments["diaspora_white.png"] = File.read("#{Rails.root}/public/images/diaspora_white.png")  
+    attachments["diaspora_white.png"] = ATTACHMENT 
 
     mail(:to => "#{recipient.real_name} <#{recipient.email}>",
     :subject => "new Diaspora* friend request from #{@sender.real_name}", :host => APP_CONFIG[:terse_pod_url])
@@ -14,7 +15,7 @@ class Notifier < ActionMailer::Base
     @receiver = recipient
     @sender = sender
     @aspect = aspect
-    attachments["diaspora_white.png"] = File.read("#{Rails.root}/public/images/diaspora_white.png")  
+    attachments["diaspora_white.png"] = ATTACHMENT 
     mail(:to => "#{recipient.real_name} <#{recipient.email}>",
     :subject => "#{@sender.real_name} has accepted your friend request on Diaspora*", :host => APP_CONFIG[:terse_pod_url])
   end
