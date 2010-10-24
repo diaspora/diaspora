661ad20a025506d655a0ad86bd3f34bedbc7e5b4
danielvincent <danielgrippi@gmail.com>
2010-10-24 13:47:42 -0700

    added delegate,  proper finders, and more chaining to stream.js.   removed div.comment_set from the DOM.

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -48,7 +48,13 @@ $(document).ready(function(){
     }
   );
 
-  $("#publisher textarea, .comment_box").keydown( function(e) {
+  $("#publisher").find("textarea").keydown( function(e) {
+    if (e.keyCode == 13) {
+      $(this).closest("form").submit();
+    }
+  });
+
+  $("#stream").delegate("textarea.comment_box", "keydown", function(e){
     if (e.keyCode == 13) {
       $(this).closest("form").submit();
     }
@@ -124,19 +130,5 @@ $(".make_profile_photo").live("click", function(){
       });
     }
   });
-});
-
-$(".comment_box").live("focus",function(evt){
-  var $this = $(this);
-  $this.attr("rows", 2);
-  $this.parents("p").parents("form").children("p").children(".comment_submit").fadeIn(200);
-});
-
-$(".comment_box").live('blur', function(evt){
-  var $this = $(this);
-  if( $this.val() == '' ) {
-    $this.parents("p").parents("form").children("p").children(".comment_submit").fadeOut(0);
-    $this.attr("rows", 1);
-  }
 });
 
