33410899d9db557b6ad593ad959e30483f82bb18
Sarah Mei <sarahmei@gmail.com>
2010-10-25 21:38:53 -0700

    Fix for build breakage

--- config/initializers/_load_services.rb
+++ config/initializers/_load_services.rb
@@ -8,9 +8,12 @@ end
 
 oauth_keys_file = "#{Rails.root}/config/oauth_keys.yml"
 
-if File.exist? oauth_keys_file
-  SERVICES = load_config_yaml(oauth_keys_file)
+
+SERVICES = nil
+silence_warnings do
+  if File.exist? oauth_keys_file
+    SERVICES = load_config_yaml(oauth_keys_file)
+  else
+    SERVICES = load_config_yaml("#{oauth_keys_file}.example")
+  end
 end
-
-SERVICES ||= load_config_yaml("#{oauth_keys_file}.example")
-
