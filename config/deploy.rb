493969e458b9e66eea30b2150121ca26eeaa2a80
maxwell <maxwell@joindiaspora.com>
2010-10-27 23:18:15 -0700

    trying a timer

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,10 @@
 #   licensed under the Affero General Public License version 3 or later.  See
 #   the COPYRIGHT file.
 
+require 'rubygems'
+require 'eventmachine'
 config = YAML.load_file(File.dirname(__FILE__) + '/deploy_config.yml')
+
 all = config['cross_server']
 
 set :backers,  config['servers']['backer']
@@ -134,10 +137,21 @@ namespace :db do
   task :tom_seed, :roles => :tom do
     run "cd #{current_path} && bundle exec rake db:seed:tom --trace RAILS_ENV=#{rails_env}"
     run "curl -silent -u tom@tom.joindiaspora.com:evankorth http://tom.joindiaspora.com/zombiefriends"
-    backers.each do |backer|
-      run "curl -silent -u  #{backer['username']}@#{backer['username']}.joindiaspora.com:#{backer['username']}#{backer['pin']} http://#{backer['username']}.joindiaspora.com/zombiefriendaccept"
-      #run "curl -silent -u  #{backer['username']}@#{backer['username']}.joindiaspora.com:#{backer['username']}#{backer['pin']} http://#{backer['username']}.joindiaspora.com/set_profile_photo"
-    end
+    
+    EM.run { 
+      q = EM::Queue.new 
+      
+      backers.each do |backer|
+        q.push( proc{run "curl -silent -u  #{backer['username']}@#{backer['username']}.joindiaspora.com:#{backer['username']}#{backer['pin']} http://#{backer['username']}.joindiaspora.com/zombiefriendaccept"})
+        
+      end
+     
+      timer = EventMachine::PeriodicTimer.new(5) do
+        puts "the time is #{Time.now}"
+      EM.stop  if q.size == 0
+      end
+
+    }
   end
 
   task :backer_seed, :roles => :backer do
