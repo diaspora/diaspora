8b3a9cd8011a132bbe2a6ee819d761026147c9b2
zhitomirskiyi <ilya@joindiaspora.com>
2010-10-27 17:02:10 -0700

    adding some zombie friend puts

--- app/controllers/dev_utilities_controller.rb
+++ app/controllers/dev_utilities_controller.rb
@@ -23,7 +23,14 @@ class DevUtilitiesController < ApplicationController
             rel_hash = {:friend => person}
             logger.info "Zombiefriending #{backer['given_name']} #{backer['family_name']}"
             logger.info "Calling send_friend_request with #{rel_hash[:friend]} and #{current_user.aspects.first}"
-            current_user.send_friend_request_to(rel_hash[:friend], current_user.aspects.first)
+            begin 
+
+            
+              current_user.send_friend_request_to(rel_hash[:friend], current_user.aspects.first)
+          rescue Exception => e 
+            logger.info e.inspect
+            puts e.inspect
+          end
           else
             puts "error: #{person}"
           end
