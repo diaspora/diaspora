f04634c41548c714c4d20007aab3349046c2c746
Sarah Mei <sarahmei@gmail.com>
2010-10-29 20:27:26 -0700

    Don't need to remove Gemfile.lock for 1.8.7 build

--- ci.sh
+++ ci.sh
@@ -4,7 +4,6 @@ echo "**************************************************************************
 echo "*                                  ruby 1.8.7 REE build                                         *" &&
 echo "*************************************************************************************************" &&
 echo "" &&
-rm -f Gemfile.lock &&
 source /usr/local/rvm/scripts/rvm &&
 rvm use ree@diaspora --create &&
 bundle install &&
