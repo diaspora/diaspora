cf82a0a9c8b1956a4761872f0e51e77e4ac2a542
livefromthemoon@gmail.com <livefromthemoon@gmail.com>
2010-10-31 21:55:49 +0100

    Better to set the default locale in an initializer than in a before_filter

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,4 +4,4 @@
 
 # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-I18n.default_locale = :en
+I18n.default_locale = DEFAULT_LANGUAGE
