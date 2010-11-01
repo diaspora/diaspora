fd55a56235d10baf2bb1320bee03adaaf9f87c15
Michael Sofaer <msofaer@pivotallabs.com>
2010-10-31 17:02:22 -0700

    Symlink the RVM wrapper scripts to /usr/local/bin

--- chef/cookbooks/centos/recipes/bootstrap.rb
+++ chef/cookbooks/centos/recipes/bootstrap.rb
@@ -7,3 +7,13 @@ end
 execute "eventmachine deps" do
   command "yum install -y gcc-c++"
 end
+
+def harden_ruby(ruby_string)
+  Dir.glob("/usr/local/rvm/wrappers/#{ruby_string}/*").each do |file|
+    link "/usr/local/bin/#{file.split('/').last}" do
+      to file
+    end
+  end
+end
+
+harden_ruby("ruby-1.8.7-p302")
