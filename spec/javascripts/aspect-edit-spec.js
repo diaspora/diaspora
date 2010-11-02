fad9c092121aa6a9a10801d1fcdf1c8d92d43781
Sarah Mei <sarahmei@gmail.com>
2010-11-02 13:37:27 -0700

    refactor aspect-edit.js in prep for jasmine testing

--- spec/javascripts/aspect-edit-spec.js
+++ spec/javascripts/aspect-edit-spec.js
@@ -1,27 +1,30 @@
-describe("editing aspects", function() {
+describe("AspectEdit", function() {
+
+//  describe("initialize", function() {
+//
+//  });
 
   describe("decrementRequestsCounter", function() {
     describe("when there is one request", function() {
       it("removes the counter from the new requests div", function() {
         $('#jasmine_content').html("<div class='new_requests'>Requests (1)</div>");
-        decrementRequestsCounter();
+        AspectEdit.decrementRequestsCounter();
         expect($('.new_requests').first().html()).toEqual("Requests");
       });
     });
     describe("when there is more than one request", function() {
       it("decrements the request counter", function() {
         $('#jasmine_content').html("<div class='new_requests'>Requests (67)</div>");
-        decrementRequestsCounter();
+        AspectEdit.decrementRequestsCounter();
         expect($('.new_requests').first().html()).toEqual("Requests (66)");
       });
     });
     describe("error cases", function() {
       it("fails silently if there are no requests", function() {
         $('#jasmine_content').html("<div class='new_requests'>Requests</div>");
-        decrementRequestsCounter();
+        AspectEdit.decrementRequestsCounter();
         expect($('.new_requests').first().html()).toEqual("Requests");
       });
     });
   });
-
 });
