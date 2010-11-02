2c903ea519978933717b465fa3614f2fccf0b2fe
Raphael <raphael@joindiaspora.com>
2010-11-02 16:20:10 -0700

    check that User#build does not save a Person

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -163,13 +163,16 @@ describe User do
         }
         @user = User.build(params)
       end
-      it "makes a valid user" do
-        @user.should be_valid
+      it "does not save" do
         @user.persisted?.should be_false
+        @user.person.persisted?.should be_false
         User.find_by_username("ohai").should be_nil
       end
       it 'saves successfully' do
+        @user.should be_valid
         @user.save.should be_true
+        @user.persisted?.should be_true
+        @user.person.persisted?.should be_true
         User.find_by_username("ohai").should == @user
       end
     end
