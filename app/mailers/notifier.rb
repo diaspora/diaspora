a995d250f10cb170f25a18f96d7384ee549a9faf
danielvincent <danielgrippi@gmail.com>
2010-11-02 17:19:49 -0700

    added white text on grey asset to mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,12 +2,12 @@ class Notifier < ActionMailer::Base
   include Magent::Async
   
   default :from => "no-reply@joindiaspora.com"
-  ATTACHMENT =  File.read("#{Rails.root}/public/images/diaspora_caps.png")  
+  ATTACHMENT =  File.read("#{Rails.root}/public/images/diaspora_white_on_grey.png")  
 
   def new_request(recipient, sender)
     @receiver = recipient
     @sender = sender
-    attachments["diaspora_white.png"] = ATTACHMENT 
+    attachments["diaspora_white_on_grey.png"] = ATTACHMENT 
 
     mail(:to => "#{recipient.real_name} <#{recipient.email}>",
     :subject => "new Diaspora* friend request from #{@sender.real_name}", :host => APP_CONFIG[:terse_pod_url])
