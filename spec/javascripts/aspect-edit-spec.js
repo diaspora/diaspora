dbdd6884a7f5345d4b345b967515e92c43e68995
Sarah Mei <sarahmei@gmail.com>
2010-11-03 22:19:20 -0700

    SM MS jasmine specs for aspect edit

--- spec/javascripts/aspect-edit-spec.js
+++ spec/javascripts/aspect-edit-spec.js
@@ -1,8 +1,38 @@
 describe("AspectEdit", function() {
 
-//  describe("initialize", function() {
-//
-//  });
+  describe("initialize", function() {
+    it("calls draggable on ul .person", function() {
+      spyOn($.fn, "draggable");
+      AspectEdit.initialize();
+      expect($.fn.draggable).toHaveBeenCalledWith(
+        {revert: true, start: AspectEdit.startDrag,
+         drag: AspectEdit.duringDrag, stop: AspectEdit.stopDrag});
+      expect($.fn.draggable.mostRecentCall.object.selector).toEqual("ul .person");
+    });
+    it("calls droppable on .aspect ul.dropzone", function() {
+      spyOn($.fn, "droppable");
+      AspectEdit.initialize();
+      expect($.fn.droppable).toHaveBeenCalledWith({hoverClass: 'active', drop: AspectEdit.onDropMove});
+      expect($.fn.droppable.calls[0].object.selector).toEqual(".aspect ul.dropzone");
+// This would be AWESOME:
+//      expect($.fn.droppable)
+//        .toHaveBeenCalled()
+//          .on(".aspect ul.dropzone")
+//          .with({});
+    });
+  });
+
+  describe("startDrag", function() {
+    it("animates the image", function() {
+      $('#jasmine_content').html('<ul><li class="person ui-draggable" data-aspect_id="4cae42e12367bca44e000005" data-guid="4cae42d32367bca44e000003" style="top: 0px; left: 0px; ">' +
+                  '<img alt="Alexander Hamiltom" class="avatar" data-person_id="4cae42d32367bca44e000003" src="/images/user/default.png?1287542906" original-title="Alexander Hamiltom" style="height: 70px; width: 70px; opacity: 1; display: inline; ">' +
+                '</li></ul>');
+      spyOn(AspectEdit, "animateImage");
+      $.proxy(AspectEdit.startDrag, $('.person.ui-draggable'))();
+      expect(AspectEdit.animateImage).toHaveBeenCalled();
+      expect(AspectEdit.animateImage.mostRecentCall.args[0]).toHaveClass("avatar");
+    });
+  });
 
   describe("decrementRequestsCounter", function() {
     describe("when there is one request", function() {
