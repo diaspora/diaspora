3bb6ab5545b3219b28e04d6f014c608d83e18fa6
Michael Sofaer <msofaer@pivotallabs.com>
2010-11-04 19:49:01 -0700

    Harden Ruby again.  This needs a serious refactor.

--- chef/cookbooks/centos/recipes/main.rb
+++ chef/cookbooks/centos/recipes/main.rb
@@ -1,6 +1,18 @@
-execute "say hello" do
-  command "echo welcome to diaspora chef"
+def harden_ruby(ruby_string)
+  Dir.glob("/usr/local/rvm/wrappers/#{ruby_string}/*").each do |file|
+    link "/usr/local/bin/#{file.split('/').last}" do
+      to file
+    end
+  end
+  Dir.glob("/usr/local/rvm/gems/#{ruby_string}/bin/*").each do |file|
+    link "/usr/local/bin/#{file.split('/').last}" do
+      to file
+    end
+  end
+
 end
+
+harden_ruby("ruby-1.8.7-p302")
 
 include_recipe "centos::image_magick"
 include_recipe "centos::mongo_db"
