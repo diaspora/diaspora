21cdbf08b5ed715e855ed6894f2eb9c07c71ea8e
maxwell <maxwell@joindiaspora.com>
2010-11-08 01:26:11 -0800

    we need to ask to publish on peoples wall...this was working because we all installed it with minifb which asked for everything

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, SERVICES['twitter']['consumer_key'], SERVICES['twitter']['consumer_secret']
-  provider :facebook, SERVICES['facebook']['app_id'], SERVICES['facebook']['app_secret']
+  provider :facebook, SERVICES['facebook']['app_id'], SERVICES['facebook']['app_secret'], :scope => "publish_stream,email,offline_access"
 end  
 
