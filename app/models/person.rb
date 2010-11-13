60b272b243b4d3d0a8a76259e53c08f2de0d6c9c
danielvincent <danielgrippi@gmail.com>
2010-11-13 14:55:40 -0800

    remove validations on url for now to support ip addresses in pod_url field in app_config.yml

--- app/models/person.rb
+++ app/models/person.rb
@@ -37,8 +37,8 @@ class Person
   before_destroy :remove_all_traces
   before_validation :clean_url
   validates_presence_of :url, :profile, :serialized_public_key
-  validates_format_of :url, :with =>
-    /^(https?):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*(\.[a-z]{2,5})?(:[0-9]{1,5})?(\/.*)?$/ix
+  #validates_format_of :url, :with =>
+  #  /^(https?):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*(\.[a-z]{2,5})?(:[0-9]{1,5})?(\/.*)?$/ix
 
   ensure_index :diaspora_handle
 
