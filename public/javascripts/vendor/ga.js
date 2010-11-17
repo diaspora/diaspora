ba620e5400e486c1321739951706e8614520b7cd
maxwell <maxwell@joindiaspora.com>
2010-11-17 12:25:09 -0800

    spell vendor correctly

--- public/javascripts/vendor/ga.js
+++ public/javascripts/vendor/ga.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-17207587-5']);
   _gaq.push(['_trackPageview']);
@@ -9,6 +7,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>
-
