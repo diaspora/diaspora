ea25b28c5cfc9f5693f2679cd3d237e3aa61e934
Raphael <raphael@joindiaspora.com>
2010-11-17 21:18:26 -0800

    refactor splunk recipe

--- chef/cookbooks/common/recipes/splunk.rb
+++ chef/cookbooks/common/recipes/splunk.rb
@@ -1,39 +1,35 @@
-execute "Download splunk" do
-  command "cd /tmp/install && wget 'http://www.splunk.com/index.php/download_track?file=4.1.5/linux/splunk-4.1.5-85165-Linux-x86_64.tgz&ac=&wget=true&name=wget&typed=releases'"
-end
+unless system "splunk status"
+  execute "Download splunk" do
+    command "cd /tmp/install && wget 'http://www.splunk.com/index.php/download_track?file=4.1.5/linux/splunk-4.1.5-85165-Linux-x86_64.tgz&ac=&wget=true&name=wget&typed=releases'"
+  end
 
-execute "Untar splunk" do
-  command "cd /tmp/install && tar -xvf splunk-4.1.5-85165-Linux-x86_64.tgz"
-end
+  execute "Untar splunk" do
+    command "tar -xvf /tmp/install/splunk-4.1.5-85165-Linux-x86_64.tgz -C /opt/"
+  end
 
-execute "Stop old splunk" do
-  command "/opt/splunk/bin/splunk stop || true"
-end
-
-execute "Remove old splunk" do
-  command "rm -rf /opt/splunk"
-end
-
-execute "Install splunk" do
-  command "mv /tmp/install/splunk /opt/splunk"
+  link "/usr/local/bin/splunk" do
+    to "/opt/splunk/bin/splunk"
+  end
 end
 
 execute "Start splunk" do
-  command "cd /opt/splunk/bin && ./splunk start --accept-license"
+  command "splunk start --accept-license || true"
 end
 
 execute "Put splunk into forwarding mode" do
-  command "cd /opt/splunk/bin && ./splunk enable app SplunkLightForwarder -auth admin:changeme"
+  command "splunk enable app SplunkLightForwarder -auth admin:changeme"
 end
 
 execute "Add forwarding server" do
-  command "cd /opt/splunk/bin && ./splunk add forward-server splunk.joindiaspora.com:9997 -auth admin:changeme"
+  command "splunk add forward-server splunk.joindiaspora.com:9997 -auth admin:changeme"
+  not_if "splunk list forward-server | grep splunk.joindiaspora.com:9997"
 end
 
 execute "Add monitor for diaspora" do
-  command "cd /opt/splunk/bin && ./splunk add monitor /usr/local/app/diaspora/log"
+  command "splunk add monitor /usr/local/app/diaspora/log"
+  not_if "splunk list monitor | grep diaspora"
 end
 
 execute 'Splunk Restart' do
-  command "cd /opt/splunk/bin && ./splunk restart"
+  command "splunk restart"
 end
