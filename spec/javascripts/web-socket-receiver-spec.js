b991df9d3a12f630eacd45f8093d9655da7b1bf5
Raphael <raphael@joindiaspora.com>
2010-11-21 16:19:25 -0500

    WSR spec working

--- spec/javascripts/web-socket-receiver-spec.js
+++ spec/javascripts/web-socket-receiver-spec.js
@@ -1,6 +1,5 @@
 describe("WebSocketReceiver", function() {
     it("sets a shortcut", function() {
-      expect(WebSocketReceiver).toEqual(WSR);
       expect(WSR).toEqual(WebSocketReceiver);
     });
 });
