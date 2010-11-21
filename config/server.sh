a3395b6de8c50a500fa1e33aed0c6b8f4052482f
Alec Leamas <leamas.alec@gmail.com>
2010-11-21 12:08:31 +0100

    Bugfix  (thanks, MrXYZ)

--- config/server.sh
+++ config/server.sh
@@ -4,9 +4,10 @@
 THIN_PORT=3000
 SOCKET_PORT=8080
 
-# See thin -h for possible values.
-DEFAULT_THIN_ARGS="-p $THIN_PORT"
+# Choose one mode by uncommenting
+export RAILS_ENV='development'
+#export RAILS_ENV='production'
+#export RAILS_ENV='test'
 
-# Uncomment to run in production/test mode.
-# DEFAULT_THIN_ARGs="$DEFAULT_THIN_ARGS -e production"
-# DEFAULT_THIN_ARGs="$DEFAULT_THIN_ARGS -e test"
+# See thin -h for possible values.
+DEFAULT_THIN_ARGS="-p $THIN_PORT -e $RAILS_ENV"
