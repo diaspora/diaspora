6e5b097f815f0ca443fff692f094d8cdd21df2b6
MrZYX <pr0fkill@gmail.com>
2010-11-26 18:33:52 +0100

    try at fixing a translation

--- app/helpers/stream_helper.rb
+++ app/helpers/stream_helper.rb
@@ -6,9 +6,9 @@ module StreamHelper
 
   def comment_toggle(count)
     if count > 0
-      link_to "#{t('.hide_comments')} (#{count})", '#', :class => "show_post_comments"
+      link_to "#{t('stream_helper.hide_comments')} (#{count})", '#', :class => "show_post_comments"
     else
-      link_to "#{t('.show_comments')} (#{count})", '#', :class => "show_post_comments"
+      link_to "#{t('stream_helper.show_comments')} (#{count})", '#', :class => "show_post_comments"
     end
   end
 
