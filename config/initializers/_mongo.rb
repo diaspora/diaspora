5a90cfd680bda002d4a07a5a723d371b6aaeafde
Hexagon <robinnilsson@gmail.com>
2010-11-27 12:53:37 +0100

    Fixed #580 , exception when using phusion passenger, thanks yarishima42

--- config/initializers/_mongo.rb
+++ config/initializers/_mongo.rb
@@ -9,7 +9,7 @@ MongoMapper.connect ::Rails.env
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect_to_master if forked
+     MongoMapper.connection.connect if forked
    end
 end
 
