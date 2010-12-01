5d31a24e9edf460c7b89c5306a2fcc24a3df2e49
root <root@lagshot.com>
2010-12-02 02:54:38 +0300

    fixed spec, jquery event binding doesnt call the function itself, but a 'clone' of the function

--- spec/javascripts/validation-spec.js
+++ spec/javascripts/validation-spec.js
@@ -11,23 +11,12 @@ describe("Validation", function() {
   describe("events", function() { 
     it("contains all the events that use validation methods");
     describe("usernameKeypress", function() { 
-      beforeEach(function() { 
-        $("#jasmine_content").html(
-         ' <input id="user_username" name="user[username]" size="30" type="text">'
-        );
-      });
 
       it("doesn't allow the user to type anything but letters, numbers and underscores", function() { 
         expect(Validation.rules.username.characters.test("*")).toBeFalsy();
         expect(Validation.rules.username.characters.test("Aa_")).toBeTruthy();
         expect(Validation.rules.username.characters.test("ffffffffffffffffffffffffffffffffff")).toBeFalsy();
-      });
-      
-      it("is called when the user presses a key on #user_username") {
-        spyOn(Validation.events, "usernameKeypress");
-        $("#user_username").keypress();
-        expect(Validation.events.usernameKeypress).toHaveBeenCalled();
-      });
+      }); 
     });
   });
 });
