f81dc0fed6a42ce3a943848b382a7ec1b31d7523
Dan Hansen <mokker1234@gmail.com>
2010-12-01 21:25:58 -0800

    remove empty specs

--- spec/javascripts/validation-spec.js
+++ spec/javascripts/validation-spec.js
@@ -1,17 +1,15 @@
 describe("Validation", function() { 
-  describe("rules", function() { 
-    it("contains all the rules for validation");
-
+  describe("rules", function() {
     describe("username", function() {
       describe("characters", function() {
-        it("is the regex for checking if we allow what the user typed");
+        it("is the regex for checking if we allow what the user typed", function() { 
+          expect(typeof Validation.rules.username.characters).toEqual("function");
+        });
       });
     });
   });
   describe("events", function() { 
-    it("contains all the events that use validation methods");
     describe("usernameKeypress", function() { 
-
       it("doesn't allow the user to type anything but letters, numbers and underscores", function() { 
         expect(Validation.rules.username.characters.test("*")).toBeFalsy();
         expect(Validation.rules.username.characters.test("Aa_")).toBeTruthy();
