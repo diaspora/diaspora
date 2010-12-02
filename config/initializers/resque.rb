6cfa6577a5bd478ef59c79ea3873e8e29ed878d3
Raphael <raphael@joindiaspora.com>
2010-12-02 11:13:11 -0800

    Add first resque job

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,3 @@
-Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 #config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))
 #Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
 require 'resque'
