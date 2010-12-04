88aa67dba7986fc7318ba17cd766bbed9f5a5544
maxwell <maxwell@joindiaspora.com>
2010-12-04 13:54:39 -0800

    actually put the post in the xml param where we expecting it.  also, throw the errors so we know in whats going on in resque

--- app/models/jobs/http_post.rb
+++ app/models/jobs/http_post.rb
@@ -3,9 +3,10 @@ module Jobs
     @queue = :http
     def self.perform(url, body, tries_remaining)
       begin
-        RestClient.post(url, body)
-      rescue
+        RestClient.post(url, :xml => body)
+      rescue Exception => e
         Resque.enqueue(self, url, body, tries_remaining -1) unless tries_remaining <= 1
+        raise e
       end
     end
   end
