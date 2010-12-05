d7407b7aa60b915c65a4c04e9625ecc206bae025
danielvincent <danielgrippi@gmail.com>
2010-12-05 02:03:03 -0800

    request -> resource

--- spec/models/jobs/http_post_spec.rb
+++ spec/models/jobs/http_post_spec.rb
@@ -6,11 +6,11 @@ describe Jobs::HttpPost do
     @body = '<xml>California</xml>'
   end
   it 'POSTs to a given URL' do
-    RestClient::Request.should_receive(:new).with(@url, :xml=>@body, :timeout => 4)
+    RestClient::Resource.should_receive(:new).with(@url, :xml=>@body, :timeout => 4)
     Jobs::HttpPost.perform(@url, @body, 3)
   end
   it 'retries' do
-    RestClient::Request.should_receive(:new).with(@url, :xml=>@body, :timeout => 4)
+    RestClient::Resource.should_receive(:new).with(@url, :xml=>@body, :timeout => 4)
     Resque.should_receive(:enqueue).with(Jobs::HttpPost, @url, @body, 1).once
     Jobs::HttpPost.perform(@url, @body, 2)
   end
