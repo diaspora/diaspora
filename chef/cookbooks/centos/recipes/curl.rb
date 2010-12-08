936b997dff7cdd47898c03ef26af1bd6c5ad289e
maxwell <maxwell@joindiaspora.com>
2010-12-07 19:09:33 -0800

    fixed curl cookbook

--- chef/cookbooks/centos/recipes/curl.rb
+++ chef/cookbooks/centos/recipes/curl.rb
@@ -1,9 +1,18 @@
 execute "download curl into usr/local" do
-  command "mkdir -p /usr/local && wget http://curl.haxx.se/download/curl-7.19.7.tar.gz"
+  command "mkdir -p /usr/local && cd /usr/local/ && wget http://curl.haxx.se/download/curl-7.19.7.tar.gz"
 end
 
 execute "configure, make, and install curl" do
-  command "cd /usr/local/curl-7.19.7 && tar -xvzf curl-7.19.7.tar.gz && ./configure && make && make install"
+  command "tar -xvzf curl-7.19.7.tar.gz && cd /usr/local/curl-7.19.7 && ./configure && make && make install"
+end
+
+execute 'update dynamic loader cache for curl' do
+  command "echo '/usr/local/lib' >> /etc/ld.so.conf"
+  not_if "grep /usr/local/lib /etc/ld.so.conf"
+end
+
+execute 'run dynamic linker' do
+  command '/sbin/ldconfig'
 end
 
 execute 'add bundler line' do
