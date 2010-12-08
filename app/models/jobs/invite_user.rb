497dcccb8f486d84dc4476d6adf2881a36ac322f
danielvincent <danielgrippi@gmail.com>
2010-12-07 23:40:07 -0800

    merge email and mail queues in resque

--- app/models/jobs/invite_user.rb
+++ app/models/jobs/invite_user.rb
@@ -1,6 +1,6 @@
 module Jobs
   class InviteUser
-    @queue = :email
+    @queue = :mail
     def self.perform(sender_id, email, aspect_id, invite_message)
       user = User.find(sender_id)
       user.invite_user(email, aspect_id, invite_message)
