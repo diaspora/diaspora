fd9d1332fcd402e73b1d158c7d9d301bf338b2e1
danielvincent <danielgrippi@gmail.com>
2010-12-14 11:43:26 -0800

    break service http into its own queue

--- app/models/jobs/post_to_services.rb
+++ app/models/jobs/post_to_services.rb
@@ -1,6 +1,6 @@
 module Jobs
   class PostToServices 
-    @queue = :http
+    @queue = :http_service
     def self.perform(user_id, post_id, url)
       user = User.find_by_id(user_id)
       post = Post.find_by_id(post_id)
