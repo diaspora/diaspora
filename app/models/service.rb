ac9703d0572c2ad05ef5a2249288fe80fdb63387
Bèr Kessels <ber@webschuur.com>
2010-12-16 00:19:30 +0100

    Rough identi.ca integration done.

--- app/models/service.rb
+++ app/models/service.rb
@@ -25,3 +25,4 @@ class Service
 end
 require File.join(Rails.root, 'app/models/services/facebook')
 require File.join(Rails.root, 'app/models/services/twitter')
+require File.join(Rails.root, 'app/models/services/identica')
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, SERVICES['twitter']['consumer_key'], SERVICES['twitter']['consumer_secret']
   provider :facebook, SERVICES['facebook']['app_id'], SERVICES['facebook']['app_secret'], :scope => "publish_stream,email,offline_access"
+  provider :identica, SERVICES['identica']['consumer_key'], SERVICES['identica']['consumer_secret']
 end  
 
