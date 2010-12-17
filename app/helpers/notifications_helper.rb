b8a0abd12230c5e56734f53d316a174769470fc7
maxwell <maxwell@joindiaspora.com>
2010-12-16 18:06:59 -0800

    updating notification views

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,21 +1,14 @@
 module NotificationsHelper
-  def glue_text(kind)
-    translation = "notifications.#{kind.underscore}_glue"
-    t(translation)
-  end 
-
   def object_link(note)
-    kind = note.kind.underscore
-    translation = t("notifications.#{kind.underscore}_link")
+    kind = note.kind
+    translation = t("notifications.#{kind}")
     case kind
-    when 'request'
-     link_to translation, aspects_manage_path
-    when 'status_message'
-     link_to translation, status_message_path(note.object_id)
-    when 'comment'
+    when 'request_accepted'
+      translation
+    when 'new_request'
+      translation
+    when 'comment_on_post'
       link_to translation, object_path(Comment.first(:id => object_id).post)
-    when 'photo'
-      link_to translation, photo_path(note.object_id)
     else
     end
   end
