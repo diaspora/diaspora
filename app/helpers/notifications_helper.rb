3c3e03993acdc057db2a85728013763288937246
maxwell <maxwell@joindiaspora.com>
2010-12-17 11:48:08 -0800

    make the two links on comment notifications clearer

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -8,7 +8,7 @@ module NotificationsHelper
     when 'new_request'
       translation
     when 'comment_on_post'
-      link_to translation, object_path(Comment.first(:id => note.object_id).post)
+       "#{translation} #{link_to t('notifications.post'), object_path(Comment.first(:id => note.object_id).post)}".html_safe
     else
     end
   end
