bc9352eabdb00a9d05f71b4be00726dab29dbda2
danielvincent <danielgrippi@gmail.com>
2010-12-16 17:55:00 -0800

    fix id/string error in receive local job

--- app/models/jobs/receive_local.rb
+++ app/models/jobs/receive_local.rb
@@ -5,7 +5,8 @@ module Jobs
     def self.perform(user_id, person_id, object_type, object_id)
       user = User.find(user_id)
       person = Person.find(person_id)
-      object = eval("#{object_type}.first(:id => \"#{object_id}\")")
+      object = object_type.constantize.first(:id => object_id)
+
       user.receive_object(object, person)
     end
   end
