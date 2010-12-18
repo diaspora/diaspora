bcca61af6b34aba5419bdf6a763013a549d15768
maxwell <maxwell@joindiaspora.com>
2010-12-18 01:33:15 -0800

    infinate scroll

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,25 @@
-$(function () {  
-  $('#main_stream .pagination a').live('click', function () {  
-      $.getScript(this.href);  
-      return false;  
-    }
-  );
+//$(function () {  
+  //$('#main_stream .pagination a').live('click', function () {  
+      //$.getScript(this.href);  
+      //return false;  
+    //}
+  //);
+//});
+
+// infinitescroll() is called on the element that surrounds 
+// the items you will be loading more of
+ $(document).ready(function() { 
+$('#main_stream').infinitescroll({
+   navSelector  : "div.pagination",            
+                   // selector for the paged navigation (it will be hidden)
+    nextSelector : ".pagination a.next_page",    
+                   // selector for the NEXT link (to page 2)
+    itemSelector : "#main_stream",
+                   // selector for all items you'll retrieve
+    bufferPx: 300,
+    donetext: "no more.",
+    loadingText: "", 
+    loadingImg: 'images/ajax-loader.gif'
+  });
 });
+
