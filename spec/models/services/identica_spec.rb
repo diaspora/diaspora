afbba82676401a9f1c0458eddc89ff26151637e8
Bèr Kessels <ber@webschuur.com>
2010-12-27 20:57:32 +0100

    Added rspec for identi.ca.

---
+++ spec/models/services/identica_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Services::Twitter do
+
+  before do
+    @user = make_user
+    @user.aspects.create(:name => "whatever")
+    @post = @user.post(:status_message, :message => "hello", :to =>@user.aspects.first.id)
+    @service = Services::Twitter.new(:access_token => "yeah", :access_secret => "foobar", :endpoint => 'http://identi.ca/api')
+    @user.services << @service
+  end
+
+  describe '#post' do
+    it 'posts a status message to identica' do
+      Twitter.should_receive(:update).with(@post.message)
+      @service.post(@post)
+    end
+
+    it 'swallows exception raised by identica always being down' do
+      Twitter.should_receive(:update).and_raise
+      @service.post(@post)
+    end
+
+    it 'should call public message' do
+      Twitter.stub!(:update)
+      url = "foo"
+      @service.should_receive(:public_message).with(@post, url)
+      @service.post(@post, url)
+    end
+  end
+end
+
