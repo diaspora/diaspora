215d6a85044886937bc586e70b9a36282d78f8f1
Alec Leamas <leamas.alec@gmail.com>
2010-12-28 01:26:16 +0100

    Build test diaspora_handle properly.

--- spec/factories.rb
+++ spec/factories.rb
@@ -35,7 +35,7 @@ Factory.define :user do |u|
   user.person = Factory.build(:person, :profile => Factory.create(:profile),
                               :owner_id => user._id,
                               :serialized_public_key => user.encryption_key.public_key.export,
-                              :diaspora_handle => "#{user.username}@#{AppConfig[:pod_url].gsub(/(https?:|www\.)\/\//, '').chop!}")
+                              :diaspora_handle => "#{user.username}@#{AppConfig[:pod_uri].host}")
   end
 end
 
