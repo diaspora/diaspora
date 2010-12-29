3faff76b0c425fad5ac1d16d4337cc3136ac2f61
Nick van der Burgt <nickvanderburgt@gmail.com>
2010-12-29 16:11:34 +0100

    Reverted the changes in ie.js

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-var elements = new Array('header','footer','article','nav','section');
-
-for(var element in elements) { document.createElement(elements[element]); }
-=======
 document.createElement('header');
 document.createElement('footer');
->>>>>>> parent of 2e9aab1... Added some more HTML5 elements so no suprises in the future
