3b13a8e77b8ae830e08c0176b3d02528244af036
Scott Bahling <sbahling@mudgum.net>
2011-01-03 15:55:11 +0100

    receive: Fixed resque:worker crash if user is nil.
    In this case adding a new user resulted in a crash cause User.find
    returns nil and so we are not able to call the receive function.

--- app/models/jobs/receive.rb
+++ app/models/jobs/receive.rb
@@ -5,7 +5,7 @@ module Jobs
     def self.perform(user_id, xml, salmon_author_id)
       user = User.find(user_id)
       salmon_author = Person.find(salmon_author_id)
-      user.receive(xml, salmon_author)
+      user.receive(xml, salmon_author) if user and salmon_author
     end
   end
 end
