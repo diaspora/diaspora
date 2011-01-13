b4fae58f126720ae029d564d227b37d74ae06c8d
maxwell <maxwell@joindiaspora.com>
2011-01-13 14:48:30 -0800

    just say no to 404 facebox errors

--- public/javascripts/vendor/facebox.js
+++ public/javascripts/vendor/facebox.js
@@ -85,15 +85,15 @@
     settings: {
       opacity      : 0.2,
       overlay      : true,
-      loadingImage : '/facebox/loading.gif',
-      closeImage   : '/facebox/closelabel.png',
+      loadingImage : '/images/facebox/loading.gif',
+      closeImage   : '/images/facebox/closelabel.png',
       imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
       <div class="popup"> \
         <div class="content"> \
         </div> \
-        <a href="#" class="close"><img src="/facebox/closelabel.png" title="close" class="close_image" /></a> \
+        <a href="#" class="close"><img src="/images/facebox/closelabel.png" title="close" class="close_image" /></a> \
       </div> \
     </div>'
     },
