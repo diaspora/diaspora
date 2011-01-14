b42bffa6faab451fd8e61a7fbae292474ebdd7ea
Raphael <raphael@joindiaspora.com>
2011-01-13 16:51:09 -0800

    Serialize youtube titles to yaml to ActiveRecord can read them

--- lib/data_conversion/export_from_mongo.rb
+++ lib/data_conversion/export_from_mongo.rb
@@ -85,7 +85,13 @@ module DataConversion
 
     def comments_json_to_csv model_hash
       generic_json_to_csv(model_hash) do |hash|
-        model_hash[:mongo_attrs].map { |attr_name| hash[attr_name] }
+        model_hash[:mongo_attrs].map { |attr_name|
+          if attr_name == "youtube_titles"
+            hash[attr_name].to_yaml
+          else
+            hash[attr_name]
+          end
+        }
       end
     end
 
@@ -155,7 +161,13 @@ module DataConversion
 
     def posts_json_to_csv model_hash
       generic_json_to_csv(model_hash) do |hash|
-        model_hash[:mongo_attrs].map { |attr_name| hash[attr_name] }
+        model_hash[:mongo_attrs].map { |attr_name|
+          if attr_name == "youtube_titles"
+            hash[attr_name].to_yaml
+          else
+            hash[attr_name]
+          end
+        }
       end
       #has to handle the polymorphic stuff
     end
