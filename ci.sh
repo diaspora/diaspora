8b7bfd540d6c6263ceaec0e75186e4bd60b81db3
Sarah Mei <sarahmei@gmail.com>
2011-01-15 10:04:39 -0800

    Expose gem upgrade failures on CI. Build will break. Next checkin will fix it. :)

--- ci.sh
+++ ci.sh
@@ -4,6 +4,7 @@ echo "**************************************************************************
 echo "*                                  ruby 1.8.7 REE build                                         *" &&
 echo "*************************************************************************************************" &&
 echo "" &&
+rm -f Gemfile.lock &&
 source /usr/local/rvm/scripts/rvm &&
 rvm use ree@diaspora --create &&
 bundle install &&
