db417abc186c308cdb465434cd5ad9d0483985c7
Raphael <raphael@joindiaspora.com>
2011-01-17 14:30:57 -0800

    Fix person show

--- lib/diaspora/user/querying.rb
+++ lib/diaspora/user/querying.rb
@@ -72,7 +72,7 @@ module Diaspora
       def posts_from(person)
         asp = Aspect.arel_table
         p = Post.arel_table
-        Post.includes(:aspects).where( p[:public].eq(true).or(asp[:user_id].eq(self.id))).order("posts.updated_at DESC")
+        person.posts.includes(:aspects).where( p[:public].eq(true).or(asp[:user_id].eq(self.id))).select('DISTINCT `posts`.*').order("posts.updated_at DESC")
       end
     end
   end
