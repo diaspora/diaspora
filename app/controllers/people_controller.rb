28b7302c9ea6761a8f1986ad8d08539ea4d57eaf
Raphael <raphael@joindiaspora.com>
2011-01-17 19:42:49 -0800

    Specify type again

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -63,7 +63,7 @@ class PeopleController < ApplicationController
         @commenting_disabled = false
       end
 
-      @posts = current_user.posts_from(@person).paginate :page => params[:page]
+      @posts = current_user.posts_from(@person).where(:type => "StatusMessage").paginate :page => params[:page]
       @post_hashes = hashes_for_posts @posts
 
       respond_with @person, :locals => {:post_type => :all}
