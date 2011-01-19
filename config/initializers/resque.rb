fc6d6416434e64479e55ec274e31b41a380367f1
zhitomirskiyi <ilya@joindiaspora.com>
2011-01-19 14:30:34 -0800

    correct requires

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
+require File.join(Rails.root, 'app', 'models', 'jobs', 'base')
 Dir[File.join(Rails.root, 'app', 'models', 'jobs', '*.rb')].each { |file| require file }
 #config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))
 #Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
