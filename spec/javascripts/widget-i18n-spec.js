9046273ebd8bf5505329723f4a9a791e7aeff98a
Dan Hansen <mokker1234@gmail.com>
2011-01-27 21:00:14 -0600

    some specs for the i18n widget

---
+++ spec/javascripts/widget-i18n-spec.js
@@ -0,0 +1,45 @@
+/**
+ * Created by .
+ * User: dan
+ * Date: Jan 27, 2011
+ * Time: 3:20:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+describe("Diaspora", function() {
+  beforeEach(function() {
+    Diaspora.widgets.i18n.loadLocale(null/* TODO: spec.retrieveFixture("locale")*/, "en");
+  });
+  describe("widgets", function() {
+    describe("i18n", function() {
+      describe("loadLocale", function() {
+        it("sets the class's locale variable", function() {
+          Diaspora.widgets.i18n.loadLocale({sup: "hi"});
+          expect(Diaspora.widgets.i18n.locale).toEqual({sup: "hi"});
+        });
+      });
+      describe("t", function() {
+        it("returns the specified translation", function() {
+          Diaspora.widgets.i18n.loadLocale({yo: "sup"}, "en");
+          var translation = Diaspora.widgets.i18n.t("yo");
+          expect(translation).toEqual("sup");
+        });
+        it("will go through a infinitely deep object", function() {
+          Diaspora.widgets.i18n.loadLocale({
+            yo: {
+              hi: {
+                sup: {
+                  test: "test"
+                }
+              }
+            },
+            more: {
+              another: "i hope this spec is green"
+            }
+          });
+         expect(Diaspora.widgets.i18n.t("yo.hi.sup.test")).toEqual("test");
+         expect(Diaspora.widgets.i18n.t("more.another")).toEqual("i hope this spec is green");
+        });
+      });
+    });
+  });
+});
