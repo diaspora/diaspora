e63b605b2afe40856a8a59eff32fbebcfd3aa929
Andreas Schneider <asn@cryptomilk.org>
2011-01-29 17:22:31 +0100

    db: Create limited varchar to be able to add index.

--- db/migrate/20110126232040_add_unique_index_on_invitation_service_and_invitation_identifier_to_users.rb
+++ db/migrate/20110126232040_add_unique_index_on_invitation_service_and_invitation_identifier_to_users.rb
@@ -1,5 +1,7 @@
 class AddUniqueIndexOnInvitationServiceAndInvitationIdentifierToUsers < ActiveRecord::Migration
   def self.up
+    change_column(:users, :invitation_service, :string, :limit => 127)
+    change_column(:users, :invitation_identifier, :string, :limit => 127)
     add_index(:users, [:invitation_service, :invitation_identifier], :unique => true)
   end
 
