6dffdd815258fa32d8a7f6794cdccab763a3fa6d
maxwell <maxwell@joindiaspora.com>
2011-01-29 18:58:34 -0800

    don't remove these methods

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -7,6 +7,15 @@ var Mobile = {
   initialize: function() {
     $("abbr.timeago").timeago();
     $('#main_stream + .pagination').hide();
+    $('#aspect_picker').change(Mobile.changeAspect);
   },
+  
+  changeAspect: function() {
+    Mobile.windowLocation('/aspects/' + $('#aspect_picker option:selected').val());
+  },
+  
+  windowLocation: function(url) {
+    window.location = url;
+  }
 };
 
