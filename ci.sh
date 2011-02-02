9a618d4dc0ef4a8f0dd8309c9782b96ca3d26183
Sarah Mei <sarahmei@gmail.com>
2011-02-02 09:52:20 -0800

    Try removing all cached gems for REE before bundle installing on CI

--- ci.sh
+++ ci.sh
@@ -8,5 +8,7 @@ rm -f Gemfile.lock &&
 source /usr/local/rvm/scripts/rvm &&
 rvm use ree@diaspora --create &&
 rm -rf /usr/local/rvm/gems/ree-1.8.7-2010.02@diaspora/cache &&
+rm -rf /usr/local/rvm/gems/ree-1.8.7-2010.02@global/cache &&
+rm -rf /usr/local/rvm/gems/ree-1.8.7-2010.02/cache &&
 bundle install &&
 bundle exec rake cruise
