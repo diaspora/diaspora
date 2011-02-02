34a0abe0c894f2bed6aa598d5a3912c40485003d
maxwell <maxwell@joindiaspora.com>
2011-02-02 13:14:34 -0800

    fix 500 on mobile notifications page

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,19 @@ ActiveRecord::Schema.define(:version => 20110202015222) do
 
   add_index "data_points", ["statistic_id"], :name => "index_data_points_on_statistic_id"
 
+  create_table "histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
+
   create_table "invitations", :force => true do |t|
     t.text     "message"
     t.integer  "sender_id",    :null => false
@@ -459,6 +472,7 @@ ActiveRecord::Schema.define(:version => 20110202015222) do
     t.string   "mongo_id"
     t.string   "invitation_service"
     t.string   "invitation_identifier"
+    t.text     "open_aspects"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
