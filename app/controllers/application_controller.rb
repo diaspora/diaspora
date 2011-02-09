e75c285049c1a370b3bec38dbb82dd3b9d7b1fcc
zhitomirskiyi <ilya@joindiaspora.com>
2011-02-09 15:07:25 -0800

    added created_at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < ActionController::Base
     str = "controller=#{self.class} action=#{self.action_name} "
     if current_user
       str << "uid=#{current_user.id}"
+      str << "created_at=#{current_user.created_at.to_date.to_s}" if current_user.created_at
     else
       str << 'uid=nil'
     end
