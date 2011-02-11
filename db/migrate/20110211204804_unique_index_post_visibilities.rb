88da878c97fd3537af14d31376d40acd9272b672
Raphael Sofaer <raphael@joindiaspora.com>
2011-02-11 12:52:19 -0800

    Move migration to avoid conflict

--- db/migrate/20110211204804_unique_index_post_visibilities.rb
+++ db/migrate/20110211204804_unique_index_post_visibilities.rb
@@ -1,4 +1,4 @@
-class UniquePostVisibilities < ActiveRecord::Migration
+class UniqueIndexPostVisibilities < ActiveRecord::Migration
   def self.up
     remove_index :post_visibilities, [:aspect_id, :post_id]
     add_index :post_visibilities, [:aspect_id, :post_id], :unique => true
