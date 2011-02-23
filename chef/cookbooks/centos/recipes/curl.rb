d75bc10ac0939434205643572a7c0dbbcd887d29
Raphael Sofaer <raphael@joindiaspora.com>
2011-02-23 12:00:32 -0800

    Working on curl chef script

--- chef/cookbooks/centos/recipes/curl.rb
+++ chef/cookbooks/centos/recipes/curl.rb
@@ -1,9 +1,16 @@
-execute "download curl into usr/local" do
-  command "mkdir -p /usr/local && cd /usr/local/ && wget http://curl.haxx.se/download/curl-7.19.7.tar.gz"
+curl = 'curl-7.21.4'
+
+execute 'download curl' do
+  command "mkdir -p /tmp/install && wget http://curl.download.nextag.com/download/#{curl}.tar.gz"
+  not_if "ls /tmp/install/#{curl}.tar.gz"
 end
 
-execute "configure, make, and install curl" do
-  command "tar -xvzf curl-7.19.7.tar.gz && cd /usr/local/curl-7.19.7 && ./configure && make && make install"
+execute "unzip curl" do
+  command "cd /tmp/install && tar -xvf #{curl}.tar.gz"
+  command "cd /tmp/install/#{curl} && ./configure --with-ssl"
+  command "cd /tmp/install/#{curl} && make"
+  command "cd /tmp/install/#{curl} && make install"
+  not_if "ls /usr/local/lib/libcurl.so.4"
 end
 
 execute 'update dynamic loader cache for curl' do
@@ -15,10 +22,10 @@ execute 'run dynamic linker' do
   command '/sbin/ldconfig'
 end
 
-execute 'add bundler line' do
-  command 'cd /usr/local/app/diaspora/ && bundle config build.typhoeus --with-curl=/usr/local/curl-7.19.7/'
-end
-
-execute 'rebundle' do
-  command 'bundle install'
-end
+#execute 'add bundler line' do
+#  command "cd /usr/local/app/diaspora/ && bundle config build.typhoeus --with-curl=/usr/local/#{curl}/"
+#end
+#
+#execute 'rebundle' do
+#  command 'bundle install'
+#end
