31f5500b11e8d8eff7255c7e5cb4205f3eb4eb63
MrZYX <pr0fkill@gmail.com>
2011-03-01 17:28:35 +0100

    removed RAILS_ENV=test example from config/server.sh

--- config/server.sh
+++ config/server.sh
@@ -7,7 +7,6 @@ SOCKET_PORT=8080
 # Choose one mode by uncommenting
 export RAILS_ENV='development'
 #export RAILS_ENV='production'
-#export RAILS_ENV='test'
 
 # See thin -h for possible values.
 DEFAULT_THIN_ARGS="-p $THIN_PORT -e $RAILS_ENV"
