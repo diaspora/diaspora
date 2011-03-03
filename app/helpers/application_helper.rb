2522ab7ee4b3a58290db108731eb3f8156ace67a
zhitomirskiyi <ilya@joindiaspora.com>
2011-03-03 10:39:32 -0800

    refactoring the conversation views, wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -133,8 +133,8 @@ module ApplicationHelper
     "<img alt=\"#{h(person.name)}\" class=\"avatar\" #{("data-owner_id="+@user_id.to_s) if @user_id} data-person_id=\"#{person.id}\" src=\"#{person.profile.image_url(size)}\" title=\"#{h(person.name)}\">".html_safe
   end
 
-  def person_link(person)
-    "<a href='/people/#{person.id}'>
+  def person_link(person, opts={})
+    "<a href='/people/#{person.id}' class='#{opts[:class]}'>
   #{h(person.name)}
 </a>".html_safe
   end
