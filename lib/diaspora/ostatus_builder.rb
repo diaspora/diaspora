337831bba27c0034b035946e6bdbd2b0f288e6cc
maxwell <maxwell@joindiaspora.com>
2011-03-09 17:58:57 -0800

    move ostatus builder, actually added the file.

--- lib/diaspora/ostatus_builder.rb
+++ lib/diaspora/ostatus_builder.rb
@@ -79,5 +79,4 @@ module Diaspora
       XML
     end
   end
-
 end
---
+++ spec/lib/diaspora/ostatus_builder_spec.rb
@@ -0,0 +1,32 @@
+#   Copyright (c) 2010, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+
+require 'spec_helper'
+require File.join(Rails.root,  'lib/diaspora/ostatus_builder')
+
+
+describe Diaspora::OstatusBuilder do
+
+  let!(:user) { alice }
+  let(:aspect) { user.aspects.first }
+  let!(:public_status_messages) {
+    3.times.inject([]) do |arr,n|
+      s = user.post(:status_message, :message => "hey#{n}", :public => true, :to => aspect.id)
+      arr << s
+    end
+  }
+  let!(:private_status_messages) {
+    3.times.inject([]) do |arr,n|
+      s = user.post(:status_message, :message => "secret_ney#{n}", :public => false, :to => aspect.id)
+      arr << s
+    end
+  }
+  let!(:atom) { director = Diaspora::Director.new; director.build(Diaspora::OstatusBuilder.new(user, public_status_messages)) }
+
+  it 'should include a users posts' do
+    public_status_messages.each{ |status| atom.should include status.message }
+  end
+
+end
+
