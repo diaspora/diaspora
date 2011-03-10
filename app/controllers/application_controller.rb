eafa965504396d8bc31a4827d173ca6041fd1532
Raphael Sofaer <raphael@joindiaspora.com>
2011-03-09 16:17:48 -0800

    Try explicitly querying rather than getting postvisibilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       @aspect = nil
       @object_aspect_ids = []
-      @all_aspects = current_user.aspects.includes(:aspect_memberships, :post_visibilities)
+      @all_aspects = current_user.aspects.includes(:aspect_memberships)
       @notification_count = Notification.for(current_user, :unread =>true).count
       @unread_message_count = ConversationVisibility.sum(:unread, :conditions => "person_id = #{current_user.person.id}")
       @user_id = current_user.id
