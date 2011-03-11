e4be5db881569a3d14c1bde80275fc1a713e8970
MrZYX <pr0fkill@gmail.com>
2011-03-11 15:36:42 +0100

    there can be no reason to not set the locale

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@
 class PostsController < ApplicationController
   skip_before_filter :count_requests
   skip_before_filter :set_invites
-  skip_before_filter :set_locale
   skip_before_filter :which_action_and_user
   skip_before_filter :set_grammatical_gender
 
