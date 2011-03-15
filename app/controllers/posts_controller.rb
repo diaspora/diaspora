9115a8f1f58f580b90ca1584266170d259238ab6
danielgrippi <daniel@joindiaspora.com>
2011-03-15 16:22:17 -0700

    Only show searchable people in postscontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
     @posts = @posts.tagged_with(params[:tag])
     @posts = @posts.includes(:comments, :photos).paginate(:page => params[:page], :per_page => 15, :order => 'created_at DESC')
 
-    profiles = Profile.tagged_with(params[:tag]).select('profiles.id')
+    profiles = Profile.tagged_with(params[:tag]).where(:searchable => true).select('profiles.id')
     @people = Person.where(:id => profiles.map{|p| p.id}).limit(15)
 
     @fakes = PostsFake.new(@posts)
