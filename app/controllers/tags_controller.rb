3eeecebb67840bf81832ec6b95ee5eff75f0c82a
MrZYX <pr0fkill@gmail.com>
2011-04-02 14:18:46 +0200

    201 means created…

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -27,7 +27,7 @@ class TagsController < ApplicationController
 
       respond_to do |format|
         format.json{
-          render(:json => @array.to_json, :status => 201)
+          render(:json => @array.to_json, :status => 200)
         }
       end
     else
