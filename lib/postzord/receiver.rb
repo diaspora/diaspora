e7eb9adbf591baffbea17a1bd089546e0aade0a6
MrZYX <pr0fkill@gmail.com>
2011-04-09 18:40:40 +0200

    do not webfinger if we have no clue about the handle

--- lib/postzord/receiver.rb
+++ lib/postzord/receiver.rb
@@ -52,7 +52,7 @@ module Postzord
       if @object.respond_to?(:relayable?)
         #if A and B are friends, and A sends B a comment from C, we delegate the validation to the owner of the post being commented on
         xml_author = @user.owns?(@object.parent) ? @object.diaspora_handle : @object.parent.author.diaspora_handle
-        @author = Webfinger.new(@object.diaspora_handle).fetch
+        @author = Webfinger.new(@object.diaspora_handle).fetch if @object.author
       else
         xml_author = @object.diaspora_handle
       end
