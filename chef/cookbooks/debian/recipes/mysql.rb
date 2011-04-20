8fa19f7a34a6a262c19eb1ca6e370cec62ed8b89
DenSchub <software@dsx.cc>
2011-04-20 23:16:53 +0200

    We don't need that on debian.

--- chef/cookbooks/debian/recipes/mysql.rb
+++ chef/cookbooks/debian/recipes/mysql.rb
@@ -1,11 +1,3 @@
 execute "install mysql" do
   command "apt-get install -y mysql-server libmysqlclient-dev libmysql-ruby"
 end
-
-execute "start mysql service to create the system tables" do
-  command "service mysqld start"
-end
-
-execute "stop service again" do
-  command "service mysqld stop"
-end
