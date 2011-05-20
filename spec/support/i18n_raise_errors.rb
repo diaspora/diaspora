c95ce3bd0c3737166e0c1c8f76a500aaa8d05798
Raphael Sofaer <raphael@joindiaspora.com>
2011-05-19 17:00:06 -0700

    Raise translation errors in specs

--- spec/support/i18n_raise_errors.rb
+++ spec/support/i18n_raise_errors.rb
@@ -5,3 +5,18 @@ module I18n
 end
 
 I18n.exception_handler = :just_raise_that_exception
+module ActionView
+  module Helpers
+    module TranslationHelper
+      def translate(key, options = {})
+        translation = I18n.translate(scope_key_by_partial(key), options.merge!(:raise => true))
+        if html_safe_translation_key?(key) && translation.respond_to?(:html_safe)
+          translation.html_safe
+        else
+          translation
+        end
+      end
+      alias :t :translate
+    end
+  end
+end
