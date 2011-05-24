db22be43a817f4156b06a2bb1f94627949abf692
MrZYX <pr0fkill@gmail.com>
2011-05-24 16:56:33 +0200

    really no string concatenation and proper pluralization in notifications helper AKA THE REASON I DIDN'T WANTED TO TOUCH THIS STUFF

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,37 +1,38 @@
 module NotificationsHelper
-  def object_link(note)
+  def object_link(note, actors)
     target_type = note.popup_translation_key
+    actors_count = note.actors.count
     if note.instance_of?(Notifications::Mentioned)
       post = Mention.find(note.target_id).post
       if post
-        translation(target_type, :post_link => link_to(t('notifications.post'), object_path(post)).html_safe)
+        translation(target_type, :actors => actors, :count => actors_count, :post_link => link_to(t('notifications.post'), object_path(post)).html_safe)
       else
-        t('notifications.mentioned_deleted')
+        t('notifications.mentioned_deleted', :actors => actors, :count => actors_count).html_safe
       end
     elsif note.instance_of?(Notifications::CommentOnPost)
       post = Post.where(:id => note.target_id).first
       if post
-        translation(target_type, :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
+        translation(target_type, :actors => actors, :count => actors_count, :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
       else
-        t('notifications.also_commented_deleted')
+        t('notifications.also_commented_deleted', :actors => actors, :count => actors_count).html_safe
       end
     elsif note.instance_of?(Notifications::AlsoCommented)
       post = Post.where(:id => note.target_id).first
       if post
-        translation(target_type, :post_author => h(post.author.name), :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
+        translation(target_type, :actors => actors, :count => actors_count, :post_author => h(post.author.name), :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
       else
-        t('notifications.also_commented_deleted')
+        t('notifications.also_commented_deleted', :actors => actors, :count => actors_count).html_safe
       end
     elsif note.instance_of?(Notifications::Liked)
       post = note.target
       post = post.post if post.is_a? Like
       if post
-        translation(target_type, :post_author => h(post.author.name), :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
+        translation(target_type, :actors => actors, :count => actors_count, :post_author => h(post.author.name), :post_link => link_to(t('notifications.post'), object_path(post), 'data-ref' => post.id, :class => 'hard_object_link').html_safe)
       else
-        t('notifications.liked_post_deleted')
+        t('notifications.liked_post_deleted', :actors => actors, :count => actors_count).html_safe
       end
     else #Notifications:StartedSharing, etc.
-      translation(target_type)
+      translation(target_type, :actors => actors, :count => actors_count)
     end
   end
 
@@ -62,10 +63,14 @@ module NotificationsHelper
         others_sentence = " #{t('notifications.index.and')} " + others_sentence
       end
       message = "#{first}, #{second}, #{third},"
-      message += "<a class='more' href='#'> #{t('notifications.index.and_others', :number =>(number_of_actors - 3))}</a>"
+      message += "<a class='more' href='#'> #{t('notifications.index.and_others', :count =>(number_of_actors - 3))}</a>"
       message += "<span class='hidden'> #{others_sentence} </span>"
     end
     message.html_safe
+  end
+
+  def notification_message_for(note)
+    object_link(note, notification_people_link(note))
   end
 
   def peoples_names(note)
--- spec/helpers/notifications_helper_spec.rb
+++ spec/helpers/notifications_helper_spec.rb
@@ -67,25 +67,26 @@ describe NotificationsHelper do
   describe '#object_link' do
     describe 'for a like' do
       it 'should include a link to the post' do
-        output = object_link(@notification)
+        output = object_link(@notification, notification_people_link(@notification))
         output.should include status_message_path(@post)
       end
 
       it 'includes the boilerplate translation' do
-        output = object_link(@notification)
-        output.should include t("#{@notification.popup_translation_key}", 
+        output = object_link(@notification,  notification_people_link(@notification))
+        output.should include t("#{@notification.popup_translation_key}.one",
+                                :actors => notification_people_link(@notification),
                                 :post_link => "<a href=\"#{status_message_path(@post)}\" class=\"hard_object_link\" data-ref=\"#{@post.id}\">#{t('notifications.post')}</a>")
       end
 
       context 'when post is deleted' do
         it 'works' do
           @post.destroy
-          expect{ object_link(@notification)}.should_not raise_error
+          expect{ object_link(@notification, notification_people_link(@notification))}.should_not raise_error
         end
 
         it 'displays that the post was deleted' do
           @post.destroy
-          object_link(@notification).should == t('notifications.liked_post_deleted')
+          object_link(@notification,  notification_people_link(@notification)).should == t('notifications.liked_post_deleted.one', :actors => notification_people_link(@notification))
         end
       end
     end
