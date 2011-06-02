668c4ddfc5400d154d7ae1af2c88a11fc540bd97
MrZYX <pr0fkill@gmail.com>
2011-06-02 12:32:21 +0200

    d'oh
    - just check for existance before accessing in services initializer
    - before_validation has no option :post
    - dry up routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
          :timeoutable, :token_authenticatable
 
   before_validation :strip_and_downcase_username
-  before_validation :set_current_language, :post => :create
+  before_validation :set_current_language, :on => :create
 
   validates_presence_of :username
   validates_uniqueness_of :username
