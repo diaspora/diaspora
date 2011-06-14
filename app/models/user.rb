b4464043480615a4358ce5e5441fcf6681cc0384
danielgrippi <danielgrippi@gmail.com>
2011-06-13 22:56:36 -0700

    Revert "Without loaded? I was confused by the liked? and like_for methods."
    This reverts commit 2103d54fe43a7c2c111b940c3b06a40d578e3cb1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -171,21 +171,20 @@ class User < ActiveRecord::Base
   end
 
   def liked?(post)
-    if post.likes.loaded?
-      !(like_for(post).nil?)
+    if self.like_for(post)
+      return true
     else
-      Like.exists?(:author_id => self.person.id, :post_id => post.id)
+      return false
     end
   end
 
   def like_for(post)
-    if post.likes.loaded?
-      [post.likes, post.dislikes].each do |likes|
-        likes.detect { |l| l.author_id == self.person.id }
+    [post.likes, post.dislikes].each do |likes|
+      likes.each do |like|
+        return like if like.author_id == self.person.id
       end
-    else
-      return Like.where(:post_id => post.id, :author_id => self.person.id).first
     end
+    return nil
   end
 
   ######### Mailer #######################
