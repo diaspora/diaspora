c12103aed99c9871eb7059802efd0dd9ff0293fa
Raphael Sofaer <raphael@joindiaspora.com>
2011-06-16 06:36:43 +0200

    Deal with ids from PG coming out as strings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -83,3 +83,7 @@ end
 def photo_fixture_name
     @photo_fixture_name = File.join(File.dirname(__FILE__), 'fixtures', 'button.png')
 end
+
+def postgres?
+  defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) && ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+end
