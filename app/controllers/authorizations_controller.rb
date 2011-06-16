75a1012c1cd2577e9dcfc9ca0a1ce6aa67729ead
Ilya Zhitomirskiy <ilya@laptop.(none)>
2011-06-15 18:30:05 -0700

    moved the monkey patching to a model, need to do a couple of validations

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -81,16 +81,3 @@ class AuthorizationsController < ApplicationController
     !OAuth2::Provider.client_class.exists?(:nonce => nonce)
   end
 end
-
-OAuth2::Provider.client_class.instance_eval do
-  def self.create_or_reset_from_manifest! manifest
-    if obj = find_by_name(manifest['name'])
-      obj.oauth_identifier = OAuth2::Provider::Random.base62(16)
-      obj.oauth_secret = OAuth2::Provider::Random.base62(32)
-      obj.save!
-      obj
-    else
-      create!(manifest)
-    end
-  end
-end
---
+++ spec/models/oauth2_provider_models_activerecord_client_spec.rb
@@ -0,0 +1,13 @@
+#   Copyright (c) 2010, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+#
+require 'spec_helper'
+
+describe OAuth2::Provider::Models::ActiveRecord::Client do
+  #TODO
+  describe 'validations'do
+    it 'is pending for now'
+  end
+end
+
