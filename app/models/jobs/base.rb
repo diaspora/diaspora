18138e150765fd9bd015ec2938a98c5795fc6181
Raphael Sofaer <raphael@joindiaspora.com>
2011-06-23 11:22:09 -0700

    Doc

--- app/models/jobs/base.rb
+++ app/models/jobs/base.rb
@@ -2,12 +2,15 @@ module Job
   class Base
     extend ResqueJobLogging
 
-    def self.perform(*args)  
-      ActiveRecord::Base.verify_active_connections!  
-      self.perform_delegate(*args)  
+    # Perform this job.  This wrapper method
+    def self.perform(*args)
+      ActiveRecord::Base.verify_active_connections!
+      self.perform_delegate(*args)
     end
 
-    def self.perform_delegate(*args) # override this  
+    # Override this in your Job class.
+    # @abstract
+    def self.perform_delegate(*args)
     end
   end
 end
