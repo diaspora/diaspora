f2e31025626cfdd6e2d72a22ddb883ba052bbf3d
Raphael Sofaer <raphael@joindiaspora.com>
2011-06-29 11:21:44 -0700

    Make the aspect_dropdown_list_item easier to read

--- app/helpers/aspect_global_helper.rb
+++ app/helpers/aspect_global_helper.rb
@@ -87,15 +87,16 @@ module AspectGlobalHelper
     klass = checked ? "selected" : ""
     hidden = !checked ? "hidden" : ""
 
-    str = "<li data-aspect_id=#{aspect.id} class='#{klass}'>"
-    #str << "<input #{checked} id=\"in_aspect\" name=\"in_aspect\" type=\"checkbox\" value=\"in_aspect\" />"
-    str << "<img src='/images/icons/check_yes_ok.png' width=18 height=18 class='check #{hidden}'/>"
-    str << "<img src='/images/icons/check_yes_ok_white.png' width=18 height=18 class='checkWhite'/>"
-    str << aspect.name
-    str << "<div class=\"hidden\">"
-    str << aspect_membership_button(aspect, contact, person)
-    str << "</div>"
-    str << "</li>"
+    str = <<LISTITEM
+<li data-aspect_id=#{aspect.id} class='#{klass}'>
+  <img src='/images/icons/check_yes_ok.png' width=18 height=18 class='check #{hidden}'/>
+  <img src='/images/icons/check_yes_ok_white.png' width=18 height=18 class='checkWhite'/>
+  #{aspect.name}
+  <div class=\"hidden\">
+    #{aspect_membership_button(aspect, contact, person)}
+  </div>
+</li>
+LISTITEM
     str.html_safe
   end
 end
