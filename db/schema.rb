e98cc4b9605987862ff5ae90df7bdfd62a577baa
Raphael Sofaer <raphael@joindiaspora.com>
2011-07-07 15:14:29 -0700

    Sort on and index taggings, ftw

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110705003445) do
+ActiveRecord::Schema.define(:version => 20110707221112) do
 
   create_table "aspect_memberships", :force => true do |t|
     t.integer  "aspect_id",  :null => false
@@ -343,6 +343,7 @@ ActiveRecord::Schema.define(:version => 20110705003445) do
     t.datetime "created_at"
   end
 
+  add_index "taggings", ["created_at"], :name => "index_taggings_on_created_at"
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
   add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
   add_index "taggings", ["taggable_id", "taggable_type", "tag_id"], :name => "index_taggings_uniquely", :unique => true
