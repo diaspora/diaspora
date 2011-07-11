e84635dae21ea68e7f31e519b7743315bfeb19b8
danielgrippi <danielgrippi@gmail.com>
2011-07-10 17:03:38 -0700

    constrain in the image uploader, don't crop

--- app/uploaders/processed_image.rb
+++ app/uploaders/processed_image.rb
@@ -21,10 +21,10 @@ class ProcessedImage < CarrierWave::Uploader::Base
     process :resize_to_fill => [50,50]
   end
   version :thumb_medium do
-    process :resize_to_fill => [100,100]
+    process :resize_to_limit => [100,100]
   end
   version :thumb_large do
-    process :resize_to_fill => [300,300]
+    process :resize_to_limit => [300,300]
   end
 
   version :scaled_full do
