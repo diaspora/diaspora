b01ffb4489882ee9b7c5650e7b23ca8a58f2a889
Raphael Sofaer <raphael@joindiaspora.com>
2011-07-19 10:55:23 -0700

    We don't use perform_delegate anymore

--- app/models/jobs/mail_confirm_email.rb
+++ app/models/jobs/mail_confirm_email.rb
@@ -1,7 +1,7 @@
 module Job
   class MailConfirmEmail < Base
     @queue = :mail
-    def self.perform_delegate(user_id)
+    def self.perform(user_id)
       Notifier.confirm_email(user_id).deliver
     end
   end
