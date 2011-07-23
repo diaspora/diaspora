00885be738a2e737e7ed18d016eb7a537213d49e
Raphael Sofaer <raphael@joindiaspora.com>
2011-07-22 17:43:05 -0700

    Provide feedback when a user tries to double-reshare

--- app/controllers/reshares_controller.rb
+++ app/controllers/reshares_controller.rb
@@ -4,7 +4,7 @@ class ResharesController < ApplicationController
 
   def create
     @reshare = current_user.build_post(:reshare, :root_guid => params[:root_guid])
-    if @reshare.save!
+    if @reshare.save
       current_user.add_to_streams(@reshare, current_user.aspects)
       current_user.dispatch_post(@reshare, :url => post_url(@reshare), :additional_subscribers => @reshare.root.author)
     end
--- app/helpers/reshares_helper.rb
+++ app/helpers/reshares_helper.rb
@@ -1,2 +1,9 @@
 module ResharesHelper
+  def reshare_error_message(reshare)
+    if @reshare.errors[:root_guid].present?
+      escape_javascript(@reshare.errors[:root_guid].first)
+    else
+      escape_javascript(t('reshares.create.failure'))
+    end
+  end
 end
