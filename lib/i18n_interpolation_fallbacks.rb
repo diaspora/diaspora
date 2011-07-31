b7b1c6b2769df2d1674fdceb21b02ea5c28244a3
Sarah Mei <sarahmei@gmail.com>
2011-07-30 22:41:46 -0700

    Fix interaction between I18n fallbacks and our interpolation fallback code. Refactor I18n tests into their own file. Remove some test support code that made it hard to test I18n.

--- lib/i18n_interpolation_fallbacks.rb
+++ lib/i18n_interpolation_fallbacks.rb
@@ -1,13 +1,10 @@
 module I18n
   module Backend
     module InterpolationFallbacks
-
       def translate(locale, key, options = {})
-        return super if options[:fallback]
         default = extract_string_or_lambda_default!(options) if options[:default]
         options.merge!(:default => default) if default
 
-        options[:fallback] = true
         I18n.fallbacks[locale].each do |fallback|
           begin
             result = super(fallback, key, options)
@@ -15,7 +12,6 @@ module I18n
           rescue I18n::MissingInterpolationArgument
           end
         end
-        options.delete(:fallback)
 
         return super(locale, nil, options) if default
         raise(I18n::MissingInterpolationArgument.new(options, "key: #{key} in locale: #{locale}"))
