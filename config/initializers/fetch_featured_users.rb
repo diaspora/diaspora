23c2eaf1c52b6e37b84028e9d6f05979b166a9de
danielgrippi <danielgrippi@gmail.com>
2011-08-03 15:52:02 -0700

    added a real profile description on the welcome tab

--- config/initializers/fetch_featured_users.rb
+++ config/initializers/fetch_featured_users.rb
@@ -1,9 +1,10 @@
 #this breaks seed scripts
-#
-#unless Rails.env == 'test' || AppConfig[:featured_users].count == Person.featured_users.count
-#  puts "Fetching featured users from remote servers..."
-#  AppConfig[:featured_users].each do |x|
-#    Webfinger.new(x).fetch
-#  end
-#  puts "Done fetching!"
-#end
+
+unless Rails.env == 'test' || AppConfig[:featured_users].count == Person.featured_users.count
+  print "Fetching featured users from remote servers"
+  AppConfig[:featured_users].each do |x|
+    Webfinger.new(x).fetch
+    print "."
+  end
+  puts " done!"
+end
