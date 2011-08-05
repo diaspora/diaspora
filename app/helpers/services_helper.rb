6d3da0d0d12ace2dcb8ec40fc5ad7ae7f972d6b8
Raphael Sofaer <raphael@joindiaspora.com>
2011-08-05 11:24:54 -0700

    Memoize the facebook invitation form on the facebook finder page

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,5 +1,16 @@
 module ServicesHelper
+  GSUB_THIS = "FIUSDHVIUSHDVIUBAIUHAPOIUXJM"
   def contact_proxy(friend)
     friend.contact || Contact.new(:person => friend.person)
+  end
+
+  # This method memoizes the facebook invite form in order to avoid the overhead of rendering it on every post.
+  # @param [ServiceUser] friend
+  # @return [String] The HTML for the form.
+  def facebook_invite_form friend
+    @form ||= controller.render_to_string(
+      :partial => 'services/facebook_invite',
+      :locals => {:uid => GSUB_THIS})
+    @form.gsub(GSUB_THIS, friend.uid).html_safe
   end
 end
