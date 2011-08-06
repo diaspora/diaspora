d5bb9952adc094b117847d0ded28723c09b42eac
Vittorio Cuculo <vittorio.cuculo@gmail.com>
2011-08-06 17:30:44 +0200

    commit

--- vendor/ruby/1.8/gems/gherkin-2.4.5/ext/gherkin_lexer_sv/extconf.rb
+++
@@ -1,6 +0,0 @@
-require 'mkmf'
-CONFIG['warnflags'].gsub!(/-Wshorten-64-to-32/, '') if CONFIG['warnflags']
-$CFLAGS << ' -O0 -Wall' if CONFIG['CC'] =~ /gcc/
-dir_config("gherkin_lexer_sv")
-have_library("c", "main")
-create_makefile("gherkin_lexer_sv")
