50f32ce5ac5949bdfdc5026e5abb1507f6ec84f7
Andrej Kacian <andrej@kacian.sk>
2011-08-06 15:36:41 +0200

    Show ajax loader and hide the hide icon when hiding a post.

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -24,6 +24,7 @@ var Stream = {
 
     Diaspora.widgets.subscribe("stream/scrolled", Stream.collapseText);
     Stream.collapseText('eventID', $(Stream.selector)[0]);
+    Stream.bindHideIcon();
   },
   collapseText: function(){
     elements = $(Array.prototype.slice.call(arguments,1));
@@ -188,6 +189,16 @@ var Stream = {
         textarea.focus();
       }
     }
+  },
+
+  bindHideIcon: function(){
+    $("a.stream_element_delete.vis_hide").live("click", function(evt){
+      $(this).toggleClass("hidden");
+      $(this).next("img.hide_loader").toggleClass("hidden");
+    });
+    $("a.stream_element_hide_undo").live("click", function(evt){
+      $("img.hide_loader").toggleClass("hidden");
+    });
   }
 };
 
