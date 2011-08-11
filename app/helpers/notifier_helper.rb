08f12ee34863aa25382ec93113ecf2b926e37189
danielgrippi <danielgrippi@gmail.com>
2011-08-11 14:40:29 -0700

    fixed the 'a post' bug in notification emails

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -1,4 +1,8 @@
 module NotifierHelper
+  
+  # @param post [Post] The post object.
+  # @param opts [Hash] Optional hash.  Accepts :length and :process_newlines parameters.
+  # @return [String] The truncated and formatted post.
   def post_message(post, opts={})
     opts[:length] ||= 200
     if post.respond_to? :formatted_message
@@ -8,5 +12,15 @@ module NotifierHelper
     else
       I18n.translate 'notifier.a_post_you_shared'
     end
+  end
+
+  # @param comment [Comment] The comment to process.
+  # @param opts [Hash] Optional hash.  Accepts :length and :process_newlines parameters.
+  # @return [String] The truncated and formatted comment.
+  def comment_message(comment, opts={})
+    opts[:length] ||= 600
+    text = truncate(@comment.text, :length => opts[:length])
+    text = process_newlines(text) if opts[:process_newlines]
+    text
   end
 end
