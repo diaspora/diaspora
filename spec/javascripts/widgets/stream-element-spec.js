36e32ba497ab022feb64b9cece5c5cf683fd277d
Andrej Kacian <andrej@kacian.sk>
2011-08-30 01:46:39 +0200

    Update jasmine tests for the stream element widget.

---
+++ spec/javascripts/widgets/stream-element-spec.js
@@ -0,0 +1,27 @@
+/*   Copyright (c) 2010, Diaspora Inc.  This file is
+ *   licensed under the Affero General Public License version 3 or later.  See
+ *   the COPYRIGHT file.
+ */
+
+describe("Diaspora.Widgets.StreamElement", function() {
+  var streamElement;
+
+  beforeEach(function() {
+    jasmine.Clock.useMock();
+    spec.loadFixture("aspects_index_with_posts");
+    streamElement = Diaspora.BaseWidget.instantiate("StreamElement", $(".stream_element:has(a.stream_element_delete.vis_hide)"));
+  });
+
+  describe("hidePost", function() {
+    it("makes sure that ajax spinner appears when hiding a post", function() {
+      expect(streamElement.deletePostLink).not.toHaveClass("hidden");
+      expect(streamElement.hidePostLoader).toHaveClass("hidden");
+      spyOn($, "ajax");
+      streamElement.deletePostLink.click();
+      jasmine.Clock.tick(200);
+      expect($.ajax).toHaveBeenCalled();
+      expect(streamElement.deletePostLink).toHaveClass("hidden");
+      expect(streamElement.hidePostLoader).not.toHaveClass("hidden");
+    });
+  });
+});
