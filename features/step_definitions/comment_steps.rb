624e7277e8edc6f64d19ead39b00d79d81fb6db9
Dan Hansen <mokker1234@gmail.com>
2011-08-30 22:08:54 -0500

    the "When I open the comment box" step used an old javascript function of ours that no longer exists. remove the feature and replace it with "When I focus the comment field"

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -2,10 +2,6 @@ When /^I focus the comment field$/ do
   find("a.focus_comment_textarea").click
 end
 
-When /^I open the comment box$/ do
-  page.evaluate_script('Stream.focusNewComment($(".stream_element"), {preventDefault: function(){}})')
-end
-
 Then /^the first comment field should be open/ do
   find("#main_stream .stream_element .new_comment").should be_visible
 end
