e6d16172684ae3c41b8c7b83c6248fc16545ed20
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-09-01 10:52:07 -0700

    added timeago to notifications page.  @ohaibbq, this is getting annoying :P

--- public/javascripts/pages/notifications-index.js
+++ public/javascripts/pages/notifications-index.js
@@ -3,5 +3,6 @@ Diaspora.Pages.NotificationsIndex = function() {
 
   this.subscribe("page/ready", function(evt, document) {
     self.infiniteScroll = self.instantiate("InfiniteScroll");
+    self.instantiate("TimeAgo", document.find("abbr.timeago"));
   });
 };
