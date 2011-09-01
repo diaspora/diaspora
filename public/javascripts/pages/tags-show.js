f5fbaaadcf5d3c42167143b7598ff0c536b3706f
maxwell <maxwell@maxwell-betty.(none)>
2011-08-31 21:54:14 -0700

    added timeago and stream to the tag page

--- public/javascripts/pages/tags-show.js
+++ public/javascripts/pages/tags-show.js
@@ -1,7 +1,9 @@
 Diaspora.Pages.TagsShow = function() {
   var self = this;
 
-  this.subscribe("page/ready", function() {
+  this.subscribe("page/ready", function(evt, document) {
+    self.stream = self.instantiate("Stream", document.find("#stream_container"));
     self.infiniteScroll = self.instantiate("InfiniteScroll");
+    self.instantiate("TimeAgo", document.find("abbr.timeago"));
   });
 };
