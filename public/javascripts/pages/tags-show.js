fd689154611f72f9e268133e0eb7f04b9f161aa9
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-09-03 14:41:45 -0700

    MS DH fix tag show page comments

--- public/javascripts/pages/tags-show.js
+++ public/javascripts/pages/tags-show.js
@@ -2,7 +2,7 @@ Diaspora.Pages.TagsShow = function() {
   var self = this;
 
   this.subscribe("page/ready", function(evt, document) {
-    self.stream = self.instantiate("Stream", document.find("#stream_container"));
+    self.stream = self.instantiate("Stream", document.find("#main_stream"));
     self.infiniteScroll = self.instantiate("InfiniteScroll");
     self.instantiate("TimeAgo", document.find("abbr.timeago"));
   });
