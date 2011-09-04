77149a9623f9f6e02554eb2711bb25aa8dfde90d
Sarah Mei <sarahmei@gmail.com>
2011-09-03 17:57:18 -0700

    Move travis startup script to its own file. Add GC settings for REE and -q2 for aptitude install.

---
+++ config/ci/before-travis.sh
@@ -0,0 +1,23 @@
+# Remove Gemfile.lock and rebundle on Ruby 1.9
+ruby -e "system('rm Gemfile.lock') if RUBY_VERSION.include?('1.9')"
+ruby -e "system('bundle install') if RUBY_VERSION.include?('1.9')"
+
+# adjust GC settings for REE
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
+
+# Install firefox and start xvfb in preparation for cucumber & jasmine
+sudo aptitude -q2 -y install firefox
+sudo cp config/ci/xvfb /etc/init.d/xvfb
+sh -e /etc/init.d/xvfb start
+
+# Regenerate css files
+sass --update public/stylesheets/sass/:public/stylesheets/
+
+# Set up database
+cp config/database.yml.example config/database.yml
+rake db:create
+rake db:schema:load
