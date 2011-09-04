1cc7026b59168d5845e95515fa2aabb6e4627964
Sarah Mei <sarahmei@gmail.com>
2011-09-03 21:44:13 -0700

    First try at postgresql support on travis.

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -2,6 +2,14 @@
 ruby -e "system('rm Gemfile.lock') if RUBY_VERSION.include?('1.9')"
 ruby -e "system('bundle install --without development production') if RUBY_VERSION.include?('1.9')"
 
+# Change Gemfile if this is a postgresql build
+echo $DB
+ruby -e "system('cp config/ci/Gemfile.postgresql Gemfile') if ENV['DB'] == 'postgres'"
+
+# Create a database.yml for the right database
+cp config/database.yml.example config/database.yml
+ruby -e "system('cp config/ci/database.yml.postgresql config/database.yml') if ENV['DB'] == 'postgres'"
+
 # adjust GC settings for REE
 export RUBY_HEAP_MIN_SLOTS=1000000
 export RUBY_HEAP_SLOTS_INCREMENT=1000000
@@ -18,6 +26,5 @@ sh -e /etc/init.d/xvfb start
 sass --update public/stylesheets/sass/:public/stylesheets/
 
 # Set up database
-cp config/database.yml.example config/database.yml
 bundle exec rake db:create
 bundle exec rake db:schema:load
