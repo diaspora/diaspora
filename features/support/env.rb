f23658f9a2110fec584a21c31d0dc977977d099e
Sarah Mei <sarahmei@gmail.com>
2011-09-03 19:57:03 -0700

    Up the default wait time so travis is happier.

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,6 +22,9 @@ require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links wi
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
+# We have a higher default wait time to account for build machines of various beefiness.
+Capybara.default_wait_time = 10
+
 # If you set this to false, any error raised from within your app will bubble
 # up to your step definition and out to cucumber unless you catch it somewhere
 # on the way. You can make Rails rescue errors and render error pages on a
