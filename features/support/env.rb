22b65bd3c178b08e18d671915bd7212ccb65d259
Sarah Mei <sarahmei@gmail.com>
2011-09-05 14:31:00 -0700

    Mock out more jobs that might make outside calls

--- features/support/env.rb
+++ features/support/env.rb
@@ -44,7 +44,7 @@ Cucumber::Rails::World.use_transactional_fixtures = false
 
 require File.join(File.dirname(__FILE__), "database_cleaner_patches")
 require File.join(File.dirname(__FILE__), "integration_sessions_controller")
-require File.join(File.dirname(__FILE__), "mock_api_calls")
+require File.join(File.dirname(__FILE__), "poor_mans_webmock")
 
 require File.join(File.dirname(__FILE__), "..", "..", "spec", "support", "fake_redis")
 require File.join(File.dirname(__FILE__), "..", "..", "spec", "helper_methods")
