41effa7bd2be8b4355e51a620d05b80897bce6d0
Jonne Hass <mrzyx@mrzyx.de>
2011-09-11 23:45:14 +0200

    load oauth_keys.yml.example as default so I don't have to touch my oauth_keys.yml to have the specs pass on my machine

--- config/initializers/_before_load_services.rb
+++ config/initializers/_before_load_services.rb
@@ -11,9 +11,8 @@ oauth_keys_file = "#{Rails.root}/config/oauth_keys.yml"
 
 SERVICES = nil
 silence_warnings do
+  SERVICES = load_config_yaml("#{oauth_keys_file}.example")
   if File.exist? oauth_keys_file
-    SERVICES = load_config_yaml(oauth_keys_file)
-  else
-    SERVICES = load_config_yaml("#{oauth_keys_file}.example")
+    SERVICES.deep_merge(load_config_yaml(oauth_keys_file))
   end
 end
