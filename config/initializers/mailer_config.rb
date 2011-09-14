70a9f3fd941b1b23c59899cb9f9726c5bd312425
Jonne Hass <mrzyx@mrzyx.de>
2011-09-14 20:55:29 +0200

    made openssl verify mode configurable for SMTP

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -17,7 +17,8 @@ Diaspora::Application.configure do
           :address => AppConfig[:smtp_address],
           :port => AppConfig[:smtp_port],
           :domain => AppConfig[:smtp_domain],
-          :enable_starttls_auto => false
+          :enable_starttls_auto => false,
+          :openssl_verify_mode => AppConfig[:smtp_openssl_verify_mode]
         }
       else
         config.action_mailer.smtp_settings = {
@@ -27,7 +28,8 @@ Diaspora::Application.configure do
           :authentication => AppConfig[:smtp_authentication],
           :user_name => AppConfig[:smtp_username],
           :password => AppConfig[:smtp_password],
-          :enable_starttls_auto => AppConfig[:smtp_starttls_auto]
+          :enable_starttls_auto => AppConfig[:smtp_starttls_auto],
+          :openssl_verify_mode => AppConfig[:smtp_openssl_verify_mode]
         }
       end
     end
