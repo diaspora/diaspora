17b5e7b066361b92f410f598e2e7ad9ac166c62b
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-09-16 13:33:05 -0700

    move admin rack to its own file

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -30,24 +30,6 @@ end
 
 if AppConfig[:mount_resque_web]
   require 'resque/server'
-
-  class AdminRack
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      user = env['warden'].authenticate(:scope => :user)
-      if user && user.admin?
-        @app.call(env)
-      else
-        [307, {"Location" => '/'}, self]
-      end
-    end
-
-    def each(&block)
-    end
-  end
-
+  require File.join(Rails.root, 'lib/admin_rack')
   Resque::Server.use AdminRack
 end
