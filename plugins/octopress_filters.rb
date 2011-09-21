3dbc379caa737157e1e76d65c0cea2305eda4a5d
Frederic Hemberger <mail@frederic-hemberger.de>
2011-09-21 11:41:55 +0200

    Updated regex to select less content on posts

--- plugins/octopress_filters.rb
+++ plugins/octopress_filters.rb
@@ -59,7 +59,7 @@ module OctopressLiquidFilters
   # Extracts raw content DIV from template, used for page description as {{ content }}
   # contains complete sub-template code on main page level
   def raw_content(input)
-    /.<div class="entry-content">(?<content>[\s\S]*?)<\/div>\s*<\/article>/ =~ input
+    /<div class="entry-content">(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
     return (content.nil?) ? input : content
   end
 
