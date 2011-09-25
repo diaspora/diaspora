9ef928588b32b055cf7f846b47ce0da36ef8af3c
Brandon Mathis <brandon@imathis.com>
2011-09-25 09:14:50 -0500

    mobile nav javascript updated to still work with role='main-navigation' since changes affecting custom partials don't get updated with the standard update script

--- .themes/classic/source/javascripts/octopress.js
+++ .themes/classic/source/javascripts/octopress.js
@@ -1,6 +1,7 @@
 function getNav() {
   var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
   mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
+  $('ul[role=main-navigation]').addClass('main-navigation');
   $('ul.main-navigation a').each(function(link) {
     mobileNav.children('select').append('<option value="'+link.href+'">&bull; '+link.text+'</option>');
   });
