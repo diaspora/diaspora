ec1ae8c07e3c72db3d34956dc0efab632930b342
Sarah Mei <sarahmei@gmail.com>
2011-10-03 22:19:25 -0500

    Aaaand now we need to install firefox too

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -5,8 +5,9 @@ export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
 
-# Start xvfb in preparation for cucumber & jasmine
-echo "Installing and starting Xvfb"
+# Install firefox & xvfb, in preparation for cucumber & jasmine
+echo "Installing Firefox and Xvfb"
+sudo aptitude --quiet=2 --without-recommends --assume-yes install firefox
 sudo aptitude --quiet=2 --without-recommends --assume-yes install xvfb
 sudo cp config/ci/xvfb /etc/init.d/xvfb
 sh -e /etc/init.d/xvfb start
