bcb52b35cf7437f86b238260eb805350765b181c
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-10-14 14:43:51 -0700

    make soup available to everyone to see if we can get feedback

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,7 +1,7 @@
 require File.join(Rails.root, 'lib', 'stream', 'soup')
 
 class SoupsController < ApplicationController
-  before_filter :redirect_unless_admin
+  #before_filter :redirect_unless_admin
 
   def index
     default_stream_action(Stream::Soup)
