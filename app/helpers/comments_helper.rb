35a0c563462eb2c18a6a057a2b4bb66ab8b5f865
Pistos <github.pistos@purepistos.net>
2011-10-15 00:18:17 -0400

    This fixes issue #2032.  Anyone can now view all comments on a public post without needing to log in.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -7,6 +7,8 @@ module CommentsHelper
   def comment_toggle(post, commenting_disabled=false)
     if post.comments.size <= 3
       link_to "#{t('stream_helper.hide_comments')}", post_comments_path(post.id), :class => "toggle_post_comments"
+    elsif ! user_signed_in?
+      link_to "#{t('stream_helper.show_more_comments', :number => post.comments.size - 3)}", post_path(post.id, :all_comments => '1'), :class => "toggle_post_comments"
     else
       link_to "#{t('stream_helper.show_more_comments', :number => post.comments.size - 3)}", post_comments_path(post.id), :class => "toggle_post_comments"
     end
