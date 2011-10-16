80821c9cc5e8c8470cecdd7004a1506c912b38ef
Sarah Mei <sarahmei@gmail.com>
2011-10-16 15:25:11 -0700

    Basic tag metadata returned. Now what to do about posts?...

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,3 +1,7 @@
+#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+
 class Api::V0::UsersController < ApplicationController
   def show
     if user = User.find_by_username(params[:username])
--- lib/stream/tag.rb
+++ lib/stream/tag.rb
@@ -1,3 +1,7 @@
+#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+
 class Stream::Tag < Stream::Base
   attr_accessor :tag_name, :people_page
 
