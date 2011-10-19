49b46f09cde22bd3b027c7089fde440104280ec5
Ilya Zhitomirskiy <iz268@nyu.edu>
2011-10-19 12:10:09 -0700

    Saving of first name

--- public/javascripts/pages/users-getting-started.js
+++ public/javascripts/pages/users-getting-started.js
@@ -5,14 +5,25 @@ Diaspora.Pages.UsersGettingStarted = function() {
     self.peopleSearch = self.instantiate("Search", body.find("form.people.search_form"));
     self.tagSearch = self.instantiate("Search", body.find("form.tag_input.search_form"));
     
+    $('#edit_profile').bind('ajax:success', function(evt, data, status, xhr){
+      $('#form_spinner').addClass("hidden");
+      $('.profile .saved').show();
+      $('.profile .saved').fadeOut(2000);
+    });
+
+    // It seems that the default behavior of rails ujs is to clear the remote form
+    $('#edit_profile').bind('ajax:complete', function(evt, xhr, status){
+      var firstNameField = $("#profile_first_name");
+      firstNameField.val(firstNameField.data("cachedValue"));
+    });
+
+
+
     $("#profile_first_name").bind("change", function(){
-      $('#edit_profile').bind('ajax:success', function(evt, data, status, xhr){
-        $('#form_spinner').addClass("hidden");
-        $('.profile .saved').removeClass("hidden");
-        $('.profile .saved').fadeOut(2000);
-      });
+      $(this).data("cachedValue", $(this).val());
       $('#edit_profile').submit();
       $('#form_spinner').removeClass("hidden");
+
     });
   });
 };
