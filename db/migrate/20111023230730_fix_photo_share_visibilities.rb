ea3c7ec439aada8c171af72e34b03ad0e093a346
danielgrippi <danielgrippi@gmail.com>
2011-10-23 16:43:21 -0700

    fixed migration

--- db/migrate/20111023230730_fix_photo_share_visibilities.rb
+++ db/migrate/20111023230730_fix_photo_share_visibilities.rb
@@ -5,14 +5,14 @@ class FixPhotoShareVisibilities < ActiveRecord::Migration
     raise 'migration currently only compatable with mysql' if postgres?
 
     if Photo.first.respond_to?(:tmp_old_id)
-      ['aspect_visibilities', 'share_visibilities'].each do
+      ['aspect_visibilities', 'share_visibilities'].each do |vis_table|
         ActiveRecord::Base.connection.execute <<SQL
         UPDATE #{vis_table}
           SET shareable_type='Post'
 SQL
         ActiveRecord::Base.connection.execute <<SQL
         UPDATE #{vis_table}, photos
-          SET #{vis_table}.shareable_type='Photo', #{vis_table}.shareable_id=photo.id
+          SET #{vis_table}.shareable_type='Photo', #{vis_table}.shareable_id=photos.id
             WHERE #{vis_table}.shareable_id=photos.tmp_old_id
 SQL
       end
