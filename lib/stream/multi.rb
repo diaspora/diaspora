8a8f022f8b589955d29ac109c599331f64d8bad6
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-10-24 14:02:54 -0700

    pass opts to multi link so commented on works

--- lib/stream/multi.rb
+++ lib/stream/multi.rb
@@ -2,7 +2,7 @@ class Stream::Multi < Stream::Base
 
   # @return [String] URL
   def link(opts)
-    Rails.application.routes.url_helpers.multi_path
+    Rails.application.routes.url_helpers.multi_path(opts)
   end
 
   # @return [String]
