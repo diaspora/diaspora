0b687ccfbfed81d8f964fdb74094fc60b9c336aa
Zach Prezkuta <fermion@gmx.com>
2011-10-28 17:55:58 -0600

    allow user to prevent his/her own text from showing up in email notifications

--- app/mailers/notification_mailers/also_commented.rb
+++ app/mailers/notification_mailers/also_commented.rb
@@ -2,14 +2,17 @@ module NotificationMailers
   class AlsoCommented < NotificationMailers::Base
     include ActionView::Helpers::TextHelper
 
-    attr_accessor :comment
+    attr_accessor :comment, :text_owner
 
     def set_headers(comment_id)
       @comment = Comment.find_by_id(comment_id)
+      @text_owner = @comment.author.owner
+      @post_owner = @comment.parent.author.owner
+      @post_author_name = @comment.post.author.name
 
       if mail?
         @headers[:from] = "\"#{@comment.author.name} (Diaspora*)\" <#{AppConfig[:smtp_sender_address]}>"
-        @headers[:subject] = truncate(@comment.parent.comment_email_subject, :length => TRUNCATION_LEN)
+        @headers[:subject] = @post_owner.user_preferences.exists?(:email_type => 'silent') ? "#{I18n.t('notifier.comment_on_post.silenced_subject', :name => "#{@post_author_name}")}" : truncate(@comment.parent.comment_email_subject, :length => TRUNCATION_LEN)
         @headers[:subject] = "Re: #{@headers[:subject]}"
       end
     end
--- app/mailers/notification_mailers/reshared.rb
+++ app/mailers/notification_mailers/reshared.rb
@@ -1,9 +1,10 @@
 module NotificationMailers
   class Reshared < NotificationMailers::Base
-    attr_accessor :reshare
+    attr_accessor :reshare, :text_owner
 
     def set_headers(reshare_id)
       @reshare = Reshare.find(reshare_id)
+      @text_owner = @reshare.root.author.owner
 
       @headers[:subject] = I18n.t('notifier.reshared.reshared', :name => @sender.name)
     end
