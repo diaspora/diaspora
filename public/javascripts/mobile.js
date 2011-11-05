2a4abc54830ab43cea4e694f56d46fcd7edded0a
Dan Hansen & Maxwell Salzberg <pair+ohaibbq+maxwell@joindiaspora.com>
2011-11-05 13:23:29 -0500

    show service icons on mobile site

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -221,6 +221,39 @@ $(document).ready(function(){
     }, 'html');
   });
 
+
+  $(".service_icon").bind("tap click", function(evt) {
+    var service = $(this).toggleClass("dim"),
+      selectedServices = $("#new_status_message .service_icon:not(.dim)"),
+      provider = service.attr("id"),
+      hiddenField = $("#new_status_message input[name='services[]'][value='" + provider + "']"),
+      publisherMaxChars = 40000,
+      serviceMaxChars;
+
+
+    $("#new_status_message .counter").remove();
+
+    $.each(selectedServices, function() {
+      serviceMaxChars = parseInt($(this).attr("maxchar"));
+      if(publisherMaxChars > serviceMaxChars) {
+        publisherMaxChars = serviceMaxChars;
+      }
+    });
+
+    $('#status_message_text').charCount({allowed: publisherMaxChars, warning: publisherMaxChars/10 });
+
+    if(hiddenField.length > 0) { hiddenField.remove(); }
+    else {
+      $("#new_status_message").append(
+        $("<input/>", {
+          name: "services[]",
+          type: "hidden",
+          value: provider
+        })
+      );
+    }
+  });
+  
   $("#submit_new_message").bind("tap click", function(evt){
     evt.preventDefault();
     $("#new_status_message").submit();
