0de65127892759d6e312d544d975d11005685a65
Gonzalo Rodriguez <gonzalo@wyeworks.com>
2011-11-22 00:18:14 -0200

    Decrease cukes default wait time
    This will not fix cukes, but at least travis is going
    to throw failures output instead of timing out

--- features/support/env.rb
+++ features/support/env.rb
@@ -25,7 +25,10 @@ require 'cucumber/api_steps'
 Capybara.default_selector = :css
 
 # We have a ridiculously high wait time to account for build machines of various beefiness.
-Capybara.default_wait_time = 30
+# Capybara.default_wait_time = 30
+
+# While there are a lot of failures, wait less, avoiding travis timeout
+Capybara.default_wait_time = 3
 
 # If you set this to false, any error raised from within your app will bubble
 # up to your step definition and out to cucumber unless you catch it somewhere
