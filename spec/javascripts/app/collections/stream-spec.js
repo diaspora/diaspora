1d178116e9fb95773dfa123b547c671578d244b1
danielgrippi <danielgrippi@gmail.com>
2011-12-10 17:15:25 -0800

    DC DG; jasmine green

--- spec/javascripts/app/collections/stream-spec.js
+++ spec/javascripts/app/collections/stream-spec.js
@@ -1,17 +1,17 @@
-describe("Stream", function() {
+describe("App.Models.Stream", function() {
   describe("url", function() {
-    var stream = new BackboneStream(),
+    var stream = new App.Collections.Stream(),
       expectedPath = document.location.pathname + ".json";
     it("returns the json path", function() {
       expect(stream.url()).toEqual(expectedPath);
-    });    
+    });
 
     it("returns the json path with max_time if the collection has models", function() {
-      var post = new Post();
+      var post = new App.Models.Post();
       spyOn(post, "createdAt").andReturn(1234);
 
       stream.add(post);
-      
+
       expect(stream.url()).toEqual(expectedPath + "?max_time=1234");
     });
   });
