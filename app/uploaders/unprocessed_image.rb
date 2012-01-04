762ecaf37935e4e88b2f78f753556d26cb6bacc8
Steven Fuchs <stwf@yahoo.com>
2012-01-04 16:31:25 -0500

    we only really need a small thumbnail in the unprocessed view

--- app/uploaders/unprocessed_image.rb
+++ app/uploaders/unprocessed_image.rb
@@ -17,8 +17,16 @@ class UnprocessedImage < CarrierWave::Uploader::Base
     model.random_string + File.extname(@filename) if @filename
   end
 
-  version :thumb_small
-  version :thumb_medium
-  version :thumb_large
-  version :scaled_full
+  version :thumb_small do
+    process :resize_to_limit => [100,100]
+    process :strip
+  end
+  def strip
+    manipulate! do |img|
+      img.strip
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
 end
