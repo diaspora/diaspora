c0e4fa831a3f7828ff6f66aecbabe157dbb5709e
Jonne Haß <mrzyx@mrzyx.de>
2012-01-05 20:17:30 +0100

    fix syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Diaspora::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
   config.threadsafe!
-
+end
 
 # Sacrifice readability for a 10% performance boost
 Haml::Template::options[:ugly] = true
