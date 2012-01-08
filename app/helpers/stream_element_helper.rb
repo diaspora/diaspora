fa7f4c667748399931fcc6fafa545db4b77d75dd
Vittorio Cuculo <lasek88@gmail.com>
2012-01-08 15:02:05 +0100

    #2586 Fix hide posts

--- app/helpers/stream_element_helper.rb
+++ app/helpers/stream_element_helper.rb
@@ -10,10 +10,12 @@ module StreamElementHelper
   end
 
   def delete_or_hide_button(post)
-    if user_signed_in? && current_user.owns?(post)
-      link_to image_tag('deletelabel.png'), post_path(post), :confirm => t('are_you_sure'), :method => :delete, :remote => true, :class => "delete control_icon remove_post", :title => t('delete')
-    else
-      link_to image_tag('deletelabel.png'), share_visibility_path(:id => "42", :post_id => post.id), :method => :put, :remote => true, :class => "delete remove_post control_icon vis_hide", :title => t('.hide_and_mute')
+    if user_signed_in?
+      if current_user.owns?(post)
+        link_to image_tag('deletelabel.png'), post_path(post), :confirm => t('are_you_sure'), :method => :delete, :remote => true, :class => "delete control_icon remove_post", :title => t('delete')
+      else
+        link_to image_tag('deletelabel.png'), share_visibility_path(:id => "42", :post_id => post.id), :method => :put, :remote => true, :class => "delete remove_post control_icon vis_hide", :title => t('.hide_and_mute')
+      end
     end
   end
 
