cb18f204ef630b0d8cd9fe54245e62f601c90270
Joe Bivins <jbivins@tampabay.rr.com>
2012-01-10 01:59:42 -0500

    Fixes #2610 (attempt 2).
    Works-around the IE 9 login problem without modifying vendor/backbone.js.
    
    Tested with IE 9.0.4.

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,2 +1,7 @@
 document.createElement('header');
 document.createElement('footer');
+
+// IE 9 work-around for vendor/backbone.js
+if ((window.history) && (document.documentMode == 9)) {
+	window.history.pushState = function() { };
+}
