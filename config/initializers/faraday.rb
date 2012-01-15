bec3e2c92bb4f3ca55303717591cba7fb2277994
Jonne Haß <mrzyx@mrzyx.de>
2012-01-15 02:49:14 +0100

    set faradays timeout 25

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,7 +1,7 @@
 #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
 #   licensed under the Affero General Public License version 3 or later.  See
 #   the COPYRIGHT file.
-options = {:timeout => 5}
+options = {:timeout => 25}
 
 options[:ssl] = {:ca_file => EnviromentConfiguration.ca_cert_file_location}
 Faraday.default_connection = Faraday::Connection.new(options) do |b|
