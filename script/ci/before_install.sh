ecc79fb3339c58b1e65d0c17d5f4d4b0243afb82
Jonne Haß <mrzyx@mrzyx.de>
2012-01-19 14:07:11 +0100

    bump to rubygems 1.8.15

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Ensure known RubyGems version
-if [ "$(gem --version)" != "1.8.12" ]; then
+if [ "$(gem --version)" != "1.8.15" ]; then
   echo "RubyGems version is $(gem --version). Changing..."
-  rvm rubygems 1.8.12
+  rvm rubygems 1.8.15
   echo "Changed RubyGems version to $(gem --version)."
 fi
 
