5ce4f607369c1adda5b1cb3ee5b512baccc6dacd
Frederico Araujo <fred.the.master@gmail.com>
2012-01-19 13:20:59 +0700

    Added Fallback to use old hoptoad_api_key if not application.yml is not update

--- config/initializers/airbreak.rb
+++ config/initializers/airbreak.rb
@@ -1,6 +1,13 @@
+# if old key still there, use it for new API,
+# update application.yml to use airbrake_api_key instead
 # (Former Hoptoad)
-if AppConfig[:airbreak_api_key].present?
+if AppConfig[:airbrake_api_key].present?
   Airbrake.configure do |config|
-    config.api_key = AppConfig[:airbreak_api_key]
+    config.api_key = AppConfig[:airbrake_api_key]
+  end
+elsif AppConfig[:hoptoad_api_key].present?
+  puts "You're using old hoptoad_api_key, please update application.yml to use airbrake_api_key instead."
+  Airbrake.configure do |config|
+    config.api_key = AppConfig[:hoptoad_api_key]
   end
 end
