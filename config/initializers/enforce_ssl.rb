98bdf680034e531ca7006bb4c39662bcb50b053e
Jonne Haß <me@mrzyx.de>
2012-01-22 18:42:06 +0100

    do not rely on hoptoad to insert rack ssl

--- config/initializers/enforce_ssl.rb
+++ config/initializers/enforce_ssl.rb
@@ -1,4 +1,8 @@
+# Copyright (c) 2010-2011, Diaspora Inc.  This file is
+# licensed under the Affero General Public License version 3 or later.  See
+# the COPYRIGHT file.
+
 if EnviromentConfiguration.enforce_ssl?
-  Rails.application.config.middleware.insert_before HoptoadNotifier::UserInformer, Rack::SSL
+  Rails.application.config.middleware.insert_before ActionDispatch::Cookies, Rack::SSL
   puts "Rack::SSL is enabled"
 end
