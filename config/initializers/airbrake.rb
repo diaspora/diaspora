25a15259bc10291258ac941c56e00991afe5ebc7
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-01-22 11:06:18 -0800

    explicitly require airbrake middleware if it is configured.... i should not have to do this however

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -4,7 +4,11 @@
 
 if AppConfig[:airbrake_api_key].present?
   require 'airbrake'
+  puts "in airbrake"
   Airbrake.configure do |config|
     config.api_key = AppConfig[:airbrake_api_key]
   end
+
+
+  Rails.application.config.middleware.insert_before 1, Airbrake::Rack
 end
