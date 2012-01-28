8529293eeecd0583432fcbd252a6631cce28bad1
danielgrippi <danielgrippi@gmail.com>
2012-01-27 16:48:08 -0800

    resque deals with stringified hashes, not symbolized ones [ci skip]

--- app/models/jobs/deferred_dispatch.rb
+++ app/models/jobs/deferred_dispatch.rb
@@ -9,6 +9,7 @@ module Jobs
     def self.perform(user_id, object_class_name, object_id, opts)
       user = User.find(user_id)
       object = object_class_name.constantize.find(object_id)
+      opts = HashWithIndifferentAccess.new(opts)
       opts[:services] = user.services.where(:type => opts.delete(:service_types)).all
 
       if opts[:additional_subscribers].present?
