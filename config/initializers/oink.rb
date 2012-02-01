e5fe6a2280ac17a3af6d9f9186949296eeb96a4a
Jonne Haß <me@mrzyx.de>
2012-02-01 20:06:18 +0100

    fix typo, sorry [ci skip]

--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -5,7 +5,7 @@ if Rails.env == 'development'
     $stderr.puts "WARNING:"
     $stderr.puts "  You run in development but Oink isn't available in your gems."
     $stderr.puts "  That most likely means you copied the bundle command from the installation guide"
-    $sterrr.puts "  and didn't removed development from the --without parameter while having"
+    $stderr.puts "  and didn't removed development from the --without parameter while having"
     $stderr.puts "  the intention to run under development mode. To fix this either switch to"
     $stderr.puts "  production mode or do a rm .bundle/config and run bundle install --without heroku"
   end
