fafa74a222aae645961e77f7d8c3c3eb33106d84
danielgrippi <danielgrippi@gmail.com>
2012-02-05 21:23:45 -0800

    fix spec, mustache scoping issue in partial.

--- spec/controllers/jasmine_fixtures/template_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe "template generation" do
-  it "generates templates", :fixture => true do
-    extend JasmineFixtureGeneration
-    templates = Haml::Engine.new(Rails.root.join("app", "views", "layouts", "_templates.haml").read).render
-    save_fixture(templates, "underscore_templates")
-  end
-end
