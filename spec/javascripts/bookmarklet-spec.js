0222e4b2ee1abb32c0ad15b8dd86683e7669068d
Florian Staudacher <florian_staudacher@yahoo.de>
2012-02-07 21:47:53 +0100

    added intentionally failing specs, because dirty input breaks bookmarklet see also #2741 #2751 #2800

---
+++ spec/javascripts/bookmarklet-spec.js
@@ -0,0 +1,39 @@
+/*   Copyright (c) 2010-2012, Diaspora Inc.  This file is
+*   licensed under the Affero General Public License version 3 or later.  See
+*   the COPYRIGHT file.
+*/ 
+
+describe("bookmarklet", function() {
+
+  describe("base functionality", function(){
+    beforeEach( function(){
+      spec.loadFixture('empty_bookmarklet');
+    });
+
+    it('verifies the publisher is loaded', function(){
+      expect(typeof Publisher === "object").toBeTruthy();
+    });
+
+    it('verifies we are using the bookmarklet', function(){
+      expect(Publisher.bookmarklet).toBeTruthy();
+    });
+  });
+
+  describe("prefilled bookmarklet", function(){
+    it('fills in some text into the publisher', function(){
+      spec.loadFixture('prefilled_bookmarklet');
+      expect($("#publisher #status_message_fake_text").val() == "").toBeFalsy();
+      expect($("#publisher #status_message_text").val() == "").toBeFalsy();
+    });
+
+    it('handles dirty input well', function(){
+      spec.loadFixture('prefilled_bookmarklet_dirty');
+      expect($("#publisher #status_message_fake_text").val() == "").toBeFalsy();
+      expect($("#publisher #status_message_text").val() == "").toBeFalsy();      
+    });
+  });
+
+  
+
+  
+});
