28504f09bb5e4718156208ee1d82485d3b7ecc6c
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-02-14 15:13:45 -0800

    federation logger now in place.

--- config/environments/integration1.rb
+++ config/environments/integration1.rb
@@ -8,20 +8,20 @@ Diaspora::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
-  #config.threadsafe!
+  
+  config.threadsafe!
 
   # Monkeypatch around the nasty "2.5MB exception page" issue, caused by very large environment vars
   # This snippet via: http://stackoverflow.com/questions/3114993/exception-pages-in-development-mode-take-upwards-of-15-30-seconds-to-render-why
