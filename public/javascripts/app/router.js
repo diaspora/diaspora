c3c23209c312a0be0e6974ceffe741dd732d3847
danielgrippi <danielgrippi@gmail.com>
2012-02-15 22:33:45 -0800

    more fiddling with box model; added arrows

--- public/javascripts/app/router.js
+++ public/javascripts/app/router.js
@@ -47,7 +47,7 @@ app.Router = Backbone.Router.extend({
         templateName : templateName
       }).render();
 
-      $("#main_stream").html(view.el);
+      $("#post-content").html(view.el);
     }})
   }
 });
