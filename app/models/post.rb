db9a1be0fbab60502a7f6656a3fe8f49b8af295e
Prathamesh Sonpatki <csonpatki@gmail.com>
2012-02-16 19:02:39 +0530

    removed syntax error in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ActiveRecord::Base
   has_many :participations, :dependent => :delete_all, :as => :target
 
   attr_accessor :user_like,
-                :user_participation
+                :user_participation,
                 :likes_count
   # NOTE API V1 to be extracted
   acts_as_api
