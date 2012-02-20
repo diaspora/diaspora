a1fe55fcb787afeec602f149aee033b8996f492e
danielgrippi <danielgrippi@gmail.com>
2012-02-20 13:58:32 -0800

    mark icons colored if user has interacted with a post.

--- app/presenters/post_presenter.rb
+++ app/presenters/post_presenter.rb
@@ -13,6 +13,8 @@ class PostPresenter
       :post => self.post.as_api_response(:backbone).update(
         {
         :user_like => self.user_like,
+        :user_participation => self.user_participation,
+        :user_reshare => self.user_reshare,
         :next_post => self.next_post_url,
         :previous_post => self.previous_post_url
       }),
@@ -25,6 +27,19 @@ class PostPresenter
     if like = Like.where(:target_id => self.post.id, :target_type => "Post", :author_id => current_user.person.id).first
       like.as_api_response(:backbone)
     end
+  end
+
+  def user_participation
+    return unless self.current_user.present?
+
+    if participation = Participation.where(:target_id => self.post.id, :target_type => "Post", :author_id => current_user.person.id).first
+      participation.as_api_response(:backbone)
+    end
+  end
+
+  def user_reshare
+    return unless self.current_user.present?
+    Reshare.where(:root_guid => self.post.guid, :author_id => current_user.person.id).exists?
   end
 
   def next_post_url
