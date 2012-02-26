f70b2903162e659462b330852f4751c5b3bbf40f
danielgrippi <danielgrippi@gmail.com>
2012-02-25 19:18:32 -0800

    added two helpers to handlebars for avatar images / links to people.

--- public/javascripts/app/helpers/handlebars-helpers.js
+++ public/javascripts/app/helpers/handlebars-helpers.js
@@ -5,3 +5,18 @@ Handlebars.registerHelper('t', function(scope, values) {
 Handlebars.registerHelper('imageUrl', function(path){
   return app.baseImageUrl() + path;
 })
+
+Handlebars.registerHelper('linkToPerson', function(context, block) {
+  var html = "<a href=\"/people/" + context.guid + "\" class=\"author-name\">";
+      html+= block.fn(context);
+      html+= "</a>";
+
+  return html
+})
+
+Handlebars.registerHelper('avatar', function(person, size, imageClass) {
+  size = (typeof(size) != "string" ? "small" : size);
+  imageClass = (typeof(imageClass) != "string" ? size : imageClass);
+
+  return "<img src=\"" + person.avatar[size] +"\" class=\"avatar " + imageClass + "\" title=\"" + person.name +"\" />";
+})
