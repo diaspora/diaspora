ac8cd8368effabbf1cbf6788b07327e0d0f5e451
Steven Fuchs <stwf@yahoo.com>
2012-03-07 21:01:15 -0500

    fixing conflicts in merge

---
+++ spec/javascripts/aspect-spec.js
@@ -0,0 +1,12 @@
+describe("Diaspora.Aspects", function() {
+  beforeEach(function() {
+    spec.loadFixture("aspects_index");
+  });
+
+
+  describe("edit aspect name", function() {
+    it("it should ", function() {
+      var startName = $("a [data-aspect_id=13]").text();
+    });
+  });
+});
