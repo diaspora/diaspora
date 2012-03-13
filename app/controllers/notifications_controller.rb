97d6b8dab87a2659bce4c1a9616ff9965db96b6a
Diaspora Europe <info@diasp.eu>
2012-03-13 11:46:14 +0100

    notifications update spec and cleanup json response

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -36,8 +36,10 @@ class NotificationsController < ApplicationController
 
       pager.replace(result)
     end
-    @notifications.each do |n|
-      n[:note_html] = render_to_string( :partial => 'notify_popup_item', :locals => { :n => n } )
+    if request.format != :json
+      @notifications.each do |n|
+        n[:note_html] = render_to_string( :partial => 'notify_popup_item', :locals => { :n => n } )
+      end
     end
     @group_days = @notifications.group_by{|note| I18n.l(note.created_at, :format => I18n.t('date.formats.fullmonth_day')) }
 
