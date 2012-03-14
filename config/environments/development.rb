41e18fdf33cd522b8e463cb867823167d50f8e45
= <social@conpanna.net>
2012-03-14 18:03:42 +0200

    email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Diaspora::Application.configure do
   # Don't care if the mailer can't send
   #   But we do care => added.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
   
   config.active_support.deprecation = [:stderr, :log]
   #config.threadsafe!
