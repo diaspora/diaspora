7b4c377df12b8382670d8d8232aaaa8af8664186
Florian Staudacher <florian_staudacher@yahoo.de>
2012-03-15 19:44:18 +0100

    make tests happy

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -1,20 +1,27 @@
 require 'oembed'
 require 'uri'
 
-OEmbedCubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
-
-OEmbed::Providers.register(
-  OEmbed::Providers::Youtube,
-  OEmbed::Providers::Vimeo,
-  OEmbed::Providers::Flickr,
-  OEmbed::Providers::SoundCloud,
-  OEmbedCubbies
-)
-OEmbed::Providers.register_fallback(OEmbed::ProviderDiscovery)
-
 #
 # SECURITY NOTICE! CROSS-SITE SCRIPTING!
 # these endpoints may inject html code into our page
 # note that 'trusted_endpoint_url' is the only information
 # in OEmbed that we can trust. anything else may be spoofed!
+
+OEmbedCubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
+
+oembed_provider_list = [
+  OEmbed::Providers::Youtube,
+  OEmbed::Providers::Vimeo,
+  OEmbed::Providers::Flickr,
+  OEmbed::Providers::SoundCloud,
+  OEmbedCubbies
+]
+
+SECURE_ENDPOINTS = oembed_provider_list.map do |provider|
+  OEmbed::Providers.register(provider)
+  provider.endpoint
+end
+
+OEmbed::Providers.register_fallback(OEmbed::ProviderDiscovery)
+
 TRUSTED_OEMBED_PROVIDERS = OEmbed::Providers
