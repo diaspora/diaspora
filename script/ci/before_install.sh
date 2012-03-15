3a149d8cc7ab010e7fa0bb668f2dcafebee5880c
Jonne Haß <me@mrzyx.de>
2012-03-15 15:07:22 +0100

    rubygems 1.8.17 should be fine

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ensure known RubyGems version
-if [ "$(gem --version)" != "1.8.15" ]; then
+if [ "$(gem --version)" != "1.8.17" ]; then
   echo "RubyGems version is $(gem --version). Changing..."
-  rvm rubygems 1.8.15
+  rvm rubygems 1.8.17
   echo "Changed RubyGems version to $(gem --version)."
 fi
