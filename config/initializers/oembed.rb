e6b403434f5726b964e8428db9886be9cc73ae0f
Florian Staudacher <florian_staudacher@yahoo.de>
2012-03-15 18:34:47 +0100

    some restructuring around oembed providers

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -1,16 +1,20 @@
 require 'oembed'
 require 'uri'
 
-OEmbed::Providers.register_all
+OEmbedCubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
+
+OEmbed::Providers.register(
+  OEmbed::Providers::Youtube,
+  OEmbed::Providers::Vimeo,
+  OEmbed::Providers::Flickr,
+  OEmbed::Providers::SoundCloud,
+  OEmbedCubbies
+)
 OEmbed::Providers.register_fallback(OEmbed::ProviderDiscovery)
+
 #
 # SECURITY NOTICE! CROSS-SITE SCRIPTING!
 # these endpoints may inject html code into our page
 # note that 'trusted_endpoint_url' is the only information
 # in OEmbed that we can trust. anything else may be spoofed!
-SECURE_ENDPOINTS = [::OEmbed::Providers::Youtube.endpoint,
-                    ::OEmbed::Providers::Flickr.endpoint,
-                    'http://soundcloud.com/oembed',
-                    'http://cubbi.es/oembed'
-                   ]
-ENDPOINT_HOSTS_STRING = SECURE_ENDPOINTS.map{|e| URI.parse(e.split('{')[0]).host}.to_s
+TRUSTED_OEMBED_PROVIDERS = OEmbed::Providers
