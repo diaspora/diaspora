f17b23867523df4b344d572e2036fab5b2cdfe9e
= <social@conpanna.net>
2012-03-20 23:25:03 +0200

    unicorn

--- config/application.rb
+++ config/application.rb
@@ -42,10 +42,14 @@ module Diaspora
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Jerusalem'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+#    config.i18n.load_path += Dir[Rails.root.join('config','locales','diaspora', '*.{rb,yml}').to_s]
+#    config.i18n.load_path += Dir[Rails.root.join('config','locales','devise', '*.{rb,yml}').to_s]
+#    config.i18n.load_path += Dir[Rails.root.join('config','locales','javascript', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
      config.generators do |g|
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,11 @@
+#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'e9dac1c42c052909bc94729591b28ea73e7564b51b652219046decdff0109123f06be1c9448bb1b3'
