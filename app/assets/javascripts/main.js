8b92ed73a481d86be38339b0a2aa1095ef8a4345
Steven Hancock <stevenh512@gmail.com>
2012-03-23 18:32:26 -0700

    Remove jquery_ujs from main.js manifest
    Since jquery and jquery_ujs are now included by the jquery_include_tag
    helper (see commit 9e752585), they shouldn't be included by the asset
    pipeline in our javascript manifests. :)

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,7 +2,6 @@
  *   licensed under the Affero General Public License version 3 or later.  See
  *   the COPYRIGHT file.
  */
-//= require jquery_ujs
 //= require underscore
 //= require backbone
 //= require rails.validations
