f96fc5834fb3ed006e472453870abc7ed2046e1c
Steven Hancock <stevenh512@gmail.com>
2012-03-24 17:53:55 -0700

    Make the Mention button work with the asset pipeline
    * Fix the javascript_include_tag in the status_messages/new view
    * Precompile pubhlsiher.js and jquery.textchange.min.js
    
    This fixes a 500 error that was being thrown when the Mention button
    was clicked, due to the above two javascripts not being precompiled
    and a javascript include that needed to be updated to load
    jquery.textchange.js from the correct path.

--- config/application.rb
+++ config/application.rb
@@ -76,9 +76,9 @@ module Diaspora
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     # Javascripts
     config.assets.precompile += [ "contact-list.js", "finder.js", "home.js",
-      "ie.js", "inbox.js", "jquery.js", "jquery_ujs.js", "login.js", "mailchimp.js",
-      "main.js", "mobile.js", "profile.js", "people.js", "photos.js", "templates.js",
-      "validation.js" ]
+      "ie.js", "inbox.js", "jquery.js", "jquery_ujs.js", "jquery.textchange.min.js",
+      "login.js", "mailchimp.js", "main.js", "mobile.js", "profile.js",
+      "people.js", "photos.js", "publisher.js", "templates.js", "validation.js" ]
 
     # Stylesheets
     config.assets.precompile += [ "blueprint.css", "bootstrap.css", "default.css",
