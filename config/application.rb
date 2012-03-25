76bbe7be3fee508a25b1d07e3f63957156337d32
Steven Hancock <stevenh512@gmail.com>
2012-03-24 18:53:04 -0700

    Precompile more Javascript assets
    This should be the last of them, I've gone through every view and made sure
    that every javascript file included by a javascript_include_tag is now on the
    list of assets to precompile.

--- config/application.rb
+++ config/application.rb
@@ -75,10 +75,10 @@ module Diaspora
 
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     # Javascripts
-    config.assets.precompile += [ "contact-list.js", "finder.js", "home.js",
-      "ie.js", "inbox.js", "jquery.js", "jquery_ujs.js", "jquery.textchange.min.js",
-      "login.js", "mailchimp.js", "main.js", "mobile.js", "profile.js",
-      "people.js", "photos.js", "publisher.js", "templates.js", "validation.js" ]
+    config.assets.precompile += [ "aspect-contacts.js", "contact-list.js", "finder.js",
+      "home.js", "ie.js", "inbox.js", "jquery.js", "jquery_ujs.js", "jquery.textchange.min.js",
+      "login.js", "mailchimp.js", "main.js", "mobile.js", "profile.js", "people.js", "photos.js",
+      "profile.js", "publisher.js", "templates.js", "validation.js" ]
 
     # Stylesheets
     config.assets.precompile += [ "blueprint.css", "bootstrap.css", "default.css",
