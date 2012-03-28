48392e717b195f2cb01465978ba5fed1bd2b1855
Benjamin Neff <benjamin@coding4coffee.ch>
2012-03-28 03:17:02 +0200

    migration for old invitation-emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120322223517) do
+ActiveRecord::Schema.define(:version => 20120328025842) do
 
   create_table "account_deletions", :force => true do |t|
     t.string  "diaspora_handle"
