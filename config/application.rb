7e517c2636704ed6b142aa6cc42832044ee059eb
Steven Hancock <stevenh512@gmail.com>
2012-04-13 20:42:05 -0700

    Initialize on asset precompile
    This re-enables 'config.assets.initialize_on_precompile' to allow the
    Rails Admin assets to precompile. On Heroku deploys, this may require
    the Heroku Labs 'user_env_compile' feature to be enabled, which you
    should be using anyway for Asset Sync to work properly. :)

--- config/application.rb
+++ config/application.rb
@@ -73,9 +73,6 @@ module Diaspora
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # For easier deployment to Heroku
-    config.assets.initialize_on_precompile = false
-
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     # Javascripts
     config.assets.precompile += [ "aspect-contacts.js", "contact-list.js", "finder.js",
