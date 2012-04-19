3130bbad97caba1ebf47ae626af3dd4ec5747d04
Dennis Collinson <dennis.collective@gmail.com>
2012-04-19 12:29:19 -0700

    render template currently needs to be overridden WHOA junglist massiv

--- app/assets/javascripts/app/views/canvas_view.js
+++ app/assets/javascripts/app/views/canvas_view.js
@@ -4,10 +4,12 @@ app.views.Canvas = app.views.Base.extend(_.extend(app.views.infiniteScrollMixin,
     this.collection = this.stream.items
     this.postClass = app.views.SmallFrame,
     this.setupInfiniteScroll()
-  }
-//  renderTemplate : function() {
+  },
+
+  renderTemplate : function() {
+    this.postRenderTemplate();
 //   setTimeout(_.bind(this.mason, this), 1000)
-//  },
+  },
 //
 //  mason : function() {
 //    this.$el.isotope({
