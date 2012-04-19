f7d320b622fad1ceed72ae113bb2105027fb0571
Dennis Collinson <dennis.collective@gmail.com>
2012-04-19 14:05:40 -0700

    MS DG; checkboxes are togglable, with tested css magick

--- spec/javascripts/app/views/services_selector_view_spec.js
+++ spec/javascripts/app/views/services_selector_view_spec.js
@@ -2,7 +2,7 @@ describe("app.views.ServicesSelector", function(){
   beforeEach(function(){
     loginAs({
       services : [
-        { provider : "fakeBook" }
+        { provider : "facebook" }
       ]
     });
 
@@ -11,12 +11,21 @@ describe("app.views.ServicesSelector", function(){
 
   describe("rendering", function(){
     beforeEach(function(){
+      this.view.setElement("#jasmine_content")
       this.view.render();
     });
 
     it("displays all services", function(){
       var checkboxes = $(this.view.el).find('input[type="checkbox"]');
-      expect(checkboxes.val()).toBe("fakeBook");
+      expect(checkboxes.val()).toBe("facebook");
     });
+
+    // this tests the crazy css we have in a bassackwards way
+    // check out toggling the services on the new publisher and make sure it works if you change stuff.
+    it("selects the checkbox when the image is clicked", function(){
+      expect($("label[for=service_toggle_facebook]").css("opacity")).toBeLessThan(0.8) //floating point weirdness, be safe.
+      this.view.$("input[value='facebook']").select()
+      expect($("label[for=service_toggle_facebook]").css("opacity")).toBe('1')
+    })
   });
 });
