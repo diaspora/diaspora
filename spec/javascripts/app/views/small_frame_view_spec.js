ca33070b9e9f8c222bed20636084219a93abc7a8
Dennis Collinson <dennis.collective@gmail.com>
2012-04-21 15:21:52 -0700

    loader is background image of span fix tests

--- spec/javascripts/app/views/small_frame_view_spec.js
+++ spec/javascripts/app/views/small_frame_view_spec.js
@@ -20,18 +20,18 @@ describe("app.views.SmallFrame", function(){
   })
 
   describe("textClass", function(){
-    it("returns and empty string with a post with text longer than 140 characters", function(){
-      expect(this.view.textClass()).toBe("")
+    it("returns and 'text extra-small' with a post with text longer than 140 characters", function(){
+      expect(this.view.textClass()).toBe("text extra-small")
     });
 
-    it("returns 'x2 height' if if it is less than 500 characters", function(){
+    it("returns 'text medium' if if it is less than 500 characters", function(){
       this.view.model.set({text: "ldfkdfdkfkdfjdkjfdkfjdkjfkdfjdkjfkdjfkdjfdkjdfkjdkfjkdjfkdjfkdfkdjf" +
         "dfkjdkfjkdjfkdjfkdjfdkfjdkfjkd;fklas;dfkjsad;kljf ;laskjf;lkajsdf;kljasd;flkjasd;flkjasdf;l" +
         "jasd;fkjasd;lfkja;sdlkjf;alsdkf;lasdjf;alskdfj;alsdkjf;alsdkfja;sdlkj "})
-      expect(this.view.textClass()).toBe("x2 height")
+      expect(this.view.textClass()).toBe("text medium")
     });
 
-    it("returns 'x2 width' if if it is more than 500 characters", function(){
+    it("returns 'text large' if if it is more than 500 characters", function(){
       this.view.model.set({text: "ldfkdfdkfkdfjdkjfdkfjdkjfkdfjdkjfkdjfkdjfdkjdfkjdkfjkdjfkdjfkdfkdjf" +
         "dfkjdkfjkdjfkdjfkdjfdkfjdkfjkd;fklas;dfkjsad;kljf ;laskjf;lkajsdf;kljasd;flkjasd;flkjasdf;l" +
         "jasd;fkjasd;lfkja;sdlkjf;alsdkf;lasdjf;alskdfj;alsdkjf;alsdkfja;sdlkj f;lkajs;dflkjasd;lfkja;sldkjf;alskdjfs" +
@@ -41,7 +41,7 @@ describe("app.views.SmallFrame", function(){
         "as;dlfk;alsdkjf;lkasdf;lkjasd;flkjasd;lfkjasdkl;" +
         "asl;dkfj;asldkfj;alsdkfj;alsdfjk" +
         "askdjf;laksdf;lkdflkjhasldfhoiawufjkhasugfoiwaufaw "})
-      expect(this.view.textClass()).toBe("x2 width")
+      expect(this.view.textClass()).toBe("text large")
     })
   })
 })
