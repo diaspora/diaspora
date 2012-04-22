ab7ac667d8fc2981598488cc7d0dc378fc5f1b76
danielgrippi <danielgrippi@gmail.com>
2012-04-21 20:42:50 -0700

    only focus on photos for now with ratios.  one thing at a time, yo. i've temporarily removed all the arbitrary text switching for now (which is still in version control // which we will get to later) also moved photo dimensions into a dimensions hash in photo.rb.

--- spec/javascripts/app/views/small_frame_view_spec.js
+++ spec/javascripts/app/views/small_frame_view_spec.js
@@ -13,35 +13,9 @@ describe("app.views.SmallFrame", function(){
     beforeEach(function(){
       this.view.render()
     });
-
-    it("should have an image for each photoAttr on the model", function(){
-
-    })
   })
 
-  describe("textClass", function(){
-    it("returns and 'text extra-small' with a post with text longer than 140 characters", function(){
-      expect(this.view.textClass()).toBe("text extra-small")
-    });
-
-    it("returns 'text medium' if if it is less than 500 characters", function(){
-      this.view.model.set({text: "ldfkdfdkfkdfjdkjfdkfjdkjfkdfjdkjfkdjfkdjfdkjdfkjdkfjkdjfkdjfkdfkdjf" +
-        "dfkjdkfjkdjfkdjfkdjfdkfjdkfjkd;fklas;dfkjsad;kljf ;laskjf;lkajsdf;kljasd;flkjasd;flkjasdf;l" +
-        "jasd;fkjasd;lfkja;sdlkjf;alsdkf;lasdjf;alskdfj;alsdkjf;alsdkfja;sdlkj "})
-      expect(this.view.textClass()).toBe("text medium")
-    });
-
-    it("returns 'text large' if if it is more than 500 characters", function(){
-      this.view.model.set({text: "ldfkdfdkfkdfjdkjfdkfjdkjfkdfjdkjfkdjfkdjfdkjdfkjdkfjkdjfkdjfkdfkdjf" +
-        "dfkjdkfjkdjfkdjfkdjfdkfjdkfjkd;fklas;dfkjsad;kljf ;laskjf;lkajsdf;kljasd;flkjasd;flkjasdf;l" +
-        "jasd;fkjasd;lfkja;sdlkjf;alsdkf;lasdjf;alskdfj;alsdkjf;alsdkfja;sdlkj f;lkajs;dflkjasd;lfkja;sldkjf;alskdjfs" +
-        "as;lkdfj;asldfkj;alsdkjf;laksdjf;lasdkjf;lasdkj;lafkja;sldkjf;alsdkjf;laskjf;laskdjf;laksjdf;laksjdf;lk;" +
-        "a;dslkf;laskjdf;lakjsdf;lkasd;lfkja;sldkfj;o sdfsdfsdf" +
-        "sdfsdfsdfsdfsdfdsdsfsdfsfsdsfdsf;lgkjs;dlkfj;alsdkjf;laksdjf;lkasjdf;lkajsdf;lkjasd;flkjasd;lfkjas;dlkfj;alsdkjf" +
-        "as;dlfk;alsdkjf;lkasdf;lkjasd;flkjasd;lfkjasdkl;" +
-        "asl;dkfj;asldkfj;alsdkfj;alsdfjk" +
-        "askdjf;laksdf;lkdflkjhasldfhoiawufjkhasugfoiwaufaw "})
-      expect(this.view.textClass()).toBe("text large")
-    })
+  describe("photos", function() {
+    // ratio pending...
   })
-})
+});
