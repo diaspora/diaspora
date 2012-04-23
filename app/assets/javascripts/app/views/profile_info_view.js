affba805247ccce860a642d0495cfbb1b64aada0
danielgrippi <danielgrippi@gmail.com>
2012-04-23 16:43:32 -0700

    tweak profile top layout [ci skip]

--- app/assets/javascripts/app/views/profile_info_view.js
+++ app/assets/javascripts/app/views/profile_info_view.js
@@ -2,6 +2,7 @@ app.views.ProfileInfo = app.views.Base.extend({
   templateName : "profile-info",
 
   initialize : function(){
+    console.log(this.model)
     this.model.bind("change", this.render, this) //this should go on profile info view when it gets Extracted
   }
 })
