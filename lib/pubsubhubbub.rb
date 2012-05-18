64dae868fede4645ea304bcc0acc3c6104bd0386
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-05-17 18:23:23 -0700

    fix some tests around pubsubhubub

--- lib/pubsubhubbub.rb
+++ lib/pubsubhubbub.rb
@@ -9,9 +9,9 @@ class Pubsubhubbub
   end
 
   def publish(feed)
+
     conn = Faraday.new do |c|
       c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
-      c.use Faraday::Response::Logger     # log request & response to STDOUT
       c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
     end
     conn.post @hub, {'hub.url' => feed, 'hub.mode' => 'publish'}
