67dd49b1e4e6ab992be819ca9cea1338ff89dc9d
Florian Staudacher <florian_staudacher@yahoo.de>
2012-05-20 22:44:08 +0200

    ... wait for it ...

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -195,6 +195,10 @@ end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
+  wait_until(3) do
+    current_path = path_to(page_name)
+  end
+
   if current_path.respond_to? :should
     current_path.should == path_to(page_name)
   else
