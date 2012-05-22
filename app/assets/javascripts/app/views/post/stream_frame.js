83fec811cc7461812d9812c2113e97687f62d727
danielgrippi <danielgrippi@gmail.com>
2012-05-22 13:50:07 -0700

    DG MS; author attribution in the stream; cleaned up some *new* stream styling

--- app/assets/javascripts/app/views/post/stream_frame.js
+++ app/assets/javascripts/app/views/post/stream_frame.js
@@ -1,9 +1,25 @@
-app.views.Post.StreamFrame = app.views.Post.SmallFrame.extend({
+app.views.Post.StreamFrame = app.views.Base.extend({
+
+  className : "stream-frame",
+
+  templateName : "stream-frame",
+
+  subviews : {
+    ".small-frame" : "smallFrameView"
+  },
+
+  initialize : function() {
+    this.smallFrameView = new app.views.Post.SmallFrame({model : this.model})
+  },
+
   events : _.extend({
     'click .content' : 'triggerInteracted'
   }, app.views.Post.SmallFrame.prototype.events),
 
   triggerInteracted : function() {
     app.page.trigger("frame:interacted", this.model)
-  }
-})
+  },
+
+  // this is some gross shit.
+  goToPost : $.noop
+});
