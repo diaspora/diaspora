de8d7770fe522d08eba6d7e1657d165f01506e61
danielgrippi <danielgrippi@gmail.com>
2012-05-30 14:11:45 -0700

    don't use double quotes in javascript tag helper for mixpanel [ci skip]

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -7,7 +7,7 @@ module AnalyticsHelper
     include_analytics "mixpanel" do |service_uid|
       javascript_tag do
         <<-JS
-          (function(d,c){var a,b,g,e;a=d.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===d.location.protocol?"https:":"http:")+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';b=d.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);c._i=[];c.init=function(a,d,f){var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";g="disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config".split(" ");
+          (function(d,c){var a,b,g,e;a=d.createElement("script");a.type='text/javascript';a.async=!0;a.src=('https:'===d.location.protocol?'https:':'http:')+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';b=d.getElementsByTagName('script')[0];b.parentNode.insertBefore(a,b);c._i=[];c.init=function(a,d,f){var b=c;'undefined'!==typeof f?b=c[f]=[]:f='mixpanel';g='disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config'.split(' ');
           for(e=0;e<g.length;e++)(function(a){b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,0)))}})(g[e]);c._i.push([a,d,f])};window.mixpanel=c})(document,[]);
           mixpanel.init(#{service_uid});
         JS
