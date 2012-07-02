c2bbe4e466931f600e5cfdfcc56d664d6c373329
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-02 11:34:31 -0700

    somehow some dirty state made it into my merge :(

--- app/assets/javascripts/app/collections/posts.js
+++ app/assets/javascripts/app/collections/posts.js
@@ -2,7 +2,3 @@ app.collections.Posts = Backbone.Collection.extend({
   model: app.models.Post,
   url : "/posts"
 });
-
-app.collection.PublicPosts = app.collection.Posts.extend({
-  url : '/public'
-})
