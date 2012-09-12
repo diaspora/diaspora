1277cf92b2e1893f5a986b9d897ed27f5d8cb574
Jonne Haß <me@mrzyx.de>
2012-09-12 20:44:37 +0200

    refactor ci scripts

---
+++ script/ci/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Create a database.yml for the right database
+echo "Setting up database.yml for $DB"
+cp config/database.yml.example config/database.yml
+if [ "$DB" = "postgres" ]; then
+  sed -i 's/*choose/*postgres_travis/' config/database.yml
+fi
+
+command="bundle exec rake --trace ci:travis:${BUILD_TYPE}"
+
+exec xvfb-run --auto-servernum --server-num=1 --server-args="-screen 0 1280x1024x8" $command
