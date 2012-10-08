d7e30ea6d9a071ddac8f48d6278def6f2c0e3c11
Ruxton <ruxton@thefrontiergroup.com.au>
2012-10-08 09:13:03 +0800

    updated spec to fix to_datetime issue

--- spec/lib/diaspora/fetcher/public_spec.rb
+++ spec/lib/diaspora/fetcher/public_spec.rb
@@ -95,7 +95,7 @@ describe PublicFetcher do
 
       it 'applies the created_at date from JSON to the record' do
         @data.each do |post|
-          date = ActiveSupport::TimeZone.new('UTC').parse(post['created_at'])
+          date = ActiveSupport::TimeZone.new('UTC').parse(post['created_at']).to_datetime
 
           entry = StatusMessage.find_by_guid(post['guid'])
           entry.created_at.should eql(date)
@@ -111,7 +111,7 @@ describe PublicFetcher do
 
       it 'applies now to interacted_at on the record' do
         @data.each do |post|
-          date = ActiveSupport::TimeZone.new('UTC').parse(@now.to_s)
+          date = ActiveSupport::TimeZone.new('UTC').parse(@now.to_s).to_datetime
 
           entry = StatusMessage.find_by_guid(post['guid'])
           entry.interacted_at.should eql(date)
