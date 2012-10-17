b160b8d73d41eb72989b75acd4f075513af0cdcc
Justin Thomas <justin@justinthomas.name>
2012-10-16 23:23:01 -0700

    updating (wip) scrypto

--- app/assets/javascripts/cryptography.js
+++ app/assets/javascripts/cryptography.js
@@ -7,9 +7,20 @@ $(document).ready(function() {
 			$('#entropy-value').text((progress * 100).toFixed(0) + "%")
 		})
 	}
-	
-	$(document).on("submit", "form[encryptable]", function(event) {
-		alert("submitted!!")
+
+	$(document).on("submit", "form[data-encryptable]", function(event) {
+		$(this).each(function() {
+			var ret = {}
+			var fields = $(this).find('[data-encrypt]')
+
+			fields.each(function() {
+				ret[$(this).attr('id')] = $(this).val()
+			})
+
+			$(ret).encrypt_fields()
+
+			// ret should, at this point, contain rich 'fields' and 'keys' with encrypted data
+		})
 	})
 })
 
