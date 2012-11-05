e937f47b0f56284691a45c0a4efaf9fb6372ef49
Lennart Prelle <me@lennartprelle.de>
2012-11-05 08:07:17 +0100

    add specs for notification count

--- spec/models/notifications/private_message_spec.rb
+++ spec/models/notifications/private_message_spec.rb
@@ -8,10 +8,13 @@ describe Notifications::PrivateMessage do
     before do
       @user1 = alice
       @user2 = bob
+      @user3 = FactoryGirl.create(:user_with_aspect, :username => "peter")
+      
+      connect_users_with_aspects(@user1, @user3)
 
       @create_hash = {
         :author => @user1.person,
-        :participant_ids => [@user1.contacts.first.person.id, @user1.person.id],
+        :participant_ids => [@user1.person.id, @user2.person.id, @user3.person.id],
         :subject => 'cool stuff',
         :messages_attributes => [ {:author => @user1.person, :text => 'stuff'} ]
       }
@@ -20,13 +23,63 @@ describe Notifications::PrivateMessage do
       @msg = @cnv.messages.first
     end
 
-    describe '#make_notifiaction' do
+    describe '#make_notification' do
       it 'does not save the notification' do
         lambda{
           Notification.notify(@user2, @msg, @user1.person)
         }.should_not change(Notification, :count)
       end
 
+      it 'does increase notification count for recipient if author creates a message' do
+        expect { 
+          @cnv.messages.build(:text => "this is cool", :author => @user1.person) 
+        }.to change(
+          ConversationVisibility.where(
+            :conversation_id => @cnv.id, 
+            :person_id => @user2.person.id
+          ).first, :unread).by(1)
+      end
+
+      it 'does not increase own notification if author creates a message' do
+        expect { 
+          @cnv.messages.build(:text => "this is cool", :author => @user1.person) 
+        }.to_not change(
+          ConversationVisibility.where(
+            :conversation_id => @cnv.id, 
+            :person_id => @user1.person.id
+            ).first, :unread).by(1)
+      end
+      
+      it 'does not increase own notification if recipient creates a message' do
+        expect {
+          @cnv.messages.build(:text => "this is cool",:author => @user2.person) 
+        }.to_not change(
+          ConversationVisibility.where(
+            :conversation_id => @cnv.id, 
+            :person_id => @user2.person.id
+          ).first, :unread).by(1)
+      end
+      
+      it 'does increase notification count for author if recipient creates a message' do
+        expect {
+          @cnv.messages.build(:text => "this is cool", :author => @user2.person) 
+        }.to change(
+          ConversationVisibility.where(
+            :conversation_id => @cnv.id, 
+            :person_id => @user1.person.id
+          ).first, :unread).by(1)
+      end
+      
+      it 'does increase notification counts for other recipient if recipient creates a message' do
+        expect {
+          @cnv.messages.build(:text => "this is cool", :author => @user2.person) 
+        }.to change(
+          ConversationVisibility.where(
+            :conversation_id => @cnv.id, 
+            :person_id => @user3.person.id
+          ).first, :unread).by(1)
+      end
+      
       it 'does email the user' do
         opts = {
           :actors => [@user1.person],
