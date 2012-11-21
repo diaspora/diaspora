572e6b5639e52005a080ab5fa45d897a5d5b4c23
Fabián Rodríguez <fabianrbz@gmail.com>
2012-11-21 20:42:41 -0200

    render aspects client side

--- app/assets/javascripts/app/views/aspects_list_view.js
+++ app/assets/javascripts/app/views/aspects_list_view.js
@@ -1,5 +1,17 @@
 app.views.AspectsList = app.views.Base.extend({
   templateName: 'aspects-list',
 
-  el: '#aspects_list'
+  el: '#aspects_list',
+
+  postRenderTemplate : function() {
+    this.collection.each(this.appendAspect, this);
+    this.$('a[rel*=facebox]').facebox();
+  },
+
+  appendAspect: function(aspect) {
+    $("#aspects_list > *:last").before(new app.views.Aspect({
+      model: aspect, attributes: {'data-aspect_id': aspect.get('id')}
+    }).render().el);
+  }
+
 })
