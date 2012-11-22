bbad82c5d29824938da82cddbd2290e10d36f629
Jonne Haß <me@mrzyx.de>
2012-11-22 20:04:19 +0100

    this should restore compatibillity with ruby below 1.9.3-p125, not sure if we should hotfix this, if anyone wants to...

--- script/get_config.rb
+++ script/get_config.rb
@@ -34,7 +34,7 @@ ARGV.each do |arg|
   var, setting_name = arg.split("=")
   setting_name = var unless setting_name
   setting = AppConfig.send(setting_name)
-  setting = setting.get if setting.is_a?(Configuration::Proxy)
+  setting = setting.get if setting.respond_to?(:_proxy?)
   if var != setting_name
     puts "#{var}=#{setting}"
   else
