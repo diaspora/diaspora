e8a7f4097046c971f2de472873756ec75aee2774
Jonne Haß <me@mrzyx.de>
2012-11-22 23:42:08 +0100

    attempt to install ff 16 until there's a new selenium release

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -4,3 +4,7 @@
 envdir="$(readlink -e $(dirname $0))/../env"
 . "$envdir/ruby_env"
 . "$envdir/ensure_right_rubygems"
+
+if [ "$BUILD_TYPE" = "cucumber" ]; then
+  sudo apt-get install -y firefox=16.0
+fi
