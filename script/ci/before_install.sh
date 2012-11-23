19ac3bc45ca7e5c18c9faaff504ea03e503adbc0
Jonne Haß <me@mrzyx.de>
2012-11-23 01:00:21 +0100

    install libnotify4 too

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -7,5 +7,6 @@ envdir="$(readlink -e $(dirname $0))/../env"
 
 if [ "$BUILD_TYPE" = "cucumber" ]; then
   curl http://security.ubuntu.com/ubuntu/pool/main/f/firefox/firefox_16.0.2+build1-0ubuntu0.12.04.1_i386.deb > firefox.deb
+  sudo apt-get install libnotify4
   sudo dpkg -i firefox.deb
 fi
