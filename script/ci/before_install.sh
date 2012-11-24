b2606bc8c81984301632cef980d04579cd6454b1
Jonne Haß <me@mrzyx.de>
2012-11-24 10:35:07 +0100

    givup on installing FF16, just allow cucumber to fail until FF17 is supported

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -4,9 +4,3 @@
 envdir="$(readlink -e $(dirname $0))/../env"
 . "$envdir/ruby_env"
 . "$envdir/ensure_right_rubygems"
-
-if [ "$BUILD_TYPE" = "cucumber" ]; then
-  curl http://security.ubuntu.com/ubuntu/pool/main/f/firefox/firefox_16.0.2+build1-0ubuntu0.11.04.1_i386.deb > firefox.deb
-  sudo apt-get install libnotify4
-  sudo dpkg -i firefox.deb
-fi
