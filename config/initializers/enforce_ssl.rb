ae661b562fd3d96e3bf1893ce0e6257d8cb3db81
Patrick Barret <patrick.barret@live.fr>
2012-11-29 13:01:47 +0100

    Update config/initializers/enforce_ssl.rb

--- config/initializers/enforce_ssl.rb
+++ config/initializers/enforce_ssl.rb
@@ -3,7 +3,6 @@
 # the COPYRIGHT file.
 
 if AppConfig.environment.require_ssl?
-  require 'rack/ssl'
   Rails.application.config.middleware.insert_before 0, Rack::SSL
   puts "Rack::SSL is enabled"
 end
