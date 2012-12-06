4cebe423755d5306d90c042dcc20bc0ba0c63a55
Jonne Haß <me@mrzyx.de>
2012-12-06 10:45:12 +0100

    hmm, I hate it if these things work locally...

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# install selenium prerelease
+gem install selenium-webdriver --pre
+gem list
 
 # Ensure known RubyGems version
 envdir="$(readlink -e $(dirname $0))/../env"
 . "$envdir/ruby_env"
 . "$envdir/ensure_right_rubygems"
 
-# install selenium prerelease
-gem install selenium-webdriver --pre
