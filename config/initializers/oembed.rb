40fdf3819bc9171e16800d0f06b396b96938ec3b
Ruxton <ruxton@thefrontiergroup.com.au>
2013-01-25 22:31:44 +0800

    Added DailyMotion & Twitter oEmbeds

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -9,6 +9,13 @@ require 'uri'
 
 OEmbedCubbies = OEmbed::Provider.new("http://cubbi.es/oembed")
 
+OEmbedDailyMotion = OEmbed::Provider.new("http://www.dailymotion.com/services/oembed")
+OEmbedDailyMotion << "http://www.dailymotion.com/video/*"
+
+OEmbedTwitter = OEmbed::Provider.new("https://api.twitter.com/1/statuses/oembed.json")
+OEmbedTwitter << "http://twitter.com/*/status/*"
+OEmbedTwitter << "https://twitter.com/*/status/*"
+
 # patch in support for new https soundcloud
 OEmbed::Providers::SoundCloud << "https://*.soundcloud.com/*"
 
@@ -18,7 +25,9 @@ oembed_provider_list = [
   OEmbed::Providers::SoundCloud,
   OEmbed::Providers::Instagram,
   OEmbed::Providers::Flickr,
-  OEmbedCubbies
+  OEmbedCubbies,
+  OEmbedDailyMotion,
+  OEmbedTwitter
 ]
 
 SECURE_ENDPOINTS = oembed_provider_list.map do |provider|
