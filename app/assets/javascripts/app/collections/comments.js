12d4523fd04e446cb15f2b81ae3881f77bc33657
Jishnu Mohan <jishnu7@gmail.com>
2013-02-27 03:04:50 +0530

    Fixes comments showing after fail posting
    Fixes  Issue #4005

--- app/assets/javascripts/app/collections/comments.js
+++ app/assets/javascripts/app/collections/comments.js
@@ -13,11 +13,10 @@ app.collections.Comments = Backbone.Collection.extend({
     var self = this
 
     var comment = new app.models.Comment({text: text })
-      , deferred = comment.save({}, {url : self.url()})
-
-    comment.set({author: app.currentUser.toJSON(), parent: self.post })
-
-    this.add(comment)
+      , deferred = comment.save({}, {url : self.url()}).done(function() {
+        comment.set({author: app.currentUser.toJSON(), parent: self.post })
+        self.add(comment)
+      });
 
     return deferred
   }
