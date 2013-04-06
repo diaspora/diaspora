diaspora/diaspora

commit ffc6c1102efdc5fa7d320a306d168d7feb3c191f

dry up and modularize layouts

app/views/status_messages/bookmarklet.html.haml
@@ -35,5 +35,7 @@
 
 - content_for(:head) do
   :css
+    body { padding:0; margin-top: 0; }
+    .container { width: auto; }
     #new_status_message_pane { margin: 1em 0 0; }
     #new_status_message_pane h4 { margin: 0 0 0 1em; }

app/controllers/application_controller.rb
@@ -22,6 +22,8 @@ class ApplicationController < ActionController::Base
                 :tags,
                 :open_publisher
 
+  layout ->(c) { request.format == :mobile ? "application" : "centered_with_header_with_footer" }
+
   private
 
   def ensure_http_referer_is_set

app/controllers/home_controller.rb
@@ -10,16 +10,17 @@ class HomeController < ApplicationController
     elsif is_mobile_device?
       if partial_dir.join('_show.mobile.haml').exist? ||
          partial_dir.join('_show.mobile.erb').exist?
-        render :show, layout: 'post'
+        render :show
       else
         redirect_to user_session_path
       end
     elsif partial_dir.join("_show.html.haml").exist? ||
           partial_dir.join("_show.html.erb").exist?
-      render :show, layout: 'post'
+      render :show
     else
-        render file: Rails.root.join("public", "default.html"),
-               layout: 'post'
+      @css_framework = :bootstrap # Hack, port site to one framework
+      render file: Rails.root.join("public", "default.html"),
+             layout: 'application'
     end
   end
 

app/controllers/posts_controller.rb
@@ -11,7 +11,8 @@ class PostsController < ApplicationController
   before_filter :set_format_if_malformed_from_status_net, :only => :show
   before_filter :find_post, :only => [:show, :next, :previous, :interactions]
 
-  layout 'post'
+  layout 'application'
+  before_filter -> { @css_framework = :bootstrap }
 
   respond_to :html,
              :mobile,
@@ -20,7 +21,7 @@ class PostsController < ApplicationController
 
   rescue_from Diaspora::NonPublic do |exception|
     respond_to do |format|
-      format.all { render :template=>'errors/not_public', :status=>404 }
+      format.all { @css_framework = :bootstrap; render :template=>'errors/not_public', :status=>404, :layout => "application"}
     end
   end
 
@@ -30,7 +31,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html{ gon.post = PostPresenter.new(@post, current_user); render 'posts/show' }
       format.xml{ render :xml => @post.to_diaspora_xml }
-      format.mobile{render 'posts/show', :layout => "application"}
+      format.mobile{render 'posts/show' }
       format.json{ render :json => PostPresenter.new(@post, current_user) }
     end
   end

app/controllers/registrations_controller.rb
@@ -5,7 +5,8 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :check_registrations_open_or_vaild_invite!, :check_valid_invite!
 
-  layout "post", :only => :new
+  layout "application", :only => [:new]
+  before_filter -> { @css_framework = :bootstrap }, only: [:new]
 
   def create
     @user = User.build(params[:user])

app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@
 
 class SessionsController < Devise::SessionsController
 
-  layout "post", :only => [:new]
+  layout "application", :only => [:new]
+  before_filter -> { @css_framework = :bootstrap }, only: [:new]
 
 end

app/controllers/status_messages_controller.rb
@@ -11,7 +11,7 @@ class StatusMessagesController < ApplicationController
              :mobile,
              :json
 
-  layout :bookmarklet_layout, :only => :bookmarklet
+  layout 'application', only: :bookmarklet
 
   # Called when a user clicks "Mention" on a profile page
   # @param person_id [Integer] The id of the person to be mentioned
@@ -113,15 +113,5 @@ class StatusMessagesController < ApplicationController
 
   def remove_getting_started
     current_user.disable_getting_started
-  end
-
-  # Define bookmarklet layout depending on whether
-  # user is in mobile or desktop mode
-  def bookmarklet_layout
-    if request.format == :mobile
-      'application'
-    else
-      'blank'
-    end
   end
 end

app/controllers/streams_controller.rb
@@ -64,8 +64,8 @@ class StreamsController < ApplicationController
     end
 
     respond_with do |format|
-      format.html { render 'layouts/main_stream' }
-      format.mobile { render 'layouts/main_stream' }
+      format.html { render 'streams/main_stream' }
+      format.mobile { render 'streams/main_stream' }
       format.json { render :json => @stream.stream_posts.map {|p| LastThreeCommentsDecorator.new(PostPresenter.new(p, current_user)) }}
     end
   end

commit 9c0c5a725819f2a020df025eda082dc5143acc51

Include missing javascript in bookmarklet, regression introduced in commit ffc6c1102efdc5fa7d320a306d168d7feb3c191f. Fixes #4057

app/views/status_messages/bookmarklet.html.haml
@@ -2,6 +2,8 @@
 -#   licensed under the Affero General Public License version 3 or later.  See
 -#   the COPYRIGHT file.
 
+= javascript_include_tag :home
+
 #new_status_message_pane
   .span-15.last
     %h4

spec/javascripts/bookmarklet-spec.js
@@ -37,6 +37,16 @@ describe("bookmarklet", function() {
       });
     });
   });
+  
+  describe("modified prefilled bookmarklet", function(){
+    it('allows changing of post content', function(){
+      spec.loadFixture('prefilled_bookmarklet');
+      $('div.mentions > div').html('Foo Bar');
+      _.defer(function() {
+        expect($("#publisher #status_message_text").val()).toEqual("Foo Bar");
+      });
+    });
+  });
 
 
 