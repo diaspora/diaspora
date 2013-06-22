e1ca8df15e23aed5f138d31bcacc0a3cc9044d52
twain <jakolo13@web.de>
2013-06-22 15:07:42 +0200

    added strip_markdown to notification mails

--- app/mailers/notification_mailers/comment_on_post.rb
+++ app/mailers/notification_mailers/comment_on_post.rb
@@ -1,6 +1,7 @@
 module NotificationMailers
   class CommentOnPost < NotificationMailers::Base
     include ActionView::Helpers::TextHelper
+    include MarkdownifyHelper
 
     attr_accessor :comment
 
@@ -8,7 +9,7 @@ module NotificationMailers
       @comment = Comment.find(comment_id)
 
       @headers[:from] = "\"#{@comment.author_name} (Diaspora*)\" <#{AppConfig.mail.sender_address}>"
-      @headers[:subject] = truncate(@comment.comment_email_subject, :length => TRUNCATION_LEN)
+      @headers[:subject] = truncate(strip_markdown(@comment.comment_email_subject.squish), :length => TRUNCATION_LEN)
       @headers[:subject] = "Re: #{@headers[:subject]}"
     end
   end
