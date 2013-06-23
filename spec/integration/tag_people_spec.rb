729d79e2a4745aff87fe22ec52a7e8f12e59f029
Fabian Fiorotto <fabian_1945@yahoo.com.ar>
2013-06-22 23:10:56 -0300

    test for people pagination patch

---
+++ spec/integration/tag_people_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe TagsController, type: :controller do
+  describe 'will_paginate people on the tag page' do
+    let(:people) { (1..16).map { FactoryGirl.create(:person) } }
+    let(:tag)    { "diaspora" }
+
+    before do
+      Person.should_receive(:profile_tagged_with).with(/#{tag}/).twice.and_return(people)
+    end
+
+    it 'paginates the people set' do
+      get "/tags/#{tag}"
+
+      expect(response.status).to eq(200)
+      response.body.should match(/div class="pagination"/)
+      response.body.should match(/href="\/tags\/#{tag}\?page=2"/)
+    end
+
+    it 'fetches the second page' do
+      get "/tags/#{tag}", page: 2
+
+      expect(response.status).to eq(200)
+      response.body.should match(/<em class="current">2<\/em>/)
+    end
+  end
+end
