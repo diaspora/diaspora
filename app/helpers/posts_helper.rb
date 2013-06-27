5e59ecb8739838c6847bce717f50d0553a192405
Damien <git@monetd.fr>
2013-06-27 18:49:13 +0200

    clarify regexp and correct some bad backtracking

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,13 +10,14 @@ module PostsHelper
       I18n.t "posts.show.reshare_by", :author => post.author_name
     else
       if post.text.present?
-        title = post.text(:plain_text => true).match(/^((([^\n\r]*)(\r?\n)(-+|=+)(\r?\n|$))|(\#{1,6}[[:space:]]+([^\n\r]+?)\#*(\r?\n|$)))/)
-        unless title.nil?
-          unless title[3].nil?
-            return title[3]
-          else
-            return title[8]
-          end
+        reg = /\A(?:  # Regexp to match a Markdown header present on first line and retrieve its content for :
+              (?<setext_content>.{1,80})\n(?:={1,80}|-{1,80})(?:\r?\n|$)              # Setext-style header
+                  |                                                                   # or
+              \#{1,6}[[:blank:]](?<atx_content>.{1,80})(?<![\#\r])                    # Atx-style header
+               )/x    # 80 is the max size of title
+        if reg =~ post.text(:plain_text => true)
+          return $~[:setext_content].strip unless $~[:setext_content].nil?
+          return $~[:atx_content].strip unless $~[:atx_content].nil?
         end
         truncate(post.text(:plain_text => true), :length => opts.fetch(:length, 20))
       elsif post.respond_to?(:photos) && post.photos.present?
@@ -27,7 +28,7 @@ module PostsHelper
 
   def post_iframe_url(post_id, opts={})
     opts[:width] ||= 516
-    opts[:height] ||= 315 
+    opts[:height] ||= 315
     host = AppConfig.pod_uri.authority
    "<iframe src='#{Rails.application.routes.url_helpers.post_url(post_id, :host => host)}' width='#{opts[:width]}px' height='#{opts[:height]}px' frameBorder='0'></iframe>".html_safe
   end
