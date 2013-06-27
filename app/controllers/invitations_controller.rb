686d3baaad0316f98fb71b2b981021efff9514d8
James Fleming <jflemingprod@gmail.com>
2013-06-27 18:47:33 +0200

    Strong parameters for InvitationsController.
    Make InvitationsController#create spec pass

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -50,7 +50,8 @@ class InvitationsController < ApplicationController
   end
 
   def create
-    emails = params[:email_inviter][:emails].split(',').map(&:strip).uniq
+    inviter_params = params.require(:email_inviter).permit(:message, :locale, :emails)
+    emails = inviter_params[:emails].split(',').map(&:strip).uniq
 
     valid_emails, invalid_emails = emails.partition { |email| valid_email?(email) }
 
@@ -60,8 +61,7 @@ class InvitationsController < ApplicationController
     unless valid_emails.empty?
       Workers::Mail::InviteEmail.perform_async(valid_emails.join(','),
                                                current_user.id,
-                                               params[:email_inviter])
-
+                                               inviter_params)
     end
 
     if emails.empty?
