e1239ee195794bb19663e8faa52782f015387dc8
Damien <git@monetd.fr>
2013-07-10 22:29:35 +0200

    rewrite regex

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,16 +10,18 @@ module PostsHelper
       I18n.t "posts.show.reshare_by", :author => post.author_name
     else
       if post.text.present?
-        reg = /\A(?:  # Regexp to match a Markdown header present on first line and retrieve its content for :
-              (?<setext_content>.{1,80})\n(?:={1,80}|-{1,80})(?:\r?\n|$)              # Setext-style header
+        if opts.has_key?(:length)
+           truncate(post.text(:plain_text => true), :length => opts.fetch(:length))
+        elsif /\A(?:  # Regexp to match a Markdown header present on first line :
+              (?<setext_content>.{1,200}\n(?:={1,200}|-{1,200}))(?:\r?\n|$)           # Setext-style header
                   |                                                                   # or
-              \#{1,6}[[:blank:]](?<atx_content>.{1,80})(?<![\#\r])                    # Atx-style header
-               )/x    # 80 is the max size of title
-        if reg =~ post.text(:plain_text => true)
-          return $~[:setext_content].strip unless $~[:setext_content].nil?
-          return $~[:atx_content].strip unless $~[:atx_content].nil?
+              (?<atx_content>\#{1,6}\s.{1,200})(?:\r?\n|$)                            # Atx-style header
+               )/x   =~  post.text(:plain_text => true)
+          return setext_content unless setext_content.nil?
+          return atx_content unless atx_content.nil?
+        else
+          truncate(post.text(:plain_text => true), :length => 20 )
         end
-        truncate(post.text(:plain_text => true), :length => opts.fetch(:length, 20))
       elsif post.respond_to?(:photos) && post.photos.present?
         I18n.t "posts.show.photos_by", :count => post.photos.size, :author => post.author_name
       end
