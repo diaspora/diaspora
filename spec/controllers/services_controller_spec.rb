55a58713a50fa0d9ea98db2d18bf3ea6f523bf6f
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-08-02 19:01:20 -0500

    Issue #4124 Check write access before authorizing Twitter

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -5,61 +5,78 @@
 require 'spec_helper'
 
 describe ServicesController do
-  let(:mock_access_token) { Object.new }
-
-  let(:omniauth_auth) {
+  let(:omniauth_auth) do
     { 'provider' => 'twitter',
       'uid'      => '2',
       'info'   => { 'nickname' => 'grimmin' },
-      'credentials' => { 'token' => 'tokin', 'secret' =>"not_so_much" }
-      }
-  }
+      'credentials' => { 'token' => 'tokin', 'secret' =>"not_so_much" }}
+    end
+  let(:user) { alice }
 
   before do
-    @user   = alice
-    sign_in :user, @user
-    @controller.stub!(:current_user).and_return(@user)
-    mock_access_token.stub!(:token => "12345", :secret => "56789")
+    sign_in :user, user
+    @controller.stub!(:current_user).and_return(user)
   end
 
   describe '#index' do
-    it 'displays all connected serivices for a user' do
-      4.times do
-        FactoryGirl.create(:service, :user => @user)
-      end
+    before do
+      FactoryGirl.create(:service, user: user)
+    end
 
+    it "displays user's connected services" do
       get :index
-      assigns[:services].should == @user.services
+      assigns[:services].should == user.services
     end
   end
 
   describe '#create' do
-    context 'when not fetching a photo' do
-      before do
-        request.env['omniauth.auth'] = omniauth_auth
-      end
+    before do
+      request.env['omniauth.auth'] = omniauth_auth
+      request.env['omniauth.origin'] = root_url
+    end
 
-      it 'creates a new OmniauthService' do
+    it 'creates a new service and associates it with the current user' do
+      expect {
+        post :create, :provider => 'twitter'
+      }.to change(user.services, :count).by(1)
+    end
+
+    it 'saves the provider' do
+      post :create, :provider => 'twitter'
+      user.reload.services.first.class.name.should == "Services::Twitter"
+    end
+
+    context 'when service exists with the same uid' do
+      before { Services::Twitter.create!(uid: omniauth_auth['uid'], user_id: user.id) }
+
+      it 'doesnt create a new service' do
         expect {
-          post :create, :provider => 'twitter'
-        }.to change(@user.services, :count).by(1)
+        post :create, :provider => 'twitter'
+      }.to_not change(Service, :count).by(1)
       end
 
-      it 'creates a twitter service' do
-        Service.delete_all
-        @user.getting_started = false
+      it 'flashes an already_authorized error with the diaspora handle for the user'  do
         post :create, :provider => 'twitter'
-        @user.reload.services.first.class.name.should == "Services::Twitter"
+        flash[:error].include?(user.profile.diaspora_handle).should be_true
+        flash[:error].include?( 'already authorized' ).should be_true
+      end
+    end
+
+    context 'when the access-level is read-only' do
+      before do 
+        access_level_hash = { 'extra' => { 'access_token' => { 'response' => { 'header' => { 'x_access_level' => 'read' }}}}}
+        request.env['omniauth.auth'] = omniauth_auth["info"].merge!( access_level_hash )
       end
 
-      it 'returns error if the user already a service with that uid' do
-        Services::Twitter.create!(:nickname => omniauth_auth["info"]['nickname'],
-                                  :access_token => omniauth_auth['credentials']['token'],
-                                  :access_secret => omniauth_auth['credentials']['secret'],
-                                  :uid => omniauth_auth['uid'],
-                                  :user => bob)
+      it 'doesnt create a new service' do
+        expect {
         post :create, :provider => 'twitter'
-        flash[:error].include?(bob.person.profile.diaspora_handle).should be_true
+      }.to_not change(Service, :count).by(1)
+      end
+
+      it 'flashes an read-only access error'  do
+        post :create, :provider => 'twitter'
+        flash[:error].include?( 'Access level is read-only' ).should be_true
       end
     end
 
@@ -72,9 +89,7 @@ describe ServicesController do
       end
 
       it 'does not queue a job if the profile photo is set' do
-        profile = @user.person.profile
-        profile[:image_url] = "/non/default/image.jpg"
-        profile.save
+        @controller.stub!(:no_profile_image?).and_return false
 
         Workers::FetchProfilePhoto.should_not_receive(:perform_async)
 
@@ -82,11 +97,9 @@ describe ServicesController do
       end
 
       it 'queues a job to save user photo if the photo does not exist' do
-        profile = @user.person.profile
-        profile[:image_url] = nil
-        profile.save
+        @controller.stub!(:no_profile_image?).and_return true
 
-        Workers::FetchProfilePhoto.should_receive(:perform_async).with(@user.id, anything(), "https://service.com/fallback_lowres.jpg")
+        Workers::FetchProfilePhoto.should_receive(:perform_async).with(user.id, anything(), "https://service.com/fallback_lowres.jpg")
 
         post :create, :provider => 'twitter'
       end
@@ -95,13 +108,13 @@ describe ServicesController do
 
   describe '#destroy' do
     before do
-      @service1 = FactoryGirl.create(:service, :user => @user)
+      @service1 = FactoryGirl.create(:service, :user => user)
     end
 
     it 'destroys a service selected by id' do
       lambda{
         delete :destroy, :id => @service1.id
-      }.should change(@user.services, :count).by(-1)
+      }.should change(user.services, :count).by(-1)
     end
   end
 end
