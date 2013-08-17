45f8f489b60c7517729ef15acfb5198b7a3c4412
Jonne Haß <me@mrzyx.de>
2013-08-18 00:30:29 +0200

    improve new SPV
    * Hopefully fix OEmbed view
    * Fix OpenGraph view
    * Fix middle border
    * Add spacing at the bottom
    * Fix comment styles by not including old SPV styles for them (TODO: maybe rescue some stuff there, like the code tag styling)
    * Fix avatar size for default avatar
    * Remove border below comment pane
    * Fix overflow of post body with a lot of text
    * Fix stylesheet inclusion

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -56,8 +56,7 @@ module LayoutHelper
 
   def include_base_css_framework(use_bootstrap=false)
     if use_bootstrap || @aspect == :getting_started
-      stylesheet_link_tag('bootstrap-complete') +
-      stylesheet_link_tag('bootstrap-headerfix')
+      stylesheet_link_tag('bootstrap-complete')
     else
       stylesheet_link_tag 'blueprint', :media => 'screen'
     end
