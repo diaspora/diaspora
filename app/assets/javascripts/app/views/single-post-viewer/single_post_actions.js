867efeb80f1f8c02773a6b8266adca4ea3b172fd
Roger Braun <roger@rogerbraun.net>
2013-08-18 18:16:39 +0200

    Add comment focus button.

--- app/assets/javascripts/app/views/single-post-viewer/single_post_actions.js
+++ app/assets/javascripts/app/views/single-post-viewer/single_post_actions.js
@@ -1,3 +1,16 @@
 app.views.SinglePostActions = app.views.Feedback.extend({
   templateName: "single-post-viewer/single-post-actions",
+
+  events: function() {
+    return _.defaults({
+      "click .focus-comment" : "focusComment"
+    }, app.views.Feedback.prototype.events);
+  },
+
+  focusComment: function() {
+    $('.comment_box').focus();
+    $("html, body").animate({ scrollTop: $(document).height()-$(window).height() }); // Go to the bottom.
+    return false;
+  }
+
 });
