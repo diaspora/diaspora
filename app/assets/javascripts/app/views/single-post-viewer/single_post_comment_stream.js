0c7d2b592d5f4d8251af93c33d9c3de219e7cad6
Roger Braun <roger@rogerbraun.net>
2013-08-18 17:14:49 +0200

    Don't collapse comment content in the SPV.

--- app/assets/javascripts/app/views/single-post-viewer/single_post_comment_stream.js
+++ app/assets/javascripts/app/views/single-post-viewer/single_post_comment_stream.js
@@ -5,6 +5,16 @@ app.views.SinglePostCommentStream = app.views.CommentStream.extend({
     this.$(".new_comment_form_wrapper").removeClass('hidden')
   },
 
+  appendComment: function(comment) {
+    // Set the post as the comment's parent, so we can check
+    // on post ownership in the Comment view.
+    comment.set({parent : this.model.toJSON()})
+
+    this.$(".comments").append(new app.views.ExpandedComment({
+      model: comment
+    }).render().el);
+  },
+
   presenter: function(){
     return _.extend(this.defaultPresenter(), {
       moreCommentsCount : 0,
