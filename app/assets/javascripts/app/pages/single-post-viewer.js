0932a0fffc6f26e94b6e0704ba66052e6d88a147
Roger Braun <roger@rogerbraun.net>
2013-08-18 11:01:05 +0200

    Implement a way to deactivate hovercards.

--- app/assets/javascripts/app/pages/single-post-viewer.js
+++ app/assets/javascripts/app/pages/single-post-viewer.js
@@ -33,7 +33,8 @@ app.pages.SinglePostViewer = app.views.Base.extend({
       // formats title to html...
       var html_title = app.helpers.textFormatter(this.model.get("title"), this.model);
       //... and converts html to plain text
-      document.title = $('<div>').html(html_title).text(); 
+      document.title = $('<div>').html(html_title).text();
+      app.hovercard.deactivate() // No hovercards for now.
     }
   },
 
