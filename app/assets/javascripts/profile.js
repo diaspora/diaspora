a3f70c152161ae30da1034ce4c4a584471662507
sjain1107 <sakshijain1107@gmail.com>
2013-08-26 20:47:43 +0530

    Add ignore user icon, added jquery

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,12 +3,15 @@
  *   the COPYRIGHT file.
  */
 //= require jquery.autoSuggest.custom
-       
+
 $(document).ready(function() {
   $("#block_user_button").click(function(evt) {
     if(!confirm(Diaspora.I18n.t('ignore_user'))) { return }
       var personId = $(this).data('person-id');
       var block = new app.models.Block();
+      block.save({block : {person_id : personId}});
+      $('#profile_buttons').attr('class', 'blocked');
+      
       return false;
-  }); 
-});              
+   });
+});
