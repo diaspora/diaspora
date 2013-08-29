890aa09c20ad8985b179e9d0a2b03a727be9a13a
sjain1107 <sakshijain1107@gmail.com>
2013-08-29 16:54:24 +0530

    Changed profile.js

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -5,14 +5,4 @@
 //= require jquery.autoSuggest.custom
 
 $(document).ready(function() {
-  $("#block_user_button").click(function(evt) {
-    if(!confirm(Diaspora.I18n.t('ignore_user'))) { return }
-      var personId = $(this).data('person-id');
-      var block = new app.models.Block();
-      block.save({block : {person_id : personId}});
-      $('#profile_buttons').attr('class', 'blocked');
-      $('#sharing_message').attr('class', 'icons-circle');
-      $('.profile_button, .white_bar').remove();
-      return false;
-   });
 });
