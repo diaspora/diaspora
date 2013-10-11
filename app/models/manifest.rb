39c79f18beaf319ea9d0fec3988df9dd38887f06
akilai <iroshan.akila@gmail.com>
2013-10-11 13:23:10 +0530

    Added verify and sign methods to manifest model. These methods will be used to sign and verify contents of a manifest using a users key pair.

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -15,12 +15,14 @@ class Manifest < ActiveRecord::Base
                   :scopes
 
   before_validation :generate_app_id, :on => :create
+  before_validation :sign, :on => :create
 
   validates :app_name, presence: true
   validates :app_description, length: { maximum: 500 }
   validates :app_id, presence: true
   validates :callback_url, presence: true, format: URI::regexp(%w(http https))
   validates :redirect_url, presence: true, format: URI::regexp(%w(http https))
+  validates :signed_jwt, presence: true
 
   def manifest_hash
     {
@@ -34,14 +36,25 @@ class Manifest < ActiveRecord::Base
       },
       :callback_url => self.callback_url,
       :redirect_url => self.redirect_url,
-      :access => self.scopes
+      :access => self.scopes,
+      :signed_jwt => self.signed_jwt
     }
   end
 
   def create_manifest_json
     manifest_hash = self.manifest_hash
-    manifest_hash[:signed_jwt] = self.signed_jwt
     manifest_hash.to_json
+  end
+    
+  def sign
+    private_key = self.dev.serialized_private_key
+    self.signed_jwt = JWT.encode(manifest_hash, OpenSSL::PKey::RSA.new(private_key), "RS256")
+  end
+
+  def verify
+    developer_id = self.dev.diaspora_handle
+    person = Webfinger.new(developer_id).fetch
+    JWT.decode(self.signed_jwt, person.public_key)
   end
 
   private
