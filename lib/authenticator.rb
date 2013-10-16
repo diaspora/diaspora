c628e55f7f109a15ee9214a8d311dc9d9d5f3125
akilai <iroshan.akila@gmail.com>
2013-10-16 21:00:58 +0530

    Added send_refresh_token_to_app method to Authenticator lib.

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -2,4 +2,8 @@ module Authenticator
   def send_error_to_app(callback_url, error_code)
     Workers::PostToApp.perform_async(callback_url, {:error => error_code})
   end
+
+  def send_refresh_token_to_app(callback_url, refresh_token, diaspora_handle)
+    Workers::PostToApp.perform_async(callback_url, {:refresh_token => refresh_token, :diaspora_handle => diaspora_handle})
+  end
 end
