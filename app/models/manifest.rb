f969998727403d49800e247d94fe4c92d0c7fe7b
Aruna Herath <arunabherath@gmail.com>
2013-10-19 14:39:50 +0530

    Added method manifest_hash which can be used to take the information of a manifest in the form of a hash.
    temp

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -22,6 +22,28 @@ class Manifest < ActiveRecord::Base
   validates :callback_url, presence: true, format: URI::regexp(%w(http https))
   validates :redirect_url, presence: true, format: URI::regexp(%w(http https))
 
+  def manifest_hash
+    {
+      :dev_handle => self.dev.diaspora_handle,
+      :manifest_version => "1.0",
+      :app_details => {
+        :name => self.app_name,
+        :id => self.app_id,
+        :description => self.app_description,
+        :version => self.app_version
+      },
+      :callback_url => self.callback_url,
+      :redirect_url => self.redirect_url,
+      :access => self.scopes
+    }
+  end
+
+  def create_manifest_json
+    manifest_hash = self.manifest_hash
+    manifest_hash[:signed_jwt] = self.signed_jwt
+    manifest_hash.to_json
+  end
+
   private
 
   def generate_app_id
--- spec/models/manifest_spec.rb
+++ spec/models/manifest_spec.rb
@@ -47,5 +47,24 @@ describe Manifest do
         FactoryGirl.build(:manifest, redirect_url: "not a valid url").should_not be_valid
       end
     end
+    
+    describe "of manifest_hash" do
+      it "should return a hash correctly describing the manifest" do
+        dev = FactoryGirl.create(:user)
+        dev.person = FactoryGirl.create(:person, diaspora_handle: "test@pod.com")
+        manifest = FactoryGirl.create(:manifest, :dev => dev)
+
+        expected_hash = {
+          :dev_handle => "test@pod.com",          
+          :manifest_version => "1.0",
+          :app_details => { :name=>"cool birds", :id=> manifest.app_id, :description=>"Birds in this app are cool!", :version=>"1" },
+          :callback_url => "http://coolbirds.com/diasporaapp",
+          :redirect_url => "http://coolbirds.com/welcome",
+          :access => ["post_write", "contact_list_read"]
+        }
+
+        manifest.manifest_hash.should == expected_hash
+      end
+    end
   end
 end
