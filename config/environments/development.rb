d9c987e9d2892d851ee02a3980f27f5830951793
maruf <maruf@leopard>
2013-12-13 21:48:27 +0600

    Asset precompiling is false for development mode. It will take less time to load page during development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ Diaspora::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
