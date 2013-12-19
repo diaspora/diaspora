8509acd370219eff3fda69fd4bbdfba13a584846
maruf <maruf@leopard>
2013-12-19 22:48:25 +0600

    change reverted. change was not related with the context of this branch.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ Diaspora::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
