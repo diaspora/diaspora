29a98ddc29f55beb8dd3fd8a23b74230c03c6e4d
goobertron <gooberfox@fastmail.co.uk>
2014-01-13 18:47:47 +0000

    Remove duplicate test steps

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -6,16 +6,3 @@ And /^I mark myself as safe for work$/ do
   uncheck('profile[nsfw]')
 end
 
-And /^the "profile[nsfw]" checkbox should be checked$/ do |label, selector|
-  with_scope(selector) do
-    field_checked = find_field(label)['checked']
-	field_checked.should be_true
-  end
-end
-
-And /^the "profile[nsfw]" checkbox should not be checked$/ do |label, selector|
-  with_scope(selector) do
-    field_checked = find_field(label)['checked']
-	field_checked.should be_false
-  end
-end
