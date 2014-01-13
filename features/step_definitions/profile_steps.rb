706b11b14df42302c666560c9c634bda72f674de
goobertron <gooberfox@fastmail.co.uk>
2014-01-13 18:14:43 +0000

    Now with passing Cucumber tests

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -5,3 +5,17 @@ end
 And /^I mark myself as safe for work$/ do
   uncheck('profile[nsfw]')
 end
+
+And /^the "profile[nsfw]" checkbox should be checked$/ do |label, selector|
+  with_scope(selector) do
+    field_checked = find_field(label)['checked']
+	field_checked.should be_true
+  end
+end
+
+And /^the "profile[nsfw]" checkbox should not be checked$/ do |label, selector|
+  with_scope(selector) do
+    field_checked = find_field(label)['checked']
+	field_checked.should be_false
+  end
+end
