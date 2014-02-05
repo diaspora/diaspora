3dddf67568e73edc0180052138a4c56b5848de58
geirkairam <krieg.maria@googlemail.com>
2014-02-05 23:30:45 +0100

    Issue #4661: moved html in test to fixture file

--- spec/javascripts/app/views/hovercard_view_spec.js
+++ spec/javascripts/app/views/hovercard_view_spec.js
@@ -1,34 +1,10 @@
 describe("app.views.Hovercard", function(){
   beforeEach(function(){
-    var html = '<a href="/people/1402141b454f7fc8" class="author-name hovercardable">'+
-      '<div id="hovercard_container" style=" display: none;">'+
-        '<div id="hovercard">'+
-          '<img class="avatar" src="https://localhost/image.jpg">'+
-          '<h4>'+
-            '<span class="person" href="/people/1402141b454f7fc8">Amalthea</span>'+ // can not be an <a> don#t know why
-          '</h4>'+
-          '<p class="handle">amaletha@localhost</p>'+
-          '<div id="hovercard_dropdown_container"></div>'+
-          '<div class="hovercard_footer">'+
-            '<div class="footer_container">'+
-              '<div class="hashtags"></div>'+
-            '</div>'+
-          '</div>'+
-        '</div>'+      
-      '</div>'+
-      '<img src="https://localhost/image.jpg" class="avatar small" data-original-title="Amalthea">'+
-      '<div class="tooltip fade top in" style="top: 56px; left: 890px; display: block;">'+
-        '<div class="tooltip-arrow"></div>'+
-        '<div class="tooltip-inner">Amalthea</div>'+
-      '</div>'+
-    '</a>';
-    $('body').append(html);
+    loadFixtures('/spec/fixtures/link_with_hovercard.html'); 
     this.view = new app.views.Hovercard();
   });
 
   afterEach(function() {
-    $('.hovercardable').empty();
-    $('.hovercardable').remove();  
   });
 
   describe("._positionHovercard", function(){
@@ -42,7 +18,6 @@ describe("app.views.Hovercard", function(){
       this.view._positionHovercard();
       expect($(this.view.el).css("left")).toBe($(this.view.el).parent().position().left-$(this.view.el).width()+'px');
     })
-
   })
 
 })
