0c15aac1e227bb0fa156d517ef7a7cd6709f3173
geirkairam <krieg.maria@googlemail.com>
2014-02-08 00:01:25 +0100

    Issue 4706: moved n/p hotkey handling to comment_stream_view.js

--- app/assets/javascripts/app/views/stream/shortcuts.js
+++ app/assets/javascripts/app/views/stream/shortcuts.js
@@ -9,8 +9,6 @@ app.views.StreamShortcuts = {
 
     this.on('hotkey:gotoNext', this.gotoNext, this);
     this.on('hotkey:gotoPrev', this.gotoPrev, this);
-    this.on('hotkey:gotoNextComment', this.gotoNextComment, this);
-    this.on('hotkey:gotoPrevComment', this.gotoPrevComment, this);
     this.on('hotkey:likeSelected', this.likeSelected, this);
     this.on('hotkey:commentSelected', this.commentSelected, this);
   },
@@ -21,7 +19,7 @@ app.views.StreamShortcuts = {
     if(jQuery.inArray(event.target.type, textAcceptingInputTypes) > -1){
       return;
     }
-		
+
     // trigger the events based on what key was pressed
     switch (String.fromCharCode( event.which ).toLowerCase()) {
       case "j":
@@ -29,12 +27,6 @@ app.views.StreamShortcuts = {
         break;
       case "k":
         this.trigger('hotkey:gotoPrev');
-        break;
-      case "n":
-        this.trigger('hotkey:gotoNextComment');
-        break;
-      case "p":
-        this.trigger('hotkey:gotoPrevComment');
         break;
       default:
     }
@@ -92,61 +84,7 @@ app.views.StreamShortcuts = {
       this.selectPost(stream_elements[0]);
     }
   },
-
-  _getSelectedPost: function() {
-     return this.$('div.stream_element.loaded.shortcut_selected');
-  },
-
-  _getComments: function(post) {
-    return post.find('div.comment_stream > div.comments > div.comment');
-  },
-
-  _getSelectedCommentIndex: function(comments) {
-    var selectedCommentIndex = -1;
-    comments.each(function(index, element) {
-      if ($(element).hasClass('shortcut_selected')) {
-        selectedCommentIndex = index;
-      }
-    });
-    return selectedCommentIndex;
-  },
-
-  gotoNextComment: function() {
-    //TODO index > size expand hidden comments prev comment
-    var selectedPost = this._getSelectedPost();
-    if (selectedPost.length > 0) {
-      var comments = this._getComments(selectedPost);
-      var selectedCommentIndex = this._getSelectedCommentIndex(comments);
-      if (selectedCommentIndex < 0) {
-        this.selectComment(comments.get(0));
-      } else if (selectedCommentIndex == comments.length-1) {
-        // do nothing, maybe next comment in next post?
-      } else {
-        this.selectComment(comments.get(selectedCommentIndex + 1));
-      }
-    } else {
-      //no post selected
-    }
-  },
-
-  gotoPrevComment: function() {
-    //TODO index > size expand hidden comments prev comment
-    var selectedPost = this._getSelectedPost();
-    if (selectedPost.length > 0) {
-      var comments = this._getComments(selectedPost);
-      var selectedCommentIndex = this._getSelectedCommentIndex(comments);
-      if (selectedCommentIndex < 0) {
-        this.selectComment(comments.get(0));
-      } else if (selectedCommentIndex < 1) {
-        // do nothing, mybe select comment in previous post?
-      } else {
-        this.selectComment(comments.get(selectedCommentIndex -1));
-      }
-    } else {
-      //no post selected
-    }
-  },
-
+    
   commentSelected: function() {
     $('a.focus_comment_textarea',this.$('div.stream_element.loaded.shortcut_selected')).click();
   },    
@@ -155,28 +93,14 @@ app.views.StreamShortcuts = {
     $('a.like:first',this.$('div.stream_element.loaded.shortcut_selected')).click();
   },
     
-  selectPost: function(element) {
-    this._deselectComments();
+  selectPost: function(element){
     //remove the selection and selected-class from all posts
     var selected=this.$('div.stream_element.loaded.shortcut_selected');
     selected.removeClass('shortcut_selected').removeClass('highlighted');
     //move to new post
     window.scrollTo(window.pageXOffset, element.offsetTop-this._headerSize);
     //add the selection and selected-class to new post
-    element.className+=" shortcut_selected highlighted";	
-  },
-
-  _deselectComments: function() {
-    //remove the selection and selected-class from all comments
-    var selected=this.$('div.comment.shortcut_selected');
-    selected.removeClass('shortcut_selected').removeClass('highlighted');
-  },
-
-  selectComment: function(element) {
-    this._deselectComments();
-    //move to new post
-    window.scrollTo(window.pageXOffset, element.offsetTop-this._headerSize);
-    //add the selection and selected-class to new post
     element.className+=" shortcut_selected highlighted";
-  }
+    $(element).attr('tabindex','0').focus();
+  },
 };
