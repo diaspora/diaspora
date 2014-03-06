870bb539314a55fb950b452044a39fa82dcee8b4
flaburgan <flaburgan@geexxx.fr>
2014-03-06 23:32:12 +0100

    Now interpret markdown in email notifications

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -5,7 +5,7 @@ module NotifierHelper
   # @return [String] The formatted post.
   def post_message(post, opts={})
     if post.respond_to? :formatted_message
-      message = strip_markdown(post.formatted_message(:plain_text => true))
+      message = post.formatted_message
       message = process_newlines(message) if opts[:process_newlines]
       message.html_safe
     else
@@ -17,8 +17,8 @@ module NotifierHelper
   # @param opts [Hash] Optional hash. Accepts the :process_newlines parameter.
   # @return [String] The formatted comment.
   def comment_message(comment, opts={})
-    text = strip_markdown(comment.text)
+    text = comment.text
     text = process_newlines(text) if opts[:process_newlines]
-    text
+    text.html_safe
   end
 end
