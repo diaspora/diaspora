1b70f6fe093f36054f31edba42469f8ad52d84d4
Jonne Haß <me@mrzyx.de>
2014-03-15 20:08:39 +0100

    Replace .rvmrc by .ruby-version
    Also relax the requirement. The .ruby-version represents
    merely a recomendation for the Ruby version. The incompability
    issues in the Ruby world are far less currently, than they were
    when we introduced strict version enforcement. Thus strict
    enforcement is no longer appropiate. Same holds for RubyGems.
    
    This implicity also removes the recomendation on using RVM.
    The .ruby-version file is supported by all major Ruby version
    managers. Gemsets are no longer a needed feature since Bundler
    handles multiple concurrrently installed Gems just well.

--- script/ci/before_install.sh
+++ script/ci/before_install.sh
@@ -2,8 +2,3 @@
 
 # Travis CI still includes 1.2.x by default
 gem install bundler --version '>= 1.3.4'
-
-# Ensure known RubyGems version
-envdir="$(readlink -e $(dirname $0))/../env"
-. "$envdir/ruby_env"
-. "$envdir/ensure_right_rubygems"
