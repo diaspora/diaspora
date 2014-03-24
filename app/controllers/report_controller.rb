8b8a232b17a395c7f004a5d9df6c0e2fc83139d2
Lukas Matt <lukas@zauberstuhl.de>
2014-03-24 13:52:10 -0400

    Added diaspora copyright

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,3 +1,7 @@
+#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+
 class ReportController < ApplicationController
   before_filter :authenticate_user!
   before_filter :redirect_unless_admin, :except => [:create]
