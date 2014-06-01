6cf484c9a3ce5aa141363ae67c58fa94d055a105
Jason Robinson <mail@jasonrobinson.me>
2014-06-01 15:02:40 +0300

    Make sure publisher is totally hidden in bookmarklet after post success

--- app/assets/javascripts/app/views/bookmarklet_view.js
+++ app/assets/javascripts/app/views/bookmarklet_view.js
@@ -35,6 +35,7 @@ app.views.Bookmarklet = Backbone.View.extend({
   _postSuccess: function(evt) {
     this.$('h4').text(Diaspora.I18n.t('bookmarklet.post_success'));
     app.publisher.close();
+    this.$("#publisher").addClass("hidden");
     _.delay(window.close, 2000);
   },
 
