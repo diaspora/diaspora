6833ca46bb67815dc1fc0b9793e7715cddac7d88
kpcyrd <git@rxv.cc>
2014-07-01 00:27:38 +0200

    Fix broken aspect-edit sidebar update

--- app/assets/javascripts/aspect-edit-pane.js
+++ app/assets/javascripts/aspect-edit-pane.js
@@ -13,7 +13,7 @@ function updateAspectName(new_name) {
   $('input#aspect_name').val(new_name);
 }
 function updatePageAspectName( an_id, new_name) {
-  $('ul#aspect_nav [data-guid="'+an_id+'"]').html(new_name);
+  $('ul#aspect_nav [data-aspect-id="'+an_id+'"] .selectable').text(new_name);
 }
 
 $(document).ready(function() {
