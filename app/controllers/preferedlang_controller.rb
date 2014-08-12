0963e773d24921c68eb322ba7b56ecfd3f03740b
Abhineet Agarwal <agarwal.abhi93@gmail.com>
2014-08-12 22:34:04 +0530

    Integrated with new controller in place. BUG: Two forms/buttons used for getting started

---
+++ app/controllers/preferedlang_controller.rb
@@ -0,0 +1,13 @@
+class PreferedlangController < ApplicationController
+  before_filter :authenticate_user!
+  def create
+      list_of_languages = params[:pref_languages]
+      list_of_languages.each do |lang|
+        lang = lang.to_i
+        print lang
+        language = Preferedlanguage.find(lang)
+        current_user.preferedlanguages << language
+      end
+      redirect_to '/stream'
+  end
+end
---
+++ spec/helpers/preferedlang_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the PreferedlangHelper. For example:
+#
+# describe PreferedlangHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe PreferedlangHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
