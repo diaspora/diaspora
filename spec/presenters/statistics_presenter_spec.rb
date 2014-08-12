63090a1a08bf2808ac44121c42f7cd6a1a2baa82
damzcodes <damzcodes@gmail.com>
2014-08-12 17:11:36 +0100

    Service have been added to /statistics.json
    Services have been added to /statistics.json
    
    tidying up
    
    edits

--- spec/presenters/statistics_presenter_spec.rb
+++ spec/presenters/statistics_presenter_spec.rb
@@ -18,28 +18,44 @@ describe StatisticsPresenter do
       AppConfig.privacy.statistics.user_counts = false
       AppConfig.privacy.statistics.post_counts = false
       AppConfig.privacy.statistics.comment_counts = false
-      @presenter.as_json.should == {
-        "name" => AppConfig.settings.pod_name,
-        "version" => AppConfig.version_string,
-        "registrations_open" => AppConfig.settings.enable_registrations
-      }
-    end
-    
-    it 'provides generic pod data and counts in json' do
-      AppConfig.privacy.statistics.user_counts = true
-      AppConfig.privacy.statistics.post_counts = true
-      AppConfig.privacy.statistics.comment_counts = true
-
+      AppConfig.services = {"facebook" => nil}
       @presenter.as_json.should == {
         "name" => AppConfig.settings.pod_name,
         "version" => AppConfig.version_string,
         "registrations_open" => AppConfig.settings.enable_registrations,
-        "total_users" => User.count,
-        "active_users_halfyear" => User.halfyear_actives.count,
-        "active_users_monthly" => User.monthly_actives.count,
-        "local_posts" => @presenter.local_posts,
-        "local_comments" => @presenter.local_comments
+        "facebook" => false
       }
+    end
+    
+    context 'when services are enabled' do
+      before do
+        AppConfig.privacy.statistics.user_counts = true
+        AppConfig.privacy.statistics.post_counts = true
+        AppConfig.privacy.statistics.comment_counts = true
+        AppConfig.services = {
+          "facebook" => {"enable" => true}, 
+          "twitter" => {"enable" => true}, 
+          "wordpress" => {"enable" => false},
+          "tumblr" => {"enable" => false}
+        }
+      end
+
+      it 'provides generic pod data and counts in json' do
+        @presenter.as_json.should == {
+          "name" => AppConfig.settings.pod_name,
+          "version" => AppConfig.version_string,
+          "registrations_open" => AppConfig.settings.enable_registrations,
+          "total_users" => User.count,
+          "active_users_halfyear" => User.halfyear_actives.count,
+          "active_users_monthly" => User.monthly_actives.count,
+          "local_posts" => @presenter.local_posts,
+          "local_comments" => @presenter.local_comments,
+          "facebook" => true,
+          "twitter" => true,
+          "tumblr" => false,
+          "wordpress" => false
+        }
+      end
     end
 
   end
