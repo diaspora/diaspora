1de67281f4a186ddea34ecde44db1a0415b35523
jaideng123 <jaideng123@yahoo.com>
2014-08-26 14:04:28 -0500

    Simplified range check

--- config/initializers/setup_simple_captcha.rb
+++ config/initializers/setup_simple_captcha.rb
@@ -1,11 +1,6 @@
 SimpleCaptcha.setup do |sc|
   sc.image_size = AppConfig.settings.captcha.image_size
-  value_length = AppConfig.settings.captcha.captcha_length.to_i
-  if value_length <= 12 && value_length > 0 
-    sc.length = value_length
-  else
-    sc.length = 5
-  end
+  sc.length = [1, [AppConfig.settings.captcha_length.to_i, 12].min].max
   sc.image_style = AppConfig.settings.captcha.image_style
   sc.distortion = AppConfig.settings.captcha.distortion
   p AppConfig.settings.captcha
