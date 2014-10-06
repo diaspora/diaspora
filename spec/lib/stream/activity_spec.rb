226f1bbfc3f0847afd86e12047f27b221060b8e9
Steffen van Bergerem <svbergerem@online.de>
2014-10-06 02:39:53 +0200

    Convert activity stream cuke

--- spec/lib/stream/activity_spec.rb
+++ spec/lib/stream/activity_spec.rb
@@ -3,10 +3,19 @@ require Rails.root.join('spec', 'shared_behaviors', 'stream')
 
 describe Stream::Activity do
   before do
-    @stream = Stream::Activity.new(alice, :max_time => Time.now, :order => 'updated_at')
+    @stream = Stream::Activity.new(alice)
   end
 
   describe 'shared behaviors' do
     it_should_behave_like 'it is a stream'
+  end
+
+  describe "#posts" do
+    it "calls EvilQuery::Participation with correct parameters" do
+      expect(::EvilQuery::Participation).to receive(:new)
+              .with(alice)
+              .and_return(double.tap { |m| allow(m).to receive(:posts)})
+      @stream.posts
+    end
   end
 end
