0641738fa4e707087142c05b8a7e99a4a626de10
Faldrian <faldrian@och-noe.de>
2014-11-03 23:27:04 +0100

    added cucumber tests for collapsing posts
    Conflicts:
    	features/step_definitions/custom_web_steps.rb

--- features/step_definitions/posts_steps.rb
+++ features/step_definitions/posts_steps.rb
@@ -55,6 +55,14 @@ When /^I expand the post$/ do
   expand_first_post
 end
 
+When /^I collapse the post$/ do
+  collapse_first_post
+end
+
+Then /^I debug and wait for keypress$/ do
+  STDIN.getc
+end
+
 Then /^I should see "([^"]*)" as the first post in my stream$/ do |text|
   first_post_text.should include(text)
 end
