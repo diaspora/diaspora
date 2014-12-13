f1d4adad3c654affb35ecf85a3c84f7d474d95b1
Maxwell Salzberg <maxwell@backerkit.com>
2014-12-13 01:33:29 -0800

    this is already run

--- db/migrate/20140601102543_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++ db/migrate/20140601102543_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
@@ -1,12 +1,12 @@
 # This migration comes from acts_as_taggable_on_engine (originally 3)
 class AddTaggingsCounterCacheToTags < ActiveRecord::Migration
   def self.up
-    add_column :tags, :taggings_count, :integer, default: 0
-
-    ActsAsTaggableOn::Tag.reset_column_information
-    ActsAsTaggableOn::Tag.find_each do |tag|
-      ActsAsTaggableOn::Tag.reset_counters(tag.id, :taggings)
-    end
+    # add_column :tags, :taggings_count, :integer, default: 0
+    # 
+    # ActsAsTaggableOn::Tag.reset_column_information
+    # ActsAsTaggableOn::Tag.find_each do |tag|
+    #   ActsAsTaggableOn::Tag.reset_counters(tag.id, :taggings)
+    # end
   end
 
   def self.down
