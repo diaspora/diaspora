f49c8884f4fb9dc8f19dc3468b9446642fb12b93
Jonne Haß <me@jhass.eu>
2014-12-28 21:02:09 +0100

    Use _url, not _path, in mailers

--- app/mailers/maintenance.rb
+++ app/mailers/maintenance.rb
@@ -3,7 +3,7 @@ class Maintenance < ActionMailer::Base
 
   def account_removal_warning(user)
     @user = user
-    @login_url  = new_user_session_path
+    @login_url  = new_user_session_url
     @pod_url = AppConfig.environment.url
     @after_days = AppConfig.settings.maintenance.remove_old_users.after_days.to_s
     @remove_after = @user.remove_after
