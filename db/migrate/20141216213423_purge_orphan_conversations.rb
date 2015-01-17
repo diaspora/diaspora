7e245a008213d868cf0cd8f468b534907a6b7711
Jonne Haß <me@jhass.eu>
2015-01-17 15:30:18 +0100

    Fix orphan conversations migration
    Can't delete all with group/having

--- db/migrate/20141216213423_purge_orphan_conversations.rb
+++ db/migrate/20141216213423_purge_orphan_conversations.rb
@@ -1,6 +1,10 @@
 class PurgeOrphanConversations < ActiveRecord::Migration
   def up
-    Conversation.joins("LEFT JOIN conversation_visibilities ON conversation_visibilities.conversation_id = conversations.id").group('conversations.id').having("COUNT(conversation_visibilities.id) = 0").delete_all
+    Conversation.where(id: Conversation.joins("LEFT JOIN conversation_visibilities ON conversation_visibilities.conversation_id = conversations.id")
+		                       .group('conversations.id')
+				       .having("COUNT(conversation_visibilities.id) = 0")
+				       .pluck('conversations.id')
+    ).delete_all
   end
 
   def down
