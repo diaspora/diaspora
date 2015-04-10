ec2a209b68c1db0141cc8cd60420023d491ca8d7
Jonne Haß <me@jhass.eu>
2015-04-10 19:19:16 +0200

    Lazily initialize Contact proxy in service helper
    This prevents connecting to the database on rake assets:precompile,
    which fails in certain build environments.
    
    Running git blame on this and figuring out what in gods name this
    is for, is for another time.

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,6 +1,11 @@
 module ServicesHelper
-  @@contact_proxy = Contact.new(:aspects => [])
   def contact_proxy(friend)
-    friend.contact || @@contact_proxy.dup.tap{|c| c.person = friend.person}
+    friend.contact || contact_proxy_template.dup.tap {|c| c.person = friend.person }
+  end
+
+  private
+
+  def contact_proxy_template
+    @@contact_proxy ||= Contact.new(:aspects => [])
   end
 end
