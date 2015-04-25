1277078cd88a4a7965ed398bf5a3a67e0ac7a34f
Jan Berdajs <mrbrdo@gmail.com>
2015-04-25 17:12:04 +0200

    upgrade Twitter gem - fixes Faraday deprecation warning

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,16 +5,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if AppConfig.services.twitter.enable?
     provider :twitter, AppConfig.services.twitter.key, AppConfig.services.twitter.secret
-    Twitter.configure do |config|
-      config.consumer_key = AppConfig.services.twitter.key
-      config.consumer_secret = AppConfig.services.twitter.secret
-    end
   end
-  
+
   if AppConfig.services.tumblr.enable?
     provider :tumblr, AppConfig.services.tumblr.key, AppConfig.services.tumblr.secret
   end
-  
+
   if AppConfig.services.facebook.enable?
     provider :facebook, AppConfig.services.facebook.app_id, AppConfig.services.facebook.secret, {
       display:        "popup",
@@ -24,9 +20,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
           ca_file: AppConfig.environment.certificate_authorities
         }
       }
-    }  
+    }
   end
-  
+
   if AppConfig.services.wordpress.enable?
     provider :wordpress, AppConfig.services.wordpress.client_id, AppConfig.services.wordpress.secret
   end
