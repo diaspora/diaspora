494c03a3fd9b1addc7aee66cfcde850872fbd4f0
Benjamin Neff <benjamin@coding4coffee.ch>
2015-05-17 02:33:48 +0200

    re add user atom feed on profile-page

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -31,8 +31,6 @@ module LayoutHelper
   end
 
   def current_user_atom_tag
-    return #temp hax
-
     return unless @person.present?
     content_tag(:link, '', :rel => 'alternate', :href => "#{@person.public_url}.atom", :type => "application/atom+xml", :title => t('.public_feed', :name => @person.name))
   end
