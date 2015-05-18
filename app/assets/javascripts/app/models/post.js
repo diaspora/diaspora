1685073ffa1c6d8cc0f766be45c4845563ae56fa
Benjamin Neff <benjamin@coding4coffee.ch>
2015-05-18 03:23:54 +0200

    remove unused hasPhotos and hasText functions
    - added in a836b06d3e85acaea1d6a4c3d7eaaec47f48d561
    - removed in 49ea8c856342ec9e6085a7c5915b6e97e1b8627c

--- app/assets/javascripts/app/models/post.js
+++ app/assets/javascripts/app/models/post.js
@@ -61,14 +61,6 @@ app.models.Post = Backbone.Model.extend(_.extend({}, app.models.formatDateMixin,
   preloadOrFetch : function(){
     var action = app.hasPreload("post") ? this.set(app.parsePreload("post")) : this.fetch();
     return $.when(action);
-  },
-
-  hasPhotos : function(){
-    return this.get("photos") && this.get("photos").length > 0;
-  },
-
-  hasText : function(){
-    return $.trim(this.get("text")) !== "";
   }
 }));
 // @license-end
