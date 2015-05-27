a0460da8dcb2fc7bc441e119fc386b88b1dea98c
Sandip Trivedi <trivedi.sandip@gmail.com>
2015-05-27 18:09:22 -0400

    Replaces the zipzip gem with the rubyzip gem

---
+++ config/initializers/rubyzip.rb
@@ -0,0 +1,7 @@
+# https://github.com/rubyzip/rubyzip#configuration
+# Zip.setup do |c|
+#   c.on_exists_proc = true
+#   c.continue_on_exists_proc = true
+#   c.unicode_names = true
+#   c.default_compression = Zlib::BEST_COMPRESSION
+# end
