0b23f795ffc737982195873dfcf295e71451cfea
Sandip Trivedi <trivedi.sandip@gmail.com>
2015-05-27 20:48:05 -0400

    Remove unnecessary initializer and comments in Gemfile and add explicit rubyzip gem version in Gemfile with description

--- config/initializers/rubyzip.rb
+++
@@ -1,7 +0,0 @@
-# https://github.com/rubyzip/rubyzip#configuration
-# Zip.setup do |c|
-#   c.on_exists_proc = true
-#   c.continue_on_exists_proc = true
-#   c.unicode_names = true
-#   c.default_compression = Zlib::BEST_COMPRESSION
-# end
