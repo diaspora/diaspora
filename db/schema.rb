19e5788ab953642c23c4d995cb3eafdf8828f482
Benjamin Neff <benjamin@coding4coffee.ch>
2015-05-31 03:03:35 +0200

    add db migration to cleanup default avatar image-urls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404193023) do
+ActiveRecord::Schema.define(version: 20150531005120) do
 
   create_table "account_deletions", force: :cascade do |t|
     t.string   "diaspora_handle", limit: 255
