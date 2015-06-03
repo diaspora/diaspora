b6c46b3fc488b9bda12529c093ead4b4311fd557
Benjamin Neff <benjamin@coding4coffee.ch>
2015-06-03 02:39:12 +0200

    add logging mixin and replace federation-logger

--- lib/postzord/receiver/local_batch.rb
+++ lib/postzord/receiver/local_batch.rb
@@ -14,7 +14,7 @@ class Postzord::Receiver::LocalBatch < Postzord::Receiver
   end
 
   def receive!
-    FEDERATION_LOGGER.info("receiving local batch for #{@object.inspect}")
+    logger.info "receiving local batch for #{@object.inspect}"
     if @object.respond_to?(:relayable?)
       receive_relayable
     else
@@ -25,7 +25,7 @@ class Postzord::Receiver::LocalBatch < Postzord::Receiver
     # 09/27/11 this is slow
     notify_users
 
-    FEDERATION_LOGGER.info("receiving local batch completed for #{@object.inspect}")
+    logger.info "receiving local batch completed for #{@object.inspect}"
     true
   end
 
