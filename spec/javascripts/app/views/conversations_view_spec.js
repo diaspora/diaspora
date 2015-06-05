9197747e7817b32cacafb6b1cf7a8a4af0fe30bf
Benjamin Neff <benjamin@coding4coffee.ch>
2015-06-05 02:58:15 +0200

    refactor: iterate over visiblities

--- spec/javascripts/app/views/conversations_view_spec.js
+++ spec/javascripts/app/views/conversations_view_spec.js
@@ -3,6 +3,9 @@ describe("app.views.Conversations", function(){
     context("for unread conversations", function() {
       beforeEach(function() {
         spec.loadFixture("conversations_unread");
+        // select second conversation that is still unread
+        $(".conversation-wrapper > .conversation.selected").removeClass("selected")
+        $(".conversation-wrapper > .conversation.unread").addClass("selected")
       });
 
       it("removes the unread class from the conversation", function() {
@@ -21,16 +24,16 @@ describe("app.views.Conversations", function(){
         var badge = "<div id=\"conversations_badge\"><div class=\"badge_count\">3</div></div>";
         $("header").append(badge);
         expect($("#conversations_badge .badge_count").text().trim()).toEqual("3");
-        expect($(".conversation-wrapper > .conversation.selected .unread_message_count").text().trim()).toEqual("2");
+        expect($(".conversation-wrapper > .conversation .unread_message_count").text().trim()).toEqual("1");
         new app.views.Conversations();
-        expect($("#conversations_badge .badge_count").text().trim()).toEqual("1");
+        expect($("#conversations_badge .badge_count").text().trim()).toEqual("2");
       });
 
       it("removes the badge_count in the header if there are no unread messages left", function() {
-        var badge = "<div id=\"conversations_badge\"><div class=\"badge_count\">2</div></div>";
+        var badge = "<div id=\"conversations_badge\"><div class=\"badge_count\">1</div></div>";
         $("header").append(badge);
-        expect($("#conversations_badge .badge_count").text().trim()).toEqual("2");
-        expect($(".conversation-wrapper > .conversation.selected .unread_message_count").text().trim()).toEqual("2");
+        expect($("#conversations_badge .badge_count").text().trim()).toEqual("1");
+        expect($(".conversation-wrapper > .conversation.selected .unread_message_count").text().trim()).toEqual("1");
         new app.views.Conversations();
         expect($("#conversations_badge .badge_count").text().trim()).toEqual("0");
         expect($("#conversations_badge .badge_count")).toHaveClass("hidden");
