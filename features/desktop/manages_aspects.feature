e2ee7692550ad38260f6b5e76b22060951ab9245
Jonne Haß <me@jhass.eu>
2015-06-08 14:28:00 +0200

    Improve stability for manages_aspects.feature:79
    page.execute_scripts looks to be asynchronous, .synced is already set
    by default, so the previous expectation would be fulfilled before
    the script got to run, running into the page reload in the next step
    which then aborts or hasen't seen the to be triggered request yet.
    
    The fix is to use Capybaras drag_to instead

--- features/desktop/manages_aspects.feature
+++ features/desktop/manages_aspects.feature
@@ -81,7 +81,7 @@ Feature: User manages contacts
     And I have an aspect called "People"
     And I have an aspect called "Cat People"
     When I am on the contacts page
-    And I drag "Cat People" up 40 pixels
+    And I drag "Cat People" up
     And I go to the contacts page
     Then I should see "Cat People" as 2. aspect
     And I should see "People" as 3. aspect
--- features/step_definitions/aspects_steps.rb
+++ features/step_definitions/aspects_steps.rb
@@ -88,23 +88,13 @@ When /^(.*) in the aspect creation modal$/ do |action|
   end
 end
 
-When /^I drag "([^"]*)" (up|down) (\d+) pixels?$/ do |aspect_name, direction, distance|
-  distance = distance.to_i * -1 if direction == "up"
-  page.execute_script %{
-    function drag() {
-      $("a.aspect:contains('#{aspect_name}')")
-        .simulate("drag-n-drop", { dy: #{distance}, interpolation: { stepWidth: 10, stepDelay: 5 } });
-    }
-    function loadScripts() {
-      $.getScript("/assets/jquery-simulate/jquery.simulate.js", function(){
-        $.getScript("/assets/jquery-simulate-ext/src/jquery.simulate.ext.js", function(){
-          $.getScript("/assets/jquery-simulate-ext/src/jquery.simulate.drag-n-drop.js", drag);
-        });
-      });
-    }
-    if (!$.simulate) { loadScripts(); } else { drag(); }
-  }
-  expect(find("#aspect_nav")).to have_css ".synced"
+When /^I drag "([^"]*)" (up|down)$/ do |aspect_name, direction|
+  aspect_id = @me.aspects.where(name: aspect_name).first.id
+  aspect = find(:xpath, "//div[@id='aspect_nav']/ul/a[@data-aspect-id='#{aspect_id}']")
+  target = direction == "up" ? aspect.all(:xpath, "./preceding-sibling::a").last :
+                               aspect.all(:xpath, "./following-sibling::a").first
+  aspect.drag_to(target)
+  expect(page).to have_no_css "#aspect_nav .ui-sortable.syncing"
 end
 
 And /^I toggle the aspect "([^"]*)"$/ do |name|
