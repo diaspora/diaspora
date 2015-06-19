12678bde537d16b83513335305d14a69088e86b0
Faldrian <faldrian@och-noe.de>
2015-06-19 12:26:14 +0200

    Added cucumber step to add multiple posts in one step
    closes #6118

--- features/step_definitions/posts_steps.rb
+++ features/step_definitions/posts_steps.rb
@@ -27,6 +27,13 @@ Given /^"([^"]*)" has a public post with text "([^"]*)"$/ do |email, text|
   user.post(:status_message, :text => text, :public => true, :to => user.aspect_ids)
 end
 
+Given /^there are (\d+) public posts from "([^"]*)"$/ do |n_posts, email|
+  user = User.find_by_email(email)
+  (1..n_posts.to_i).each do |n|
+    user.post(:status_message, text: "post nr. #{n}", public: true, to: user.aspect_ids)
+  end
+end
+
 Given /^"([^"]*)" has a non public post with text "([^"]*)"$/ do |email, text|
   user = User.find_by_email(email)
   user.post(:status_message, :text => text, :public => false, :to => user.aspect_ids)
