9963259c5d3dcdfe0306d2a39b93738f009a0f3a
Brent Bartlett <brent.bartlett1@gmail.com>
2015-06-20 21:17:15 -0700

    replaced :alnum with :word and added Armenian punctuation

--- app/assets/javascripts/app/helpers/text_formatter.js
+++ app/assets/javascripts/app/helpers/text_formatter.js
@@ -36,15 +36,8 @@
     var hashtagPlugin = window.markdownitHashtag;
     md.use(hashtagPlugin, {
       // compare tag_text_regexp in app/models/acts_as_taggable_on-tag.rb
-      hashtagRegExp: "[" + PosixBracketExpressions.alnum
-							// Armenian punctuation
-							+ "\u055b"
-							+ "\u055c"
-							+ "\u055e"
-							+ "\u058a"
-							// Malayalam virama
-							+ "\u0d4d"
-							+ "_\\-]+|<3",
+      hashtagRegExp: "[" + PosixBracketExpressions.word +
+							"\u055b\u055c\u055e_\\-]+|<3",
       // compare tag_strings in lib/diaspora/taggabe.rb
       preceding: "^|\\s"
     });
