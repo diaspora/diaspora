98ccf0e6dab53f7f0591478470523ace63912a63
cmrd Senya <senya@riseup.net>
2015-06-29 16:50:14 +0300

    Add new daily job to clean cache in public/tmp (as suggested in #5946).

---
+++ app/workers/clean_cached_files.rb
@@ -0,0 +1,14 @@
+module Workers
+  class CleanCachedFiles < Base
+    include Sidetiq::Schedulable
+    
+    sidekiq_options queue: :maintenance
+    
+    recurrence { daily }
+    
+    def perform
+      CarrierWave.clean_cached_files!
+    end
+  end 
+end
+
