3cfbcbce8fa87853075f72d0dff03d1eacf32331
theworldbright <sushionigiris@gmail.com>
2015-07-10 02:09:45 +0900

    Implement authorization endpoint (part 1)
    The user can now authenticate with the authorization
    server's authorization endpoint and receive a fake
    id token.

--- app/controllers/openid_connect/authorizations_controller.rb
+++ app/controllers/openid_connect/authorizations_controller.rb
@@ -1,47 +1,54 @@
-class AuthorizationsController < ApplicationController
+class OpenidConnect::AuthorizationsController < ApplicationController
   rescue_from Rack::OAuth2::Server::Authorize::BadRequest do |e|
     @error = e
-    logger.info e.backtrace[0,10].join("\n")
-    render :error, status: e.status
+    print e.backtrace[0,10].join("\n")
+    render json: {error: :error, status: e.status} #error_message: e.message
   end
 
   before_action :authenticate_user!
 
   def new
-    call_authorization_endpoint
+    request_authorization_consent_form
   end
 
   def create
-    call_authorization_endpoint :is_create, params[:approve]
+    process_authorization_consent(params[:approve])
   end
 
-  private
+private
 
-  def call_authorization_endpoint(is_create = false, approved = false)
-    endpoint = AuthorizationEndpoint.new is_create, approved
-    rack_response = *endpoint.call(request.env)
-    @client, @response_type, @redirect_uri, @scopes, @_request_, @request_uri, @request_object = *[
-        endpoint.client, endpoint.response_type, endpoint.redirect_uri, endpoint.scopes, endpoint._request_, endpoint.request_uri, endpoint.request_object
+  def request_authorization_consent_form
+    endpoint = OpenidConnect::Authorization::EndpointStartPoint.new(current_user)
+    endpoint.call(request.env)
+    @client, @response_type, @redirect_uri, @scopes, @request_object = *[
+      endpoint.client, endpoint.response_type, endpoint.redirect_uri, endpoint.scopes, endpoint.request_object
     ]
-    if (
-    !is_create &&
-        (max_age = @request_object.try(:id_token).try(:max_age)) &&
-        current_account.last_logged_in_at < max_age.seconds.ago
-    )
-      flash[:notice] = 'Exceeded Max Age, Login Again'
-      unauthenticate!
-    end
-    respond_as_rack_app *rack_response
+    saveRequestParameters
+    render :new
   end
 
-  def respond_as_rack_app(status, header, response)
-    ["WWW-Authenticate"].each do |key|
-      headers[key] = header[key] if header[key].present?
-    end
-    if response.redirect?
-      redirect_to header['Location']
-    else
-      render :new
-    end
+  def process_authorization_consent(approvedString)
+    endpoint = OpenidConnect::Authorization::EndpointConfirmationPoint.new(current_user, to_boolean(approvedString))
+    restoreRequestParameters(endpoint)
+    status, header, response = *endpoint.call(request.env)
+    redirect_to header['Location']
+  end
+
+  def saveRequestParameters
+    session[:client_id], session[:response_type], session[:redirect_uri], session[:scopes], session[:request_object] =
+      @client.client_id, @response_type, @redirect_uri, @scopes.collect { |scope| scope.name }, @request_object
+  end
+
+  def restoreRequestParameters(endpoint)
+    req = Rack::Request.new(request.env)
+    req.update_param("client_id", session[:client_id])
+    req.update_param("redirect_uri", session[:redirect_uri])
+    req.update_param("response_type", session[:response_type])
+    endpoint.scopes, endpoint.request_object =
+      session[:scopes].collect {|scope| Scope.find_by_name(scope)}, session[:request_object]
+  end
+
+  def to_boolean(str)
+    str.downcase == "true"
   end
 end
---
+++ lib/openid_connect/authorization/endpoint_start_point.rb
@@ -0,0 +1,28 @@
+module OpenidConnect
+  module Authorization
+    class EndpointStartPoint < Endpoint
+      def initialize(current_user)
+        super(current_user)
+      end
+      def handleResponseType(req, res)
+        @response_type = req.response_type
+      end
+      def buildAttributes(req, res)
+        super(req, res)
+        verifyNonce(req, res)
+        buildScopes(req)
+        # TODO: buildRequestObject(req)
+      end
+      def verifyNonce(req, res)
+        if res.protocol_params_location == :fragment && req.nonce.blank?
+          req.invalid_request! "nonce required"
+        end
+      end
+      def buildScopes(req)
+        @scopes = req.scope.inject([]) do |_scopes_, scope|
+          _scopes_ << Scope.find_by_name(scope) or req.invalid_scope! "Unknown scope: #{scope}"
+        end
+      end
+    end
+  end
+end
