4b40359f5d161c93d58374d764096aacf695f3dc
theworldbright <sushionigiris@gmail.com>
2015-07-10 16:55:17 +0900

    Fix errors with unsupported scope and missing nonce
    Squashed Commits:
    [2e85130] Add tests for missing/invalid params

--- app/controllers/openid_connect/authorizations_controller.rb
+++ app/controllers/openid_connect/authorizations_controller.rb
@@ -1,8 +1,7 @@
 class OpenidConnect::AuthorizationsController < ApplicationController
   rescue_from Rack::OAuth2::Server::Authorize::BadRequest do |e|
-    @error = e
-    print e.backtrace[0,10].join("\n")
-    render json: {error: :error, status: e.status} #error_message: e.message
+    logger.info e.backtrace[0,10].join("\n")
+    render json: {error: e.message || :error, status: e.status}
   end
 
   before_action :authenticate_user!
@@ -19,20 +18,33 @@ private
 
   def request_authorization_consent_form
     endpoint = OpenidConnect::Authorization::EndpointStartPoint.new(current_user)
-    endpoint.call(request.env)
-    @client, @response_type, @redirect_uri, @scopes, @request_object = *[
-      endpoint.client, endpoint.response_type, endpoint.redirect_uri, endpoint.scopes, endpoint.request_object
-    ]
-    saveRequestParameters
-    render :new
+    handleStartPointResponse(endpoint)
+  end
+
+  def handleStartPointResponse(endpoint)
+    status, header, response = *endpoint.call(request.env)
+    if response.redirect?
+      redirect_to header['Location']
+    else
+      @client, @response_type, @redirect_uri, @scopes, @request_object = *[
+        endpoint.client, endpoint.response_type, endpoint.redirect_uri, endpoint.scopes, endpoint.request_object
+      ]
+      saveRequestParameters
+      render :new
+    end
   end
 
   def process_authorization_consent(approvedString)
     endpoint = OpenidConnect::Authorization::EndpointConfirmationPoint.new(current_user, to_boolean(approvedString))
     restoreRequestParameters(endpoint)
+    handleConfirmationPointResponse(endpoint)
+  end
+
+  def handleConfirmationPointResponse(endpoint)
     status, header, response = *endpoint.call(request.env)
     redirect_to header['Location']
   end
+
 
   def saveRequestParameters
     session[:client_id], session[:response_type], session[:redirect_uri], session[:scopes], session[:request_object] =
