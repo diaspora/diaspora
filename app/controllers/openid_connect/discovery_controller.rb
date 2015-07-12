9d9dc1327265f63967dbf896e04d6c09ee005d34
theworldbright <kent@kentshikama.com>
2015-07-13 01:37:10 +0900

    Adjust discovery controller to current values

--- app/controllers/openid_connect/discovery_controller.rb
+++ app/controllers/openid_connect/discovery_controller.rb
@@ -1,45 +1,32 @@
-class DiscoveryController < ApplicationController
-  def show
-    case params[:id]
-    when "webfinger"
-      webfinger_discovery
-    when "openid-configuration"
-      openid_configuration
-    else
-      raise HttpError::NotFound
+module OpenidConnect
+  class DiscoveryController < ApplicationController
+    def webfinger
+      jrd = {
+               links: [{
+                          rel: OpenIDConnect::Discovery::Provider::Issuer::REL_VALUE,
+                          href: File.join(root_url, "openid_connect")
+                       }]
+            }
+      jrd[:subject] = params[:resource] if params[:resource].present?
+      render json: jrd, content_type: "application/jrd+json"
     end
-  end
 
-  private
-
-  def webfinger_discovery
-    jrd = {
-      links: [{
-        rel:  OpenIDConnect::Discovery::Provider::Issuer::REL_VALUE,
-        href: root_path
-      }]
-    }
-    jrd[:subject] = params[:resource] if params[:resource].present?
-    render json: jrd, content_type: "application/jrd+json"
-  end
-
-  def openid_configuration
-    config = OpenIDConnect::Discovery::Provider::Config::Response.new(
-      issuer:                                      root_path,
-      authorization_endpoint:                      "#{authorizations_url}/new",
-      token_endpoint:                              access_tokens_url,
-      userinfo_endpoint:                           user_info_url,
-      jwks_uri:                                    "#{authorizations_url}/jwks.json",
-      registration_endpoint:                       "#{root_path}/connect",
-      scopes_supported:                            "iss",
-      response_types_supported:                    "Client.available_response_types",
-      grant_types_supported:                       "Client.available_grant_types",
-      request_object_signing_alg_values_supported: %i(HS256 HS384 HS512),
-      subject_types_supported:                     %w(public pairwise),
-      id_token_signing_alg_values_supported:       %i(RS256),
-      token_endpoint_auth_methods_supported:       %w(client_secret_basic client_secret_post),
-      claims_supported:                            %w(sub iss name email)
-    )
-    render json: config
+    def configuration
+      render json: OpenIDConnect::Discovery::Provider::Config::Response.new(
+               issuer: root_url,
+               registration_endpoint: openid_connect_clients_url,
+               authorization_endpoint: new_openid_connect_authorization_url,
+               token_endpoint: openid_connect_access_tokens_url,
+               userinfo_endpoint: api_v0_user_url,
+               jwks_uri: "https://not_configured_yet.com", # TODO: File.join({new_openid_connect_authorization_path} + "/jwks.json"),
+               scopes_supported: Scope.pluck(:name),
+               response_types_supported: OAuthApplication.available_response_types,
+               request_object_signing_alg_values_supported: %i(HS256 HS384 HS512),
+               subject_types_supported: %w(public pairwise),
+               id_token_signing_alg_values_supported: %i(RS256),
+               token_endpoint_auth_methods_supported: %w(client_secret_basic client_secret_post),
+             # TODO: claims_supported: ["sub", "iss", "name", "email"]
+             )
+    end
   end
 end
