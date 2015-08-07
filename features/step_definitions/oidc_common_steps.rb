f9dc8b9abfb04d81bf0a3a39dedb1f1fa478c9e1
theworldbright <kent@kentshikama.com>
2015-08-07 22:09:28 +0900

    Remove zone info claim

--- features/step_definitions/oidc_common_steps.rb
+++ features/step_definitions/oidc_common_steps.rb
@@ -32,7 +32,6 @@ Then /^I should receive "([^\"]*)"'s id, username, and email$/ do |username|
   user = User.find_by_username(username)
   user_profile_url = File.join(AppConfig.environment.url, "people", user.guid).to_s
   expect(user_info_json["profile"]).to have_content(user_profile_url)
-  expect(user_info_json["zoneinfo"]).to have_content(user.language)
 end
 
 Then /^I should receive an "([^\"]*)" error$/ do |error_message|
