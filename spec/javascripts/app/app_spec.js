452be3a3664e0b696de6f71646a472d395f86f01
Steffen van Bergerem <svbergerem@online.de>
2015-10-12 00:44:04 +0200

    Redirect to sign in on 401 ajax response

--- spec/javascripts/app/app_spec.js
+++ spec/javascripts/app/app_spec.js
@@ -9,6 +9,7 @@ describe("app", function() {
       spyOn(app, "setupGlobalViews");
       spyOn(app, "setupDisabledLinks");
       spyOn(app, "setupForms");
+      spyOn(app, "setupAjaxErrorRedirect");
 
       app.initialize();
 
@@ -20,6 +21,7 @@ describe("app", function() {
       expect(app.setupGlobalViews).toHaveBeenCalled();
       expect(app.setupDisabledLinks).toHaveBeenCalled();
       expect(app.setupForms).toHaveBeenCalled();
+      expect(app.setupAjaxErrorRedirect).toHaveBeenCalled();
     });
   });
 
@@ -42,6 +44,49 @@ describe("app", function() {
       app.setupForms();
       expect($.fn.placeholder).toHaveBeenCalled();
       expect($.fn.placeholder.calls.mostRecent().object.selector).toBe("input, textarea");
+    });
+  });
+
+  describe("setupAjaxErrorRedirect", function() {
+    it("redirects to /users/sign_in on 401 ajax responses", function() {
+      spyOn(app, "_changeLocation");
+      $.ajax("/test");
+      jasmine.Ajax.requests.mostRecent().respondWith({status: 401});
+      expect(app._changeLocation).toHaveBeenCalledWith("/users/sign_in");
+    });
+
+    it("doesn't redirect on other responses", function() {
+      spyOn(app, "_changeLocation");
+
+      [200, 201, 400, 404, 500].forEach(function(code) {
+        $.ajax("/test");
+        jasmine.Ajax.requests.mostRecent().respondWith({status: code});
+        expect(app._changeLocation).not.toHaveBeenCalled();
+      });
+    });
+
+    it("doesn't redirect when error handling is suppressed", function() {
+      spyOn(app, "_changeLocation");
+      $.ajax("/test", {preventGlobalErrorHandling: true});
+      jasmine.Ajax.requests.mostRecent().respondWith({status: 401});
+      expect(app._changeLocation).not.toHaveBeenCalled();
+
+      $.ajax("/test", {preventGlobalErrorHandling: false});
+      jasmine.Ajax.requests.mostRecent().respondWith({status: 401});
+      expect(app._changeLocation).toHaveBeenCalledWith("/users/sign_in");
+    });
+
+    it("doesn't redirect when global ajax events are disabled", function() {
+      spyOn(app, "_changeLocation");
+      $.ajaxSetup({global: false});
+      $.ajax("/test");
+      jasmine.Ajax.requests.mostRecent().respondWith({status: 401});
+      expect(app._changeLocation).not.toHaveBeenCalled();
+
+      $.ajaxSetup({global: true});
+      $.ajax("/test");
+      jasmine.Ajax.requests.mostRecent().respondWith({status: 401});
+      expect(app._changeLocation).toHaveBeenCalledWith("/users/sign_in");
     });
   });
 });
