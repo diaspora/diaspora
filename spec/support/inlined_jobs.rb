a1b5033707b2c79a3b5b4f22171ab017f279a26c
cmrd Senya <senya@riseup.net>
2015-10-14 00:55:20 +0300

    Remove the lines from inlined_jobs.rb which never get called.

--- spec/support/inlined_jobs.rb
+++ spec/support/inlined_jobs.rb
@@ -4,7 +4,5 @@ module HelperMethods
     result = yield Sidekiq::Worker
     Sidekiq::Worker.drain_all
     result
-  rescue NoMethodError
-    yield Sidekiq::Worker if block_given? # Never error out on our own
   end
 end
