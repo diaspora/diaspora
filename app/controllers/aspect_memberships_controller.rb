abae16b8bbd606ea45a02db4664c5c5343c645b8
tamatsyk <taisecu@gmail.com>
2015-11-21 12:47:39 +0200

    internationalize controller rescue_from text
    Fix typos
    
    change forbitten to forbidden
    
    fix styling issue and copypaste
    
    improve code style for aspec_memberships_controller.rb with rubocop
    
    fix styling issues
    
    aligned elements of hash literals
    
    fix typo
    
    fix locale name and styling of its usage
    
    fix failing tests

--- app/controllers/aspect_memberships_controller.rb
+++ app/controllers/aspect_memberships_controller.rb
@@ -9,15 +9,15 @@ class AspectMembershipsController < ApplicationController
   respond_to :html, :json
 
   def destroy
-    aspect = current_user.aspects.joins(:aspect_memberships).where(:aspect_memberships=>{:id=>params[:id]}).first
-    contact = current_user.contacts.joins(:aspect_memberships).where(:aspect_memberships=>{:id=>params[:id]}).first
+    aspect = current_user.aspects.joins(:aspect_memberships).where(aspect_memberships: {id: params[:id]}).first
+    contact = current_user.contacts.joins(:aspect_memberships).where(aspect_memberships: {id: params[:id]}).first
 
     raise ActiveRecord::RecordNotFound unless aspect.present? && contact.present?
 
     raise Diaspora::NotMine unless current_user.mine?(aspect) &&
                                    current_user.mine?(contact)
 
-    membership = contact.aspect_memberships.where(:aspect_id => aspect.id).first
+    membership = contact.aspect_memberships.where(aspect_id: aspect.id).first
 
     raise ActiveRecord::RecordNotFound unless membership.present?
 
@@ -26,17 +26,17 @@ class AspectMembershipsController < ApplicationController
 
     # set the flash message
     if success
-      flash.now[:notice] = I18n.t 'aspect_memberships.destroy.success'
+      flash.now[:notice] = I18n.t "aspect_memberships.destroy.success"
     else
-      flash.now[:error] = I18n.t 'aspect_memberships.destroy.failure'
+      flash.now[:error] = I18n.t "aspect_memberships.destroy.failure"
     end
 
     respond_to do |format|
       format.json do
         if success
-          render :json => AspectMembershipPresenter.new(membership).base_hash
+          render json: AspectMembershipPresenter.new(membership).base_hash
         else
-          render :text => membership.errors.full_messages, :status => 403
+          render text: membership.errors.full_messages, status: 403
         end
       end
 
@@ -46,37 +46,36 @@ class AspectMembershipsController < ApplicationController
 
   def create
     @person = Person.find(params[:person_id])
-    @aspect = current_user.aspects.where(:id => params[:aspect_id]).first
+    @aspect = current_user.aspects.where(id: params[:aspect_id]).first
 
     @contact = current_user.share_with(@person, @aspect)
 
     if @contact.present?
-      flash.now[:notice] =  I18n.t('aspects.add_to_aspect.success')
+      flash.now[:notice] = I18n.t("aspects.add_to_aspect.success")
       respond_with do |format|
         format.json do
-          render :json => AspectMembershipPresenter.new(
-            AspectMembership.where(:contact_id => @contact.id, :aspect_id => @aspect.id).first)
+          render json: AspectMembershipPresenter.new(
+            AspectMembership.where(contact_id: @contact.id, aspect_id: @aspect.id).first)
           .base_hash
         end
 
         format.all { redirect_to :back }
       end
     else
-      flash.now[:error] = I18n.t('contacts.create.failure')
-      render :nothing => true, :status => 409
+      flash.now[:error] = I18n.t("contacts.create.failure")
+      render nothing: true, status: 409
     end
   end
 
   rescue_from ActiveRecord::StatementInvalid do
-    render :text => "Duplicate record rejected.", :status => 400
+    render text: I18n.t("aspect_memberships.destroy.invalid_statement"), status: 400
   end
 
   rescue_from ActiveRecord::RecordNotFound do
-    render :text => I18n.t('aspect_memberships.destroy.no_membership'), :status => 404
+    render text: I18n.t("aspect_memberships.destroy.no_membership"), status: 404
   end
 
   rescue_from Diaspora::NotMine do
-    render :text => "You are not allowed to do that.", :status => 403
+    render text: I18n.t("aspect_memberships.destroy.forbidden"), status: 403
   end
-
 end
