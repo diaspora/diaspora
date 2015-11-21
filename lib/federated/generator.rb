4bfdc0322b1eb059ce2949822eddcfb79ea04500
Jason Robinson <mail@jasonrobinson.me>
2015-11-21 21:44:32 +0200

    Send comment to reshare author when commenting on reshare
    As posts are always delivered also to reshare root, comments should also be delivered to reshare root, for concistency.

--- lib/federated/generator.rb
+++ lib/federated/generator.rb
@@ -12,9 +12,18 @@ module Federated
       relayable = build(options)
       if relayable.save!
         logger.info "user:#{@user.id} dispatching #{relayable.class}:#{relayable.guid}"
+        add_root_author(relayable)
         Postzord::Dispatcher.defer_build_and_post(@user, relayable, @dispatcher_opts)
         relayable
       end
+    end
+
+    def add_root_author(relayable)
+      return unless relayable.parent.respond_to?(:root) && relayable.parent.root
+      # Comment post is a reshare, include original author in subscribers
+      root_post = relayable.parent.root
+      @dispatcher_opts[:additional_subscribers] ||= []
+      @dispatcher_opts[:additional_subscribers] << root_post.author
     end
 
     def build(options={})
