10314ffc8c12260df7aaee22113e2d30eed9d3a5
augier <christophe@c-henry.fr>
2015-11-23 10:41:40 +0100

    Fixing more remarks

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.insert 0, Rack::Cors do
     resource "/.well-known/webfinger"
     resource "/.well-known/openid-configuration"
     resource "/api/openid_connect/user_info", methods: %i(get post)
-    resource "/api/v0/*", methods: %i(get post delete)
+    resource "/api/v0/*", methods: %i(delete get post)
   end
 end
