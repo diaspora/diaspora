a2ce47fae5e969cf438b4816bae5d05f1ef83770
cmrd Senya <senya@riseup.net>
2015-12-12 23:16:19 +0300

    Remove parent author signature for relayables from the DB since it is considered redundant

--- app/models/message.rb
+++ app/models/message.rb
@@ -21,11 +21,6 @@ class Message < ActiveRecord::Base
                    # inside, which would cause an infinite recursion
     #sign comment as commenter
     self.author_signature = self.sign_with_key(self.author.owner.encryption_key) if self.author.owner
-
-    if self.author.owns?(self.parent)
-      #sign comment as post owner
-      self.parent_author_signature = self.sign_with_key(self.parent.author.owner.encryption_key) if self.parent.author.owner
-    end
     self.save!
     self
   end
