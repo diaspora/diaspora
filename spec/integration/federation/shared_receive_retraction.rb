b6c7f004e42713598cf5fd3152155e8e9d2c408f
cmrd Senya <senya@riseup.net>
2015-12-21 22:20:31 +0300

    Further receive tests development

--- spec/integration/federation/shared_receive_retraction.rb
+++ spec/integration/federation/shared_receive_retraction.rb
@@ -1,41 +1,22 @@
-def mock_private_keys_for_retraction(entity_name, entity, sender)
-  if %i(signed_retraction_entity relayable_retraction_entity).include?(entity_name)
-    allow(DiasporaFederation.callbacks).to receive(:trigger)
-                                             .with(:fetch_private_key_by_diaspora_id, sender.diaspora_handle)
-                                             .and_return(sender.encryption_key)
-  end
-  if entity_name == :relayable_retraction_entity
-    allow(DiasporaFederation.callbacks).to receive(:trigger)
-                                             .with(
-                                               :fetch_entity_author_id_by_guid,
-                                               entity.target_type,
-                                               entity.target_guid
-                                             )
-                                             .and_return(sender.encryption_key)
-  end
-end
-
-def generate_retraction(entity_name, target_object, sender)
-  entity = FactoryGirl.build(
-    entity_name,
-    diaspora_id: sender.diaspora_handle,
-    target_guid: target_object.guid,
-    target_type: target_object.class.to_s
-  )
-
-  mock_private_keys_for_retraction(entity_name, entity, sender)
-  generate_xml(entity, sender, alice)
-end
-
 shared_examples_for "it retracts non-relayable object" do
-  it_behaves_like "it retracts object" do
-    let(:sender) { remote_user_on_pod_b }
+  it "retracts object by a correct retraction message" do
+    target_klass = target_object.class.to_s.constantize
+    post_message(alice.guid, generate_retraction(entity_name, target_object))
+
+    expect(target_klass.exists?(guid: target_object.guid)).to be(false)
+  end
+
+  it "doesn't retract object when retraction has wrong signatures" do
+    target_klass = target_object.class.to_s.constantize
+    post_message(alice.guid, generate_forged_retraction(entity_name, target_object))
+
+    expect(target_klass.exists?(guid: target_object.guid)).to be(true)
   end
 
   it "doesn't retract object when sender is different from target object" do
     target_klass = target_object.class.to_s.constantize
-    Workers::ReceiveEncryptedSalmon.new.perform(
-      alice.id,
+    post_message(
+      alice.guid,
       generate_retraction(entity_name, target_object, remote_user_on_pod_c)
     )
 
@@ -43,20 +24,20 @@ shared_examples_for "it retracts non-relayable object" do
   end
 end
 
-shared_examples_for "it retracts object" do
+shared_examples_for "it retracts relayable object" do
   it "retracts object by a correct message" do
     target_klass = target_object.class.to_s.constantize
-    Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_retraction(entity_name, target_object, sender))
+    post_message(alice.guid, generate_retraction(entity_name, target_object, sender))
 
     expect(target_klass.exists?(guid: target_object.guid)).to be(false)
   end
 
   it "doesn't retract object when retraction has wrong signatures" do
     target_klass = target_object.class.to_s.constantize
-
-    allow(sender).to receive(:encryption_key).and_return(OpenSSL::PKey::RSA.new(1024))
-
-    Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_retraction(entity_name, target_object, sender))
+    post_message(
+      alice.guid,
+      generate_forged_retraction(entity_name, target_object, sender)
+    )
 
     expect(target_klass.exists?(guid: target_object.guid)).to be(true)
   end
