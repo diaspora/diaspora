ceaec18e16b2b280acefa3bf7a6b96bcd532b5db
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-28 19:46:45 +0100

    refactoring for federation tests

--- spec/integration/federation/shared_receive_relayable.rb
+++ spec/integration/federation/shared_receive_relayable.rb
@@ -1,39 +1,93 @@
 shared_examples_for "it deals correctly with a relayable" do
-  it "treats upstream receive correctly" do
-    Workers::ReceiveEncryptedSalmon.new.perform(@user.id, generate_relayable_local_parent(entity_name))
-    received_entity = klass.find_by(guid: @entity.guid)
-    expect(received_entity).not_to be_nil
-    expect(received_entity.author.diaspora_handle).to eq(@remote_person.diaspora_handle)
+  context "local" do
+    let(:entity) {
+      FactoryGirl.build(
+        entity_name,
+        parent_guid: local_message.guid,
+        diaspora_id: remote_user_on_pod_b.diaspora_handle
+      )
+    }
+
+    def mock_private_keys
+      allow(DiasporaFederation.callbacks).to receive(:trigger)
+                                               .with(:fetch_private_key_by_diaspora_id,
+                                                     remote_user_on_pod_b.diaspora_handle)
+                                               .and_return(remote_user_on_pod_b.encryption_key)
+      allow(DiasporaFederation.callbacks).to receive(:trigger)
+                                               .with(:fetch_author_private_key_by_entity_guid, "Post", kind_of(String))
+                                               .and_return(nil)
+    end
+
+    it "treats upstream receive correctly" do
+      mock_private_keys
+
+      Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_xml(entity, remote_user_on_pod_b, alice))
+      received_entity = klass.find_by(guid: entity.guid)
+      expect(received_entity).not_to be_nil
+      expect(received_entity.author.diaspora_handle).to eq(remote_user_on_pod_b.person.diaspora_handle)
+    end
+
+    # Checks when a remote pod wants to send us a relayable without having a key for declared diaspora ID
+    it "rejects an upstream entity with a malformed author signature" do
+      allow(remote_user_on_pod_b).to receive(:encryption_key).and_return(OpenSSL::PKey::RSA.new(1024))
+      mock_private_keys
+
+      Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_xml(entity, remote_user_on_pod_b, alice))
+      expect(klass.exists?(guid: entity.guid)).to be(false)
+    end
   end
 
-  it "rejects an upstream entity with a malformed author signature" do
-    Workers::ReceiveEncryptedSalmon.new.perform(
-      @user.id,
-      generate_relayable_local_parent_wrong_author_key(entity_name)
-    )
-    expect(klass.exists?(guid: @entity.guid)).to be(false)
-  end
+  context "remote parent" do
+    let(:entity) {
+      FactoryGirl.build(
+        entity_name,
+        parent_guid: remote_message.guid,
+        diaspora_id: remote_user_on_pod_c.diaspora_handle
+      )
+    }
 
-  it "treats downstream receive correctly" do
-    Workers::ReceiveEncryptedSalmon.new.perform(@user.id, generate_relayable_remote_parent(entity_name))
-    received_entity = klass.find_by(guid: @entity.guid)
-    expect(received_entity).not_to be_nil
-    expect(received_entity.author.diaspora_handle).to eq(@remote_person2.diaspora_handle)
-  end
+    def mock_private_keys
+      allow(DiasporaFederation.callbacks).to receive(:trigger)
+                                                .with(:fetch_private_key_by_diaspora_id,
+                                                      remote_user_on_pod_c.diaspora_handle)
+                                                .and_return(remote_user_on_pod_c.encryption_key)
 
-  it "rejects a downstream entity with a malformed author signature" do
-    Workers::ReceiveEncryptedSalmon.new.perform(
-      @user.id,
-      generate_relayable_remote_parent_wrong_author_key(entity_name)
-    )
-    expect(klass.exists?(guid: @entity.guid)).to be(false)
-  end
+      allow(DiasporaFederation.callbacks).to receive(:trigger)
+                                                .with(
+                                                  :fetch_author_private_key_by_entity_guid,
+                                                  "Post",
+                                                  remote_message.guid
+                                                )
+                                                .and_return(remote_user_on_pod_b.encryption_key)
+    end
 
-  it "declines downstream receive when sender signed with a wrong key" do
-    Workers::ReceiveEncryptedSalmon.new.perform(
-      @user.id,
-      generate_relayable_remote_parent_wrong_parent_key(entity_name)
-    )
-    expect(klass.exists?(guid: @entity.guid)).to be(false)
+    it "treats downstream receive correctly" do
+      mock_private_keys
+
+      Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_xml(entity, remote_user_on_pod_b, alice))
+      received_entity = klass.find_by(guid: entity.guid)
+      expect(received_entity).not_to be_nil
+      expect(received_entity.author.diaspora_handle).to eq(remote_user_on_pod_c.diaspora_handle)
+    end
+
+    # Checks when a remote pod B wants to send us a relayable with authorship from a remote pod C user
+    # without having correct signature from him.
+    it "rejects a downstream entity with a malformed author signature" do
+      allow(remote_user_on_pod_c).to receive(:encryption_key).and_return(OpenSSL::PKey::RSA.new(1024))
+      mock_private_keys
+
+      Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_xml(entity, remote_user_on_pod_b, alice))
+      expect(klass.exists?(guid: entity.guid)).to be(false)
+    end
+
+    # Checks when a remote pod C wants to send us a relayable from its user, but bypassing the pod B where
+    # remote status came from.
+    it "declines downstream receive when sender signed with a wrong key" do
+      allow(remote_user_on_pod_b).to receive(:encryption_key).and_return(OpenSSL::PKey::RSA.new(1024))
+      mock_private_keys
+
+      Workers::ReceiveEncryptedSalmon.new.perform(alice.id, generate_xml(entity, remote_user_on_pod_b, alice))
+      expect(klass.exists?(guid: entity.guid)).to be(false)
+    end
   end
 end
