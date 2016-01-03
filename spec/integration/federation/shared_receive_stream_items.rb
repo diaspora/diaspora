812510b44afa644523fd486debd3874f1142a5ed
Benjamin Neff <benjamin@coding4coffee.ch>
2016-01-03 03:12:52 +0100

    refactoring tests again closes #6595

--- spec/integration/federation/shared_receive_stream_items.rb
+++ spec/integration/federation/shared_receive_stream_items.rb
@@ -1,73 +1,83 @@
 # by "stream items" we mean everything that could appear in the stream - post, comment, like, poll, etc and therefore
 # could be send either publicly or privately
-def set_up_messages
-  @local_target = FactoryGirl.create(:status_message, author: alice.person, public: @public)
-  @remote_target = FactoryGirl.create(:status_message, author: remote_user_on_pod_b.person, public: @public)
-end
-
 shared_examples_for "messages which are indifferent about sharing fact" do
-  it "treats status message receive correctly" do
-    post_message(alice.guid, generate_status_message)
+  let(:public) { recipient.nil? }
 
-    expect(StatusMessage.exists?(guid: @entity.guid)).to be(true)
+  it "treats status message receive correctly" do
+    entity = FactoryGirl.build(:status_message_entity, diaspora_id: sender_id, public: public)
+
+    post_message(generate_xml(entity, sender, recipient), recipient)
+
+    expect(StatusMessage.exists?(guid: entity.guid)).to be_truthy
   end
 
   it "doesn't accept status message with wrong signature" do
-    post_message(alice.guid, generate_forged_status_message)
+    allow(sender).to receive(:encryption_key).and_return(OpenSSL::PKey::RSA.new(1024))
+    entity = FactoryGirl.build(:status_message_entity, diaspora_id: sender_id, public: public)
 
-    expect(StatusMessage.exists?(guid: @entity.guid)).to be(false)
+    post_message(generate_xml(entity, sender, recipient), recipient)
+
+    expect(StatusMessage.exists?(guid: entity.guid)).to be_falsey
   end
 
   describe "with messages which require a status to operate on" do
-    before do
-      set_up_messages
-    end
+    let(:local_target) { FactoryGirl.create(:status_message, author: alice.person, public: public) }
+    let(:remote_target) { FactoryGirl.create(:status_message, author: remote_user_on_pod_b.person, public: public) }
 
     describe "notifications are sent where required" do
       it "for comment on local post" do
-        post_message(alice.guid, generate_relayable_local_parent(:comment_entity))
+        entity = create_relayable_entity(:comment_entity, local_target, remote_user_on_pod_b.diaspora_handle, nil)
+        post_message(generate_xml(entity, sender, recipient), recipient)
 
         expect(
-          Notifications::CommentOnPost.where(
+          Notifications::CommentOnPost.exists?(
             recipient_id: alice.id,
             target_type:  "Post",
-            target_id:    @local_target.id
-          ).first
-        ).not_to be_nil
+            target_id:    local_target.id
+          )
+        ).to be_truthy
       end
 
       it "for like on local post" do
-        post_message(alice.guid, generate_relayable_local_parent(:like_entity))
+        entity = create_relayable_entity(:like_entity, local_target, remote_user_on_pod_b.diaspora_handle, nil)
+        post_message(generate_xml(entity, sender, recipient), recipient)
 
         expect(
-          Notifications::Liked.where(
+          Notifications::Liked.exists?(
             recipient_id: alice.id,
             target_type:  "Post",
-            target_id:    @local_target.id
-          ).first
-        ).not_to be_nil
+            target_id:    local_target.id
+          )
+        ).to be_truthy
       end
     end
 
     %w(comment like participation).each do |entity|
       context "with #{entity}" do
-        it_behaves_like "it deals correctly with a relayable" do
-          let(:entity_name) { "#{entity}_entity".to_sym }
-          let(:klass) { entity.camelize.constantize }
-        end
+        let(:entity_name) { "#{entity}_entity".to_sym }
+        let(:klass) { entity.camelize.constantize }
+
+        it_behaves_like "it deals correctly with a relayable"
       end
     end
 
     context "with poll_participation" do
-      before do
-        @local_target = FactoryGirl.create(:poll, status_message: @local_target)
-        @remote_target = FactoryGirl.create(:poll, status_message: @remote_target)
-      end
+      let(:local_target) {
+        FactoryGirl.create(
+          :poll,
+          status_message: FactoryGirl.create(:status_message, author: alice.person, public: public)
+        )
+      }
+      let(:remote_target) {
+        FactoryGirl.create(
+          :poll,
+          status_message: FactoryGirl.create(:status_message, author: remote_user_on_pod_b.person, public: public)
+        )
+      }
+      let(:entity_name) { :poll_participation_entity }
+      let(:klass) { PollParticipation }
 
-      it_behaves_like "it deals correctly with a relayable" do
-        let(:entity_name) { :poll_participation_entity }
-        let(:klass) { PollParticipation }
-      end
+      it_behaves_like "it deals correctly with a relayable"
     end
   end
 end
@@ -75,17 +85,15 @@ end
 shared_examples_for "messages which can't be send without sharing" do
   # retractions shouldn't depend on sharing fact
   describe "retractions for non-relayable objects" do
-    %w(
-      retraction
-      signed_retraction
-    ).each do |retraction_entity_name|
+    %w(retraction signed_retraction).each do |retraction_entity_name|
       context "with #{retraction_entity_name}" do
+        let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
+
         %w(status_message photo).each do |target|
           context "with #{target}" do
-            it_behaves_like "it retracts non-relayable object" do
-              let(:target_object) { FactoryGirl.create(target.to_sym, author: remote_user_on_pod_b.person) }
-              let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
-            end
+            let(:target_object) { FactoryGirl.create(target.to_sym, author: remote_user_on_pod_b.person) }
+
+            it_behaves_like "it retracts non-relayable object"
           end
         end
       end
@@ -93,70 +101,62 @@ shared_examples_for "messages which can't be send without sharing" do
   end
 
   describe "with messages which require a status to operate on" do
-    before do
-      set_up_messages
-    end
+    let(:public) { recipient.nil? }
+    let(:local_target) { FactoryGirl.create(:status_message, author: alice.person, public: public) }
+    let(:remote_target) { FactoryGirl.create(:status_message, author: remote_user_on_pod_b.person, public: public) }
 
     # this one shouldn't depend on the sharing fact. this must be fixed
     describe "notifications are sent where required" do
       it "for comment on remote post where we participate" do
-        alice.participate!(@remote_target)
-        post_message(alice.guid, generate_relayable_remote_parent(:comment_entity))
+        alice.participate!(remote_target)
+        author_id = remote_user_on_pod_c.diaspora_handle
+        entity = create_relayable_entity(:comment_entity, remote_target, author_id, sender.encryption_key)
+        post_message(generate_xml(entity, sender, recipient), recipient)
 
         expect(
-          Notifications::AlsoCommented.where(
+          Notifications::AlsoCommented.exists?(
             recipient_id: alice.id,
             target_type:  "Post",
-            target_id:    @remote_target.id
-          ).first
-        ).not_to be_nil
+            target_id:    remote_target.id
+          )
+        ).to be_truthy
       end
     end
 
     describe "retractions for relayable objects" do
-      %w(
-        retraction
-        signed_retraction
-        relayable_retraction
-      ).each do |retraction_entity_name|
+      %w(retraction signed_retraction relayable_retraction).each do |retraction_entity_name|
         context "with #{retraction_entity_name}" do
+          let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
+
           context "with comment" do
             it_behaves_like "it retracts relayable object" do
               # case for to-upstream federation
-              let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
               let(:target_object) {
-                FactoryGirl.create(:comment, author: remote_user_on_pod_b.person, post: @local_target)
+                FactoryGirl.create(:comment, author: remote_user_on_pod_b.person, post: local_target)
               }
-              let(:sender) { remote_user_on_pod_b }
             end
 
             it_behaves_like "it retracts relayable object" do
               # case for to-downsteam federation
               let(:target_object) {
-                FactoryGirl.create(:comment, author: remote_user_on_pod_c.person, post: @remote_target)
+                FactoryGirl.create(:comment, author: remote_user_on_pod_c.person, post: remote_target)
               }
-              let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
-              let(:sender) { remote_user_on_pod_b }
             end
           end
 
           context "with like" do
             it_behaves_like "it retracts relayable object" do
               # case for to-upstream federation
-              let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
               let(:target_object) {
-                FactoryGirl.create(:like, author: remote_user_on_pod_b.person, target: @local_target)
+                FactoryGirl.create(:like, author: remote_user_on_pod_b.person, target: local_target)
               }
-              let(:sender) { remote_user_on_pod_b }
             end
 
             it_behaves_like "it retracts relayable object" do
               # case for to-downsteam federation
               let(:target_object) {
-                FactoryGirl.create(:like, author: remote_user_on_pod_c.person, target: @remote_target)
+                FactoryGirl.create(:like, author: remote_user_on_pod_c.person, target: remote_target)
               }
-              let(:entity_name) { "#{retraction_entity_name}_entity".to_sym }
-              let(:sender) { remote_user_on_pod_b }
             end
           end
         end
