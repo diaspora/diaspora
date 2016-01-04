b09ee8791278518029e2b5780bb3f9e5b6a2b322
theworldbright <kent@kentshikama.com>
2016-01-04 16:59:06 +0900

    Update json-jwt legacy methods

--- spec/controllers/api/openid_connect/id_tokens_controller_spec.rb
+++ spec/controllers/api/openid_connect/id_tokens_controller_spec.rb
@@ -10,7 +10,7 @@ describe Api::OpenidConnect::IdTokensController, type: :controller do
       json = JSON.parse(response.body).with_indifferent_access
       jwks = JSON::JWK::Set.new json[:keys]
       public_keys = jwks.map do |jwk|
-        JSON::JWK.decode jwk
+        JSON::JWK.new(jwk).to_key
       end
       public_key = public_keys.first
       expect(Api::OpenidConnect::IdTokenConfig::PUBLIC_KEY.to_s).to eq(public_key.to_s)
