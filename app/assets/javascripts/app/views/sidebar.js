a10601f9a94c26a6131d84b74217e0110bd70626
fla <flaburgan@geexxx.fr>
2016-02-28 19:57:40 +0100

    collapse left navbar sections

--- app/assets/javascripts/app/views/sidebar.js
+++ app/assets/javascripts/app/views/sidebar.js
@@ -4,11 +4,16 @@ app.views.Sidebar = app.views.Base.extend({
   el: ".info-bar",
 
   events: {
-    "click input#invite_code": "selectInputText"
+    "click input#invite_code": "selectInputText",
+    "click .section .title": "toggleSection"
   },
 
   selectInputText: function(event) {
     event.target.select();
+  },
+
+  toggleSection: function(e) {
+    $(e.target).closest(".section").toggleClass("collapsed");
   }
 });
 // @license-end
