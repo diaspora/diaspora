b398b115bcc98ce97bd05fc64b7477e13f65b109
Benjamin Neff <benjamin@coding4coffee.ch>
2016-03-06 03:20:37 +0100

    refactoring CommentService

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -1,43 +1,42 @@
 class CommentService
-  attr_reader :post, :comments
-
-  def initialize(params)
-    @user = params[:user]
-    @post_id = params[:post_id]
-    @comment_id = params[:comment_id]
-    @text = params[:text]
-
-    @post = find_post! if @post_id
-    @comments = @post.comments.for_a_stream if @post
+  def initialize(user=nil)
+    @user = user
   end
 
-  def create_comment
-    @user.comment!(post, @text) if @post
+  def create(post_id, text)
+    post = find_post!(post_id)
+    user.comment!(post, text)
   end
 
-  def destroy_comment
-    @comment = Comment.find(@comment_id)
-    if @user.owns?(@comment) || @user.owns?(@comment.parent)
-      @user.retract(@comment)
+  def destroy(comment_id)
+    comment = Comment.find(comment_id)
+    if user.owns?(comment) || user.owns?(comment.parent)
+      user.retract(comment)
       true
     else
       false
     end
   end
 
+  def find_for_post(post_id)
+    find_post!(post_id).comments.for_a_stream
+  end
+
   private
 
-  def find_post!
-    find_post.tap do |post|
-      raise(ActiveRecord::RecordNotFound) unless post
+  attr_reader :user
+
+  def find_post!(post_id)
+    find_post(post_id).tap do |post|
+      raise ActiveRecord::RecordNotFound unless post
     end
   end
 
-  def find_post
-    if @user
-      @user.find_visible_shareable_by_id(Post, @post_id)
+  def find_post(post_id)
+    if user
+      user.find_visible_shareable_by_id(Post, post_id)
     else
-      Post.find_by_id_and_public(@post_id, true)
+      Post.find_by_id_and_public(post_id, true)
     end
   end
 end
