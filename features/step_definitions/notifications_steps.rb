9a2cb1517a9f35f700f50a3870287b55bf97f87d
Steffen van Bergerem <svbergerem@online.de>
2016-08-30 16:34:06 +0200

    Set participations client side when changing post interactions
    closes #7040

--- features/step_definitions/notifications_steps.rb
+++ features/step_definitions/notifications_steps.rb
@@ -5,3 +5,14 @@ end
 When /^I filter notifications by mentions$/ do
   step %(I follow "Mentioned" within "#notifications_container .list-group")
 end
+
+Then /^I should( not)? have activated notifications for the post( in the single post view)?$/ do |negate, spv|
+  selector = spv ? "#single-post-moderation" : "#main_stream .stream_element"
+  if negate
+    expect(find(selector, match: :first)).to have_no_css(".destroy_participation", visible: false)
+    expect(find(selector, match: :first)).to have_css(".create_participation", visible: false)
+  else
+    expect(find(selector, match: :first)).to have_css(".destroy_participation", visible: false)
+    expect(find(selector, match: :first)).to have_no_css(".create_participation", visible: false)
+  end
+end
