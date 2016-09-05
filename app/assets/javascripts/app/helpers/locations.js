0e5141dd678602f16a2adc8d9081e2dd7d95b41f
Benjamin Neff <benjamin@coding4coffee.ch>
2016-09-06 01:24:56 +0200

    Fix mapbox API URL
    closes #7066

--- app/assets/javascripts/app/helpers/locations.js
+++ app/assets/javascripts/app/helpers/locations.js
@@ -3,9 +3,9 @@
     getTiles: function() {
       // If the mapbox option is enabled in the diaspora.yml, the mapbox tiles with the podmin's credentials are used.
       if (gon.appConfig.map.mapbox.enabled) {
-        return L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-          id: gon.appConfig.map.mapbox.id,
+        return L.tileLayer("https://api.mapbox.com/styles/v1/{style}/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
           accessToken: gon.appConfig.map.mapbox.access_token,
+          style: gon.appConfig.map.mapbox.style,
           attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
                        "<a href='http://creativecommons.org/licenses/by-sa/2.0/''>CC-BY-SA</a>, " +
                        "Imagery © <a href='https://www.mapbox.com'>Mapbox</a>",
