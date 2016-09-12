a3fd949df71ce509fb3f6dda46bc6e8979eb9b99
Steffen van Bergerem <svbergerem@online.de>
2016-09-12 10:51:07 +0200

    Revert "Use bundler 1.12.5 on travis"
    This reverts commit 5fcc239d7cc35173835ea8cc0ee116fdd798caa1.

--- script/ci/build.sh
+++ script/ci/build.sh
@@ -8,6 +8,6 @@ if [ "$DB" = "mysql" ]; then
   sed -i 's/*common/*mysql/' config/database.yml
 fi
 
-command="bundle _1.12.5_ exec rake --trace ci:travis:${BUILD_TYPE}"
+command="bundle exec rake --trace ci:travis:${BUILD_TYPE}"
 
 exec xvfb-run --auto-servernum --server-num=1 --server-args="-screen 0 1280x1024x8" $command
