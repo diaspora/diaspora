ad5781c920b9f400324eb057a976cc9a7ac23c40
hilkoc <hilkoc@users.noreply.github.com>
2016-09-18 23:32:39 +0100

    Cuke test for post default aspects

--- features/step_definitions/aspects_steps.rb
+++ features/step_definitions/aspects_steps.rb
@@ -4,7 +4,11 @@ module AspectCukeHelpers
   end
 
   def toggle_aspect(a_name)
-    a_id = @me.aspects.where(name: a_name).pluck(:id).first
+    if "Public" == a_name
+      a_id = "public"
+    else
+      a_id = @me.aspects.where(name: a_name).pluck(:id).first
+    end
     aspect_css = ".aspect_dropdown li[data-aspect_id='#{a_id}']"
     expect(page).to have_selector(aspect_css)
     find(aspect_css).click
