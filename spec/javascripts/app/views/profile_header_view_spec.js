b7e2df47fddb837b552977bd950025a80efac4b5
Steffen van Bergerem <svbergerem@omgsrsly.net>
2016-11-12 21:31:25 +0100

    Only reload profile header when changing aspect memberships

--- spec/javascripts/app/views/profile_header_view_spec.js
+++ spec/javascripts/app/views/profile_header_view_spec.js
@@ -11,6 +11,34 @@ describe("app.views.ProfileHeader", function() {
     loginAs(factory.userAttrs());
   });
 
+  describe("initialize", function() {
+    it("calls #render when the model changes", function() {
+      spyOn(app.views.ProfileHeader.prototype, "render");
+      this.view.initialize();
+      expect(app.views.ProfileHeader.prototype.render).not.toHaveBeenCalled();
+      this.view.model.trigger("change");
+      expect(app.views.ProfileHeader.prototype.render).toHaveBeenCalled();
+    });
+
+    it("calls #mentionModalLoaded on modal:loaded", function() {
+      spec.content().append("<div id='mentionModal'></div>");
+      spyOn(app.views.ProfileHeader.prototype, "mentionModalLoaded");
+      this.view.initialize();
+      expect(app.views.ProfileHeader.prototype.mentionModalLoaded).not.toHaveBeenCalled();
+      $("#mentionModal").trigger("modal:loaded");
+      expect(app.views.ProfileHeader.prototype.mentionModalLoaded).toHaveBeenCalled();
+    });
+
+    it("calls #mentionModalHidden on hidden.bs.modal", function() {
+      spec.content().append("<div id='mentionModal'></div>");
+      spyOn(app.views.ProfileHeader.prototype, "mentionModalHidden");
+      this.view.initialize();
+      expect(app.views.ProfileHeader.prototype.mentionModalHidden).not.toHaveBeenCalled();
+      $("#mentionModal").trigger("hidden.bs.modal");
+      expect(app.views.ProfileHeader.prototype.mentionModalHidden).toHaveBeenCalled();
+    });
+  });
+
   context("#presenter", function() {
     it("contains necessary elements", function() {
       expect(this.view.presenter()).toEqual(jasmine.objectContaining({
