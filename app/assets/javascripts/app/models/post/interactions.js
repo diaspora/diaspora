f4481dd4d2892b7bf52a408b1269ac95fa73d02f
Steffen van Bergerem <svbergerem@omgsrsly.net>
2016-11-13 21:15:00 +0100

    Refactor comment creation in post interactions backbone model

--- app/assets/javascripts/app/models/post/interactions.js
+++ app/assets/javascripts/app/models/post/interactions.js
@@ -100,11 +100,10 @@ app.models.Post.Interactions = Backbone.Model.extend({
       if (options.error) { options.error(); }
     }).done(function() {
       self.post.set({participation: true});
+      self.set({"comments_count": self.get("comments_count") + 1});
       self.trigger('change'); //updates after sync
       if (options.success) { options.success(); }
     });
-
-    this.trigger("change"); //updates count in an eager manner
 
     app.instrument("track", "Comment");
   },
