05928d16bb0e8b568eb99f5d9387d79026415c4f
Justin Ramos <justin@southernmade.co>
2016-11-19 01:00:30 +0000

    add missing indexes on several tables

---
+++ db/migrate/20161118235506_add_missing_indexes_across_several_tables.rb
@@ -0,0 +1,26 @@
+class AddMissingIndexesAcrossSeveralTables < ActiveRecord::Migration
+  def change
+    add_index :account_deletions, :person_id
+    add_index :aspects, %i(user_id order_id)
+    add_index :authorizations, %i(user_id o_auth_application_id)
+    add_index :blocks, :person_id
+    add_index :blocks, :user_id
+    add_index :blocks, %i(person_id user_id)
+    add_index :invitation_codes, :user_id
+    add_index :locations, :status_message_id
+    add_index :notifications, %i(id type)
+    add_index :photos, :author_id
+    add_index :poll_participations, :author_id
+    add_index :poll_participations, :poll_answer_id
+    add_index :posts, :facebook_id
+    add_index :posts, :o_embed_cache_id
+    add_index :posts, :open_graph_cache_id
+    add_index :ppid, :guid, unique: true
+    add_index :reports, :user_id
+    add_index :reports, %i(item_id item_type)
+    add_index :roles, :person_id
+    add_index :user_preferences, :user_id
+    add_index :users, :auto_follow_back_aspect_id
+    add_index :users, :invited_by_id
+  end
+end
