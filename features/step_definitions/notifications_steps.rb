efa9e181104a1de406f95fc9c4a6c939cfefd4ec
Steffen van Bergerem <svbergerem@omgsrsly.net>
2016-11-21 00:49:32 +0100

    Fix notification cukes

--- features/step_definitions/notifications_steps.rb
+++ features/step_definitions/notifications_steps.rb
@@ -1,8 +1,8 @@
-When /^I filter notifications by likes$/ do
+When "I filter notifications by likes" do
   step %(I follow "Liked" within "#notifications_container .list-group")
 end
 
-When /^I filter notifications by mentions$/ do
+When "I filter notifications by mentions" do
   step %(I follow "Mentioned" within "#notifications_container .list-group")
 end
 
@@ -18,31 +18,30 @@ Then /^I should( not)? have activated notifications for the post( in the single 
 end
 
 And "I wait for notifications to load" do
-  page.should_not have_selector(".loading")
+  expect(find("#notification-dropdown")).to have_no_css(".loading")
 end
 
-And /^I scroll down on the notifications dropdown$/ do
+And "I scroll down on the notifications dropdown" do
   page.execute_script("$('.notifications').scrollTop(350)")
 end
 
-Then /^I should have scrolled down on the notification dropdown$/ do
-  expect(page.evaluate_script("$('.notifications').scrollTop()")).to be > 0
+Then "the notification dropdown should load more notifications" do
+  expect(find("#notification-dropdown")).to have_css(".loading")
 end
 
-Then /^the notification dropdown should be visible$/ do
+Then "the notification dropdown should be visible" do
   expect(find(:css, "#notification-dropdown")).to be_visible
 end
 
-Then /^the notification dropdown scrollbar should be visible$/ do
-  find(:css, ".ps-active-y").should be_visible
+Then "the notification dropdown scrollbar should be visible" do
+  expect(find(:css, ".ps-active-y")).to be_visible
 end
 
 Then /^there should be (\d+) notifications loaded$/ do |n|
-  result = page.evaluate_script("$('.media.stream-element').length")
-  result.should == n.to_i
+  expect(page).to have_css("#notification-dropdown .media.stream-element", count: n)
 end
 
-And /^I activate the first hovercard after loading the notifications page$/ do
-  page.should have_css '.notifications .hovercardable'
-  first('.notifications .hovercardable').hover
+When "I activate the first hovercard in the notification dropdown" do
+  expect(page).to have_css("#notification-dropdown .hovercardable")
+  first("#notification-dropdown .hovercardable").hover
 end
