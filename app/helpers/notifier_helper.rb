4e6a9aeb1e3eafe8978f0a10af6711532b42cd2d
Benjamin Neff <benjamin@coding4coffee.ch>
2017-01-17 20:28:30 +0100

    Fallback to title if the reshare root was deleted
    closes #7284

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -5,7 +5,7 @@ module NotifierHelper
   # @return [String] The formatted post.
   def post_message(post, opts={})
     if post.respond_to? :message
-      post.message.plain_text_without_markdown
+      post.message.try(:plain_text_without_markdown) || post_page_title(post)
     else
       I18n.translate 'notifier.a_post_you_shared'
     end
