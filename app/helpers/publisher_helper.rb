a59be59af7c44950576cbd68750b8040f87208d7
Benjamin Neff <benjamin@coding4coffee.ch>
2017-02-06 03:39:13 +0100

    Refactor PublisherHelper and move publisher_open? method

--- app/helpers/publisher_helper.rb
+++ app/helpers/publisher_helper.rb
@@ -3,24 +3,18 @@
 #   the COPYRIGHT file.
 
 module PublisherHelper
-  def remote?
-    params[:controller] != "tags"
-  end
-
   def service_button(service)
-    provider_title = I18n.t(
-      "services.index.share_to",
-      provider: service.provider.titleize)
+    provider_title = I18n.t("services.index.share_to", provider: service.provider.titleize)
     content_tag :div,
                 class:   "btn btn-link service_icon dim",
                 title:   "#{provider_title} (#{service.nickname})",
-                id:      "#{service.provider}",
-                maxchar: "#{service.class::MAX_CHARACTERS}",
+                id:      service.provider,
+                maxchar: service.class::MAX_CHARACTERS,
                 data:    {toggle: "tooltip", placement: "bottom"} do
       if service.provider == "wordpress"
         content_tag(:span, "", class: "social-media-logos-wordpress-16x16")
       else
-        content_tag(:i, "", class: "entypo-social-#{ service.provider } small")
+        content_tag(:i, "", class: "entypo-social-#{service.provider} small")
       end
     end
   end
@@ -35,6 +29,10 @@ module PublisherHelper
 
   def aspect_selected?(aspect, selected_aspects)
     selected_aspects.include?(aspect) && !all_aspects_selected?(selected_aspects) && !public_selected?(selected_aspects)
+  end
+
+  def publisher_open?
+    publisher_boolean?(:open)
   end
 
   private
