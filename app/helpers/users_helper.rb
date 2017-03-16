7eb3dabb444d99cf51e672e065284f9aa23628e9
Steffen van Bergerem <svbergerem@omgsrsly.net>
2017-03-16 12:25:41 +0100

    i18n for color themes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -31,13 +31,13 @@ module UsersHelper
   # specified from AVAILABLE_COLOR_THEMES in
   # config/initializers/color_themes.rb.
   #
-  # @example if AVAILABLE_COLOR_THEMES = {"original"=>"Original dark", "dark_green" => "Dark green"}
+  # @example if AVAILABLE_COLOR_THEMES = ["original", "dark_green"]
   #   available_color_themes
-  #   #=> [["Original dark", "original"], ["Dark green", "dark_green"]]
+  #   #=> [["Original gray", "original"], ["Dark green", "dark_green"]]
   def available_color_themes
     opts = []
-    AVAILABLE_COLOR_THEMES.map do |theme_code, theme_name|
-      opts << [theme_name, theme_code]
+    AVAILABLE_COLOR_THEMES.map do |theme_code|
+      opts << [I18n.t("color_themes.#{theme_code}"), theme_code]
     end
     opts
   end
