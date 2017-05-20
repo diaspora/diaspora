40d42f4b58ddc909a4addf3172f4645ecc26e6d0
Frank Rousseau <frank.rousseau@free.fr>
2017-05-21 00:31:30 +0200

    Add conversation presenter

---
+++ spec/presenters/conversation_presenter.rb
@@ -0,0 +1,12 @@
+describe ConversationPresenter do
+  before do
+    @conversation = FactoryGirl.create(:conversation)
+    @presenter = ConversationPresenter.new(@conversation)
+  end
+
+  describe "#as_json" do
+    it "works" do
+      expect(@presenter.as_json).to be_a Hash
+    end
+  end
+end
