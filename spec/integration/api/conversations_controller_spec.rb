13e2841f13b9fd64d6de297a1a0046cdd694a7d4
Frank Rousseau <frank.rousseau@free.fr>
2017-06-03 12:32:03 +0200

    Merge conv deletion and visibility deletion

--- spec/integration/api/conversations_controller_spec.rb
+++ spec/integration/api/conversations_controller_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe Api::V0::ConversationsController do
   let(:auth) { FactoryGirl.create(:auth_with_read_and_write) }
   let!(:access_token) { auth.create_access_token.to_s }
+  let(:auth_participant) { FactoryGirl.create(:auth_with_read_and_write) }
+  let!(:access_token_participant) { auth_participant.create_access_token.to_s }
 
   before do
     auth.user.share_with bob.person, auth.user.aspects[0]
@@ -86,24 +88,66 @@ describe Api::V0::ConversationsController do
     end
   end
 
-  describe "#delete" do
-    context "valid conversation GUID" do
-      before do
-        post api_v0_conversations_path, @conversation
-      end
+  describe "#destroy " do
+    before do
+      auth.user.seed_aspects
+      auth.user.share_with auth_participant.user.person, auth.user.aspects[1]
+      auth_participant.user.share_with(
+        auth.user.person, auth_participant.user.aspects[0]
+      )
 
-      it "deletes the conversation" do
-        conversation_guid = JSON.parse(response.body)["conversation"]["guid"]
+      @conversation = {
+        author_id:    auth.user.id,
+        subject:      "new conversation",
+        body:         "first message",
+        recipients:   [auth_participant.user.person.id],
+        access_token: access_token
+      }
+      post api_v0_conversations_path, @conversation
+      @conversation_guid = JSON.parse(response.body)["conversation"]["guid"]
+    end
+
+    context "destroy" do
+      it "destroys the first participant visibility" do
         delete(
-          api_v0_conversation_path(conversation_guid),
+          api_v0_conversation_path(@conversation_guid),
           access_token: access_token
         )
         expect(response.status).to eq 204
-        get(
-          api_v0_conversation_path(conversation_guid),
+        get api_v0_conversation_path(
+          @conversation_guid,
           access_token: access_token
         )
         expect(response.status).to eq 404
+        get api_v0_conversation_path(
+          @conversation_guid,
+          access_token: access_token_participant
+        )
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "destroy all visibilities" do
+      it "destroys the second participant visibilty and the conversation" do
+        delete(
+          api_v0_conversation_path(@conversation_guid),
+          access_token: access_token
+        )
+        delete(
+          api_v0_conversation_path(@conversation_guid),
+          access_token: access_token_participant
+        )
+        expect(response.status).to eq 204
+
+        get api_v0_conversation_path(
+          @conversation_guid,
+          access_token: access_token_participant
+        )
+        expect(response.status).to eq 404
+
+        expect {
+          Conversation.find({guid: @conversation_guid})
+        }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
 
