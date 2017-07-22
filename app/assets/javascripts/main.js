7e9935c8b2151f36ff5a9605395e2dda0214dfd4
Benjamin Neff <benjamin@coding4coffee.ch>
2017-07-22 05:38:24 +0200

    Replace jquery-ui-rails with rails-assets-jquery.ui

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,10 +10,10 @@
 //= require charcount
 //= require jquery-placeholder
 //= require jquery.timeago
-//= require jquery-ui/core
-//= require jquery-ui/widget
-//= require jquery-ui/mouse
-//= require jquery-ui/sortable
+//= require jquery.ui/ui/core
+//= require jquery.ui/ui/widget
+//= require jquery.ui/ui/mouse
+//= require jquery.ui/ui/sortable
 //= require keycodes
 //= require jquery.autoSuggest.custom
 //= require fine-uploader/fine-uploader.core
