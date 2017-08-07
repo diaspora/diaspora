b203862ca3e748edb3a19446a8433568ea223aff
Benjamin Neff <benjamin@coding4coffee.ch>
2017-08-07 03:11:53 +0200

    Workaround for bootstrap-sass
    The change in assets.rb should be enough, but it doesn't work, because
    sprockets `after_initialize` runs before it and initializes sprockets
    with unfiltered paths.
    
    But the trick with the underscore works, because bootstrap-sass has
    named the file `_bootstrap.scss`, and rails-assets-bootstrap has
    `bootstrap.scss`, so with `_bootstrap` it uses the correct bootstrap.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,16 @@
 # bootstrap-markdown plugin relies on rails-assets-bootstrap gem but we use
 # bootstrap-sass this line makes sure we exclude every asset comming
 # from rails-assets-bootstrap to prevent conflicts with bootstrap-sass
-Rails.configuration.assets.paths.reject! do |path|
-  path.include?("rails-assets-bootstrap") && !path.include?("rails-assets-bootstrap-markdown")
+
+# See https://github.com/tenex/rails-assets/issues/314
+Rails.application.config.after_initialize do
+  # add the gem names you wish to reject to the below array
+  excluded_gem_names = ["rails-assets-bootstrap"]
+
+  excluded_gem_full_names = Gem::Specification.select {|g| excluded_gem_names.include? g.name }.flat_map(&:full_name)
+  Rails.application.config.assets.paths.reject! do |path|
+    excluded_gem_full_names.any? {|gem_name| path.include? gem_name }
+  end
 end
 
 # Version of your assets, change this if you want to expire all your assets.
