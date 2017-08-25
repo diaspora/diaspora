1ea5c67f29919764dcf49e9f7207d0bafe800f9e
Benjamin Neff <benjamin@coding4coffee.ch>
2017-08-25 02:01:55 +0200

    Merge migrations from before 0.6.0.0 to 0000_create_schema.rb

--- db/migrate/20141007003922_add_chat_enabled_to_aspects.rb
+++
@@ -1,9 +0,0 @@
-class AddChatEnabledToAspects < ActiveRecord::Migration[4.2]
-  def self.up
-    add_column :aspects, :chat_enabled, :boolean, default: false
-  end
-
-  def self.down
-    remove_column :aspects, :chat_enabled
-  end
-end
