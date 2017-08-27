32058f271a3847ccd9106b8e51c2647ba37b4b0c
Benjamin Neff <benjamin@coding4coffee.ch>
2017-08-27 18:17:55 +0200

    Fix Diaspora::Camo.from_markdown with frozen strings

--- lib/diaspora/camo.rb
+++ lib/diaspora/camo.rb
@@ -5,7 +5,7 @@ module Diaspora
   module Camo
     def self.from_markdown(markdown_text)
       return unless markdown_text
-      markdown_text.gsub!(/(!\[(.*?)\]\s?\([ \t]*()<?(\S+?)>?[ \t]*((['"])(.*?)\6[ \t]*)?\))/m) do |link|
+      markdown_text = markdown_text.gsub(/(!\[(.*?)\]\s?\([ \t]*()<?(\S+?)>?[ \t]*((['"])(.*?)\6[ \t]*)?\))/m) do |link|
         link.gsub($4, self.image_url($4))
       end
       markdown_text.gsub(/src=(['"])(.+?)\1/m) do |link|
