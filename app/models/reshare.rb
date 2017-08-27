ca8dce5766b940deebde35e780f3513eb53db49e
Benjamin Neff <benjamin@coding4coffee.ch>
2017-08-27 20:41:56 +0200

    Allow multiple reshares without root
    Follow-up for #7578
    
    Fixes #7587

--- app/models/reshare.rb
+++ app/models/reshare.rb
@@ -6,7 +6,7 @@ class Reshare < Post
   belongs_to :root, class_name: "Post", foreign_key: :root_guid, primary_key: :guid, optional: true
   validate :root_must_be_public
   validates_presence_of :root, :on => :create
-  validates_uniqueness_of :root_guid, :scope => :author_id
+  validates :root_guid, uniqueness: {scope: :author_id}, allow_nil: true
   delegate :author, to: :root, prefix: true
 
   before_validation do
