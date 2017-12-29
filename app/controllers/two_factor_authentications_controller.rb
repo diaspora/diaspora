e94055000be3975ee19de03646017cdf3e5f5b7a
lislis <mail@lislis.de>
2017-12-30 00:27:18 +0100

    implement basic 2fa
    fix template error
    
    fix typos, revert .ruby-version
    
    make changes to Gemfile
    
    rm unused #create
    
    update with style info [skip ci]
    
    put 2fa encryption key in config, create rake task
    
    fix lint error
    
    start extracting locales
    
    change OTP label to `pod_url username`
    
    more locale extraction and re-arrangement
    
    two factor when signing in
    
    add a default 2fa encryption key that can be regenerated
    
    tweak token input fields
    
    fix some style errors
    
    try setting up controller tests
    
    fix style error
    
    add two_factor_controller_tests
    
    fix pronto secret token initializer
    
    add twofa_encryption_key.rb to .gitignore
    
    rm call to super from session_controller
    
    add twofa_encryption_token to configuration_methods
    
    some style fixes
    
    explicitly call system()
    
    change :string -> :text

---
+++ app/controllers/two_factor_authentications_controller.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+class TwoFactorAuthenticationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_otp_required, only: [:create]
+
+  def show
+    @user = current_user
+  end
+
+  def create
+    current_user.otp_secret = User.generate_otp_secret(32)
+    current_user.save!
+    redirect_to confirm_two_factor_authentication_path
+  end
+
+  def confirm_2fa
+    @qrcode_uri = qrcode_uri
+  end
+
+  def confirm_and_activate_2fa
+    if current_user.validate_and_consume_otp!(params[:user][:code])
+      current_user.otp_required_for_login = true
+      current_user.save!
+
+      flash[:notice] = t("two_factor_auth.flash.success_activation")
+      redirect_to recovery_codes_two_factor_authentication_path
+    else
+      flash[:alert] = t("two_factor_auth.flash.error_token")
+      redirect_to confirm_two_factor_authentication_path
+    end
+  end
+
+  def recovery_codes
+    @recovery_codes = current_user.generate_otp_backup_codes!
+    current_user.save!
+  end
+
+  def destroy
+    if acceptable_code?
+      current_user.otp_required_for_login = false
+      current_user.save!
+      flash[:notice] = t("two_factor_auth.flash.success_deactivation")
+    else
+      flash.now[:alert] = t("two_factor_auth.flash.error_token")
+    end
+    redirect_to two_factor_authentication_path
+  end
+
+  private
+
+  def qrcode_uri
+    pod = AppConfig.environment.url
+    label = "#{pod} #{current_user.username}"
+    current_user.otp_provisioning_uri(label, issuer: pod)
+  end
+
+  def verify_otp_required
+    redirect_to two_factor_authentication_path if current_user.otp_required_for_login?
+  end
+
+  def acceptable_code?
+    current_user.validate_and_consume_otp!(params[:two_factor_authentication][:code]) ||
+      current_user.invalidate_otp_backup_code!(params[:two_factor_authentication][:code])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,15 @@ class User < ApplicationRecord
   scope :halfyear_actives, ->(time = Time.now) { logged_in_since(time - 6.month) }
   scope :active, -> { joins(:person).where(people: {closed_account: false}) }
 
-  devise :database_authenticatable, :registerable,
+  attribute :otp_secret
+
+  devise :two_factor_authenticatable,
+         :two_factor_backupable,
+         otp_secret_encryption_key: AppConfig.twofa_encryption_key,
+         otp_backup_code_length: 16,
+         otp_number_of_backup_codes: 10
+
+  devise :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable, :lastseenable, :lock_strategy => :none, :unlock_strategy => :none
 
