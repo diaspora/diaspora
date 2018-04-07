728262c60167916ba0ae13cdc2479a1630b99cd1
Benjamin Neff <benjamin@coding4coffee.ch>
2018-04-08 01:50:37 +0200

    Bump fixture_builder

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # for fixture_builder
+  ENV["FIXTURES_PATH"] = "spec/fixtures"
 end
