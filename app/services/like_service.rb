df1129765475875e0c76be6523cb8731ec8f49d2
Frank Rousseau <frank.rousseau@free.fr>
2018-04-07 19:48:23 +0200

    Fix like deletion

--- app/services/like_service.rb
+++ app/services/like_service.rb
@@ -28,7 +28,7 @@ class LikeService
   def unlike_post(post_id)
     likes = post_service.find!(post_id).likes
     likes = likes.order("author_id = #{user.person.id} DESC")
-    if likes.empty? && user.owns?(likes[0])
+    if !likes.empty? && user.owns?(likes[0])
       user.retract(likes[0])
       true
     else
