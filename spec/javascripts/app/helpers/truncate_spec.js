f4ea138a642b44c8bee4099ba8d8baccb7e112b5
Benjamin Neff <benjamin@coding4coffee.ch>
2018-04-10 01:49:37 +0200

    Don't return a SafeString after truncate

--- spec/javascripts/app/helpers/truncate_spec.js
+++ spec/javascripts/app/helpers/truncate_spec.js
@@ -6,4 +6,16 @@ describe("app.helpers.truncate", function() {
   it("handles undefined", function() {
     expect(app.helpers.truncate(undefined, 123)).toEqual(undefined);
   });
+
+  it("returns a short string", function() {
+    expect(app.helpers.truncate("Some text", 10)).toEqual("Some text");
+  });
+
+  it("trims a long string at a space", function() {
+    expect(app.helpers.truncate("Some very long text", 10)).toEqual("Some very ...");
+  });
+
+  it("returns a string", function() {
+    expect(typeof app.helpers.truncate("Some very long text", 10)).toEqual("string");
+  });
 });
