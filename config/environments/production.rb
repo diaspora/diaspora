122e497332dc4ed4df13397760b864c5894b15e7
Benjamin Neff <benjamin@coding4coffee.ch>
2018-09-07 04:46:43 +0200

    Fix i18n fallbacks
    Fallbacks are set in config/initializers/locale.rb and this settings
    conflicts with the fallbacks set there.
    
    Fixes #7860

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,10 +74,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
