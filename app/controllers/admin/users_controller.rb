311fb70d7ee96c9a2b792b885f661fb7416948d6
Zachary Rabinovich <zachrab@Zacharys-MacBook-Pro.local>
2018-09-15 10:32:12 -0700

    POC for setting user roles in admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,7 @@
 
 module Admin
   class UsersController < AdminController
+    before_action :validate_user, only: %i(make_admin remove_admin make_moderator remove_moderator make_spotlight remove_spotlight)
 
     def close_account
       u = User.find(params[:id])
@@ -21,5 +22,71 @@ module Admin
       redirect_to user_search_path, notice: t("admins.user_search.account_unlocking_scheduled", name: u.username)
     end
 
+    def make_admin
+      unless Role.is_admin? @user.person
+        Role.add_admin @user.person
+        notice = "admins.user_search.add_admin"
+      else
+        notice = "admins.user_search.role_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    def remove_admin
+      if Role.is_admin? @user.person
+        Role.remove_admin @user.person
+        notice = "admins.user_search.delete_admin"
+      else
+        notice = "admins.user_search.role_removal_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    def make_moderator
+      unless Role.moderator_only? @user.person
+        Role.add_moderator @user.person
+        notice = "admins.user_search.add_moderator"
+      else
+        notice = "admins.user_search.role_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    def remove_moderator
+      if Role.moderator_only? @user.person
+        Role.remove_moderator @user.person
+        notice = "admins.user_search.delete_moderator"
+      else
+        notice = "admins.user_search.role_removal_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    def make_spotlight
+      unless Role.spotlight? @user.person
+        Role.add_spotlight @user.person
+        notice = "admins.user_search.add_spotlight"
+      else
+        notice = "admins.user_search.role_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    def remove_spotlight
+      if Role.spotlight? @user.person
+        Role.remove_spotlight @user.person
+        notice = "admins.user_search.delete_spotlight"
+      else
+        notice = "admins.user_search.role_removal_implemented"
+      end
+      redirect_to user_search_path, notice: t(notice, name: @user.username)
+    end
+
+    private
+
+    def validate_user
+      @user = User.where(id: params[:id]).first
+      redirect_to user_search_path, notice: t("admins.user_search.does_not_exist") unless @user
+    end
   end
 end
