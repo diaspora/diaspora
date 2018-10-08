0638619f81fa10e2efb1b362ad408e7e3ef4640b
Hank Grabowski <hankgrabowski@gmail.com>
2018-10-07 20:10:52 -0400

    7878 directly pasting images into edit box and broader drag and drop areas

--- app/assets/javascripts/jasmine-load-all.js
+++ app/assets/javascripts/jasmine-load-all.js
@@ -1,7 +1,7 @@
 //= require jquery3
 //= require handlebars.runtime
 //= require main
-//= require fine-uploader/fine-uploader.core
+//= require fine-uploader/fine-uploader
 //= require mobile/mobile
 //= require jquery.autoSuggest.custom
 //= require contact-list
