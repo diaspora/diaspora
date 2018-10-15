d6915ff5d08842d78903928e819f4d61ab4d130f
Hank Grabowski <hankgrabowski@gmail.com>
2018-10-15 10:36:34 -0400

    Likes API endpoint complete

--- spec/integration/api/likes_controller_spec.rb
+++ spec/integration/api/likes_controller_spec.rb
@@ -15,6 +15,47 @@ describe Api::V1::LikesController do
     )
   end
 
+  describe "#show" do
+    context "with right post id" do
+      it "succeeds in getting empty likes" do
+        get(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(200)
+        likes = response_body(response)
+        expect(likes.length).to eq(0)
+      end
+
+      it "succeeds in getting post with likes" do
+        like_service(bob).create(@status.guid)
+        like_service(auth.user).create(@status.guid)
+        like_service(alice).create(@status.guid)
+        get(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(200)
+        likes = response_body(response)
+        expect(likes.length).to eq(3)
+        confirm_like_format(likes, alice)
+        confirm_like_format(likes, bob)
+        confirm_like_format(likes, auth.user)
+      end
+    end
+
+    context "with wrong post id" do
+      it "fails at getting likes" do
+        get(
+          api_v1_post_likes_path(post_id: "badguid"),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(404)
+        expect(response.body).to eq(I18n.t("api.endpoint_errors.posts.post_not_found"))
+      end
+    end
+  end
+
   describe "#create" do
     context "with right post id" do
       it "succeeeds in liking post" do
@@ -23,6 +64,25 @@ describe Api::V1::LikesController do
           params: {access_token: access_token}
         )
         expect(response.status).to eq(204)
+        likes = like_service.find_for_post(@status.guid)
+        expect(likes.length).to eq(1)
+        expect(likes[0].author.id).to eq(auth.user.person.id)
+      end
+
+      it "fails in liking already liked post" do
+        post(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(204)
+
+        post(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(422)
+        expect(response.body).to eq(I18n.t("api.endpoint_errors.likes.like_exists"))
+
         likes = like_service.find_for_post(@status.guid)
         expect(likes.length).to eq(1)
         expect(likes[0].author.id).to eq(auth.user.person.id)
@@ -36,11 +96,12 @@ describe Api::V1::LikesController do
           params: {access_token: access_token}
         )
         expect(response.status).to eq(404)
+        expect(response.body).to eq(I18n.t("api.endpoint_errors.posts.post_not_found"))
       end
     end
   end
 
-  describe "#create" do
+  describe "#delete" do
     before do
       post(
         api_v1_post_likes_path(post_id: @status.guid),
@@ -58,6 +119,24 @@ describe Api::V1::LikesController do
         likes = like_service.find_for_post(@status.guid)
         expect(likes.length).to eq(0)
       end
+
+      it "fails at unliking post user didn't like" do
+        delete(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(204)
+
+        delete(
+          api_v1_post_likes_path(post_id: @status.guid),
+          params: {access_token: access_token}
+        )
+        expect(response.status).to eq(404)
+        expect(response.body).to eq(I18n.t("api.endpoint_errors.likes.no_like"))
+
+        likes = like_service.find_for_post(@status.guid)
+        expect(likes.length).to eq(0)
+      end
     end
 
     context "with wrong post id" do
@@ -67,11 +146,28 @@ describe Api::V1::LikesController do
           params: {access_token: access_token}
         )
         expect(response.status).to eq(404)
+        expect(response.body).to eq(I18n.t("api.endpoint_errors.posts.post_not_found"))
       end
     end
   end
 
-  def like_service
-    LikeService.new(auth.user)
+  private
+
+  # rubocop:disable Metrics/AbcSize
+  def confirm_like_format(likes, user)
+    like = likes.find {|like_element| like_element["author"]["guid"] == user.guid }
+    author = like["author"]
+    expect(author["diaspora_id"]).to eq(user.diaspora_handle)
+    expect(author["name"]).to eq(user.name)
+    expect(author["avatar"]).to eq(user.profile.image_url)
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def like_service(user=auth.user)
+    LikeService.new(user)
+  end
+
+  def response_body(response)
+    JSON.parse(response.body)
   end
 end
