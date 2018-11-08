32cd764786ed33c50765ef213ddef8f1d0897353
Benjamin Neff <benjamin@coding4coffee.ch>
2018-11-08 19:49:56 +0100

    Fix XSS via OpenGraph on mobile

--- app/helpers/open_graph_helper.rb
+++ app/helpers/open_graph_helper.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 module OpenGraphHelper
-  def og_html(cache)
-    "<a href=\"#{cache.url}\" target=\"_blank\">" +
-    "  <div>" +
-    "    <img src=\"#{cache.image}\" />" +
-    "    <strong>#{cache.title}</strong>" +
-    "    <p>#{truncate(cache.description, length: 250, separator: ' ')}</p>" +
-    "  </div>" +
-    "</a>"
-  end
-
   def link_to_oembed_image(cache, prefix = 'thumbnail_')
     link_to(oembed_image_tag(cache, prefix), cache.url, :target => '_blank')
   end
