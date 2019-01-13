3cd67ef10c2186d1eeb6c8a20986577c5cddbffc
flaburgan <flaburgan@geexxx.fr>
2019-01-14 00:31:38 +0100

    introduce like-interactions.js

--- app/assets/javascripts/app/models/comment.js
+++ app/assets/javascripts/app/models/comment.js
@@ -4,50 +4,7 @@ app.models.Comment = Backbone.Model.extend({
   urlRoot: "/comments",
 
   initialize: function() {
-    this.likes = new app.collections.Likes(this.get("likes"), {comment: this});
-  },
-
-  // Copied from Post.Interaction. To be merged in an "interactable" class once comments can be commented too
-  likesCount: function() {
-    return this.get("likes_count");
-  },
-
-  userLike: function() {
-    return this.likes.select(function(like) {
-      return like.get("author") && like.get("author").guid === app.currentUser.get("guid");
-    })[0];
-  },
-
-  toggleLike: function() {
-    if (this.userLike()) {
-      this.unlike();
-    } else {
-      this.like();
-    }
-  },
-
-  like: function() {
-    var self = this;
-    this.likes.create({}, {
-      success: function() {
-        self.post.set({participation: true});
-        self.trigger("change");
-        self.set({"likes_count": self.get("likes_count") + 1});
-        self.likes.trigger("change");
-      },
-      error: function(model, response) {
-        app.flashMessages.handleAjaxError(response);
-      }
-    });
-  },
-
-  unlike: function() {
-    var self = this;
-    this.userLike().destroy({success: function() {
-      self.trigger("change");
-      self.set({"likes_count": self.get("likes_count") - 1});
-      self.likes.trigger("change");
-    }});
+    this.likeInteractions = new app.models.Post.LikeInteractions(_.extend({comment: this}, this.get("likeInteractions")));
   }
 });
 // @license-end
--- app/assets/javascripts/app/views/comment_view.js
+++ app/assets/javascripts/app/views/comment_view.js
@@ -6,7 +6,11 @@ app.views.Comment = app.views.Content.extend({
   className : "comment media",
   tooltipSelector: "time",
 
-  events : function() {
+  subviews: {
+    ".likes-on-comment": "likesInfoView"
+  },
+
+  events: function() {
     return _.extend({}, app.views.Content.prototype.events, {
       "click .comment_delete": "destroyModel",
       "click .comment_report": "report",
@@ -14,33 +18,37 @@ app.views.Comment = app.views.Content.extend({
     });
   },
 
-  initialize : function(options){
+  initialize: function(options) {
     this.templateName = options.templateName || this.templateName;
     this.model.on("change", this.render, this);
   },
 
-  presenter : function() {
+  presenter: function() {
     return _.extend(this.defaultPresenter(), {
       canRemove: this.canRemove(),
       text: app.helpers.textFormatter(this.model.get("text"), this.model.get("mentioned_people"))
     });
   },
 
-  ownComment : function() {
+  ownComment: function() {
     return app.currentUser.authenticated() && this.model.get("author").diaspora_id === app.currentUser.get("diaspora_id");
   },
 
-  postOwner : function() {
+  postOwner: function() {
     return  app.currentUser.authenticated() && this.model.get("parent").author.diaspora_id === app.currentUser.get("diaspora_id");
   },
 
-  canRemove : function() {
+  canRemove: function() {
     return app.currentUser.authenticated() && (this.ownComment() || this.postOwner());
   },
 
   toggleLike: function(evt) {
     if (evt) { evt.preventDefault(); }
     this.model.toggleLike();
+  },
+
+  likesInfoView: function() {
+    return new app.views.LikesInfo({model: this.model});
   }
 });
 
