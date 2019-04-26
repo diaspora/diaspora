a3196a108946eb331d007add064182e558e448aa
cmrd Senya <senya@riseup.net>
2019-04-26 20:33:21 +0300

    ArchiveValidator: remove unused TODO

--- lib/archive_validator/relayable_validator.rb
+++ lib/archive_validator/relayable_validator.rb
@@ -20,11 +20,6 @@ class ArchiveValidator
     attr_reader :relayable
     alias json relayable
 
-    # TODO: use diaspora federation to fetch parent where possible
-    # For own relayables we could just use RelatedEntity.fetch;
-    # For others' relayables we should check the present "own posts" first, and then if the target post is missing from
-    # there we could try to fetch it with RelatedEntity.fetch.
-
     # Common methods used by subclasses:
 
     def missing_parent_message
