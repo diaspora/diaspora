554faa41164d67dfc3864b6e4cdda6f55a337e18
flaburgan <flaburgan@geexxx.fr>
2019-04-28 13:03:14 +0200

    Reorganize steps a bit

--- features/step_definitions/stream_steps.rb
+++ features/step_definitions/stream_steps.rb
@@ -4,10 +4,6 @@ When /^I (?:like|unlike) the post "([^"]*)" in the stream$/ do |post_text|
   like_stream_post(post_text)
 end
 
-Then /^I should see an image in the publisher$/ do
-  photo_in_publisher.should be_present
-end
-
 Then /^"([^"]*)" should be post (\d+)$/ do |post_text, position|
   stream_element_numbers_content(position).should have_content(post_text)
 end
@@ -24,8 +20,53 @@ Then /^I should have (\d+) nsfw posts$/ do |num_posts|
   page.should have_css(".nsfw-shield", count: num_posts.to_i)
 end
 
-When /^(?:|I )click on "([^"]*)" navbar title$/ do |title|
-  with_scope(".info-bar") do
-    find("h5", text: title).click
+When /^I prepare the deletion of the first post$/ do
+  find(".stream .stream-element", match: :first).hover
+  within(find(".stream .stream-element", match: :first)) do
+    ctrl = find(".control-icons")
+    ctrl.hover
+    ctrl.find(".remove_post").click
   end
+end
+
+When /^I prepare hiding the first post$/ do
+  find(".stream .stream-element", match: :first).hover
+  within(find(".stream .stream-element", match: :first)) do
+    ctrl = find(".control-icons")
+    ctrl.hover
+    ctrl.find(".hide_post").click
+  end
+end
+
+When /^I click to delete the first post$/ do
+  accept_alert do
+    step "I prepare the deletion of the first post"
+  end
+  expect(find(".stream")).to have_no_css(".stream-element.loaded.deleting")
+end
+
+When /^I click to hide the first post$/ do
+  accept_alert do
+    step "I prepare hiding the first post"
+  end
+end
+
+When /^I click to delete the first comment$/ do
+  within("div.comment", match: :first) do
+    find(".comment_delete", visible: false).click
+  end
+end
+
+Then /^I should not see any posts in my stream$/ do
+  expect(page).not_to have_selector("#paginate .loader")
+  expect(page).not_to have_selector(".stream-element .media")
+  expect(page).to have_selector(".stream-element .no-posts-info")
+end
+
+Then /^I should not see any picture in my stream$/ do
+  expect(page).to have_selector(".photo_area img", count: 0)
+end
+
+Then /^I should see (\d+) pictures in my stream$/ do |count|
+  expect(page).to have_selector(".photo_area img", count: count)
 end
