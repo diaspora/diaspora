2f7acbe4b3c51a5fa82cbe84a38a28e59bd04315
Jonne Haß <jonne.hass@offerista.com>
2019-04-28 14:51:09 +0200

    API: fix missing page parameter to integer conversion

--- spec/lib/api/paging/rest_paginator_builder_spec.rb
+++ spec/lib/api/paging/rest_paginator_builder_spec.rb
@@ -2,7 +2,7 @@
 
 describe Api::Paging::RestPaginatorBuilder do
   it "generates page response builder called with index-based pager" do
-    params = ActionController::Parameters.new(page: 1)
+    params = ActionController::Parameters.new(page: "1", per_page: "20")
     pager = Api::Paging::RestPaginatorBuilder.new(alice.posts, nil).index_pager(params)
     expect(pager.is_a?(Api::Paging::RestPagedResponseBuilder)).to be_truthy
   end
