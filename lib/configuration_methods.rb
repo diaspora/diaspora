165b8f4f6e4e0a0c7a4577c1f3a9ffcb3d8a7a09
Benjamin Neff <benjamin@coding4coffee.ch>
2019-05-11 19:06:42 +0200

    Don't encrypt the OTP secret
    It doesn't add any security to have this encrypted, but it adds
    complexity for podmins, because they need to backup the key.
    
    closes #8014

--- lib/configuration_methods.rb
+++ lib/configuration_methods.rb
@@ -68,24 +68,6 @@ module Configuration
       end
     end
 
-    def twofa_encryption_key
-      if heroku?
-        return ENV["TWOFA_ENCRYPTION_KEY"] if ENV["TWOFA_ENCRYPTION_KEY"]
-
-        warn "FATAL: Running on Heroku with TWOFA_ENCRYPTION_KEY unset"
-        warn "       Run heroku config:add TWOFA_ENCRYPTION_KEY=#{SecureRandom.hex(32)}"
-        abort
-      else
-        key_file = File.expand_path(
-          "../config/initializers/twofa_encryption_key.rb",
-          File.dirname(__FILE__)
-        )
-        system "DISABLE_SPRING=1 bin/rake generate:twofa_key" unless File.exist? key_file
-        require key_file
-        Diaspora::Application.config.twofa_encryption_key
-      end
-    end
-
     def version_string
       return @version_string unless @version_string.nil?
 
