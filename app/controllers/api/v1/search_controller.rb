363afe6b17cf743eadd7c8278617cb8a93781a1e
Jonne Haß <me@jhass.eu>
2020-03-16 19:57:17 +0100

    API: extend /search/user with a filter option
    See API docs for more details

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,12 +3,20 @@
 module Api
   module V1
     class SearchController < Api::V1::BaseController
+      USER_FILTER_CONTACTS = "contacts"
+      USER_FILTER_RECEIVING_CONTACTS = "contacts:receiving"
+      USER_FILTER_SHARING_CONTACTS = "contacts:sharing"
+      USER_FILTER_ASPECTS_PREFIX = "aspect:"
+      USER_FILTERS_EXACT_MATCH = [USER_FILTER_CONTACTS, USER_FILTER_RECEIVING_CONTACTS,
+                                  USER_FILTER_SHARING_CONTACTS].freeze
+      USER_FILTERS_PREFIX_MATCH = [USER_FILTER_ASPECTS_PREFIX].freeze
+
       before_action do
         require_access_token %w[public:read]
       end
 
-      rescue_from ActionController::ParameterMissing, RuntimeError do
-        render_error 422, "Search request could not be processed"
+      rescue_from ActionController::ParameterMissing, RuntimeError do |e|
+        render_error 422, e.message
       end
 
       def user_index
@@ -36,20 +44,46 @@ module Api
       end
 
       def people_query
-        parameters = params.permit(:tag, :name_or_handle)
-        raise RuntimeError if parameters.keys.length != 1
+        tag = params[:tag]
+        name_or_handle = params[:name_or_handle]
+        raise "Parameters tag and name_or_handle are exclusive" if tag.present? && name_or_handle.present?
 
-        if params.has_key?(:tag)
-          Person.profile_tagged_with(params[:tag])
-        else
-          connected_only = !private_read?
-          Person.search(
-            params[:name_or_handle],
-            current_user,
-            only_contacts: connected_only,
-            mutual:        connected_only
-          )
+        query = if tag.present?
+                  # scope filters to only searchable people already
+                  Person.profile_tagged_with(tag)
+                elsif name_or_handle.present?
+                  Person.searchable(contacts_read? && current_user) # rubocop:disable Rails/DynamicFindBy
+                        .find_by_substring(name_or_handle)
+                else
+                  raise "Missing parameter tag or name_or_handle"
+                end
+
+        query = query.where(closed_account: false)
+
+        user_filters.each do |filter|
+          query = query.contacts_of(current_user) if filter == USER_FILTER_CONTACTS
+
+          if filter == USER_FILTER_RECEIVING_CONTACTS
+            query = query.contacts_of(current_user).where(contacts: {receiving: true})
+          end
+
+          if filter == USER_FILTER_SHARING_CONTACTS
+            query = query.contacts_of(current_user).where(contacts: {sharing: true})
+          end
+
+          if filter.start_with?(USER_FILTER_ASPECTS_PREFIX) # rubocop:disable Style/Next
+            _, ids = filter.split(":", 2)
+            ids = ids.split(",").map {|id|
+              Integer(id) rescue raise("Invalid aspect filter") # rubocop:disable Style/RescueModifier
+            }
+
+            raise "Invalid aspect filter" unless current_user.aspects.where(id: ids).count == ids.size
+
+            query = Person.where(id: query.all_from_aspects(ids, current_user).select(:id))
+          end
         end
+
+        query.distinct
       end
 
       def posts_query
@@ -62,6 +96,22 @@ module Api
 
       def tags_query
         ActsAsTaggableOn::Tag.autocomplete(params.require(:query))
+      end
+
+      def user_filters
+        @user_filters ||= Array(params[:filter]).uniq.tap do |filters|
+          raise "Invalid filter" unless filters.all? {|filter|
+            USER_FILTERS_EXACT_MATCH.include?(filter) ||
+            USER_FILTERS_PREFIX_MATCH.any? {|prefix| filter.start_with?(prefix) }
+          }
+
+          # For now all filters require contacts:read
+          require_access_token %w[contacts:read] unless filters.empty?
+        end
+      end
+
+      def contacts_read?
+        access_token? %w[contacts:read]
       end
     end
   end
