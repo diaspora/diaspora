e10cf9cc85bd2b7a97ba66aa5d6eb25cbe59a44d
Jonne Haß <me@jhass.eu>
2020-09-03 23:05:08 +0200

    Drop fallback to bitcoin_wallet_id

--- lib/configuration_methods.rb
+++ lib/configuration_methods.rb
@@ -128,11 +128,6 @@ module Configuration
     end
 
     def bitcoin_donation_address
-      if AppConfig.settings.bitcoin_wallet_id.present?
-        warn "WARNING: bitcoin_wallet_id is now bitcoin_address. Change in diaspora.yml."
-        return AppConfig.settings.bitcoin_wallet_id
-      end
-
       if AppConfig.settings.bitcoin_address.present?
         AppConfig.settings.bitcoin_address
       end
