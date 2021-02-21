61fd2d4819f75704245b567695995758f42b9b2f
Thorsten Claus <thorstenclaus@web.de>
2021-02-21 10:46:14 +0100

    Added left over http links - fixed test

--- app/assets/javascripts/app/helpers/locations.js
+++ app/assets/javascripts/app/helpers/locations.js
@@ -6,8 +6,8 @@
         return L.tileLayer("https://api.mapbox.com/styles/v1/{style}/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
           accessToken: gon.appConfig.map.mapbox.access_token,
           style: gon.appConfig.map.mapbox.style,
-          attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
-                       "<a href='http://creativecommons.org/licenses/by-sa/2.0/''>CC-BY-SA</a>, " +
+          attribution: "Map data &copy; <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors, " +
+                       "<a href='https://creativecommons.org/licenses/by-sa/2.0/''>CC-BY-SA</a>, " +
                        "Imagery © <a href='https://www.mapbox.com'>Mapbox</a>",
           maxZoom: 18
         });
@@ -15,7 +15,7 @@
 
       // maptiles from the Heidelberg University are used by default.
       return L.tileLayer("http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}", {
-        attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, " +
+        attribution: "Map data &copy; <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors, " +
                      "rendering <a href='http://giscience.uni-hd.de/'>" +
                      "GIScience Research Group @ Heidelberg University</a>",
         maxZoom: 18
