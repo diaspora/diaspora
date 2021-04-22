9d89f2a67e1ce780b3e21845a2dec992f0be1a49
Thorsten Claus <thorstenclaus@web.de>
2021-04-22 22:24:42 +0200

    Adds a detailed report overview
    With undecided / reviewed and statistics page
    Already reviewed reports stay visible
    Enhances Spam/user control
    
    fixes #8239

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -5,15 +5,36 @@
 #   the COPYRIGHT file.
 
 module ReportHelper
+  # rubocop:disable Rails/OutputSafety
   def report_content(report)
     case (item = report.item)
     when Post
-      raw t("report.post_label", content: link_to(post_message(item), post_path(item.id)))
+      raw t("report.post_label", content: link_to(post_page_title(item), post_path(item.id)))
     when Comment
-      raw t("report.comment_label", data: link_to(
-        h(comment_message(item)),
+      raw t("report.comment_label", data: link_to(item.message.title,
         post_path(item.post.id, anchor: item.guid)
       ))
+    else
+      t("report.not_found")
+    end
+  end
+
+  # rubocop:enable Rails/OutputSafety
+
+  def link_to_content(report)
+    case (item = report.item)
+    when Post
+      link_to("", post_path(item.id),
+              {title:  t("report.view_reported_element"),
+               class:  "entypo-eye",
+               target: "_blank",
+               rel:    "noopener"})
+    when Comment
+      link_to("", post_path(item.post.id, anchor: item.guid),
+              {title:  t("report.view_reported_element"),
+               class:  "entypo-eye",
+               target: "_blank",
+               rel:    "noopener"})
     else
       t("report.not_found")
     end
