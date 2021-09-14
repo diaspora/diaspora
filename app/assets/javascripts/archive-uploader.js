5255767509a3991dacb2bc90a17d34caf23ec884
Thorsten Claus <thorstenclaus@web.de>
2021-09-14 19:56:24 +0200

    No submit if no photo is selected

--- app/assets/javascripts/archive-uploader.js
+++ app/assets/javascripts/archive-uploader.js
@@ -1,15 +1,24 @@
-const profileFileBtn = document.getElementById("profile-file-btn");
-
+const checkProfileUploadButton = function() {
+  let photoFiles = $("#profile-file-btn")[0].files;
+  let profileFiles = $("#photos-file-btn")[0].files;
+  if ((photoFiles.size + profileFiles.size) === 0) {
+    $("#upload_profile_files").attr("disabled", "disabled");
+  } else {
+    $("#upload_profile_files").removeAttr("disabled");
+  }
+};
+const profileFileButton = document.getElementById("profile-file-btn");
 const profileFileChosen = document.getElementById("profile-file-chosen");
 
-profileFileBtn.addEventListener("change", function() {
-  profileFileChosen.textContent = this.files[0].name;
-});
-
-const photosFileBtn = document.getElementById("photos-file-btn");
-
+const photosFileButton = document.getElementById("photos-file-btn");
 const photosFileChosen = document.getElementById("photos-file-chosen");
 
-photosFileBtn.addEventListener("change", function() {
+profileFileButton.addEventListener("change", function() {
+  profileFileChosen.textContent = this.files[0].name;
+  checkProfileUploadButton();
+});
+
+photosFileButton.addEventListener("change", function() {
   photosFileChosen.textContent = this.files[0].name;
+  checkProfileUploadButton();
 });
