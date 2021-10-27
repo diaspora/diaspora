b507ab609ee1ff7d8fd8b920ba45e1d42e287264
Benjamin Neff <benjamin@coding4coffee.ch>
2021-10-27 04:21:25 +0200

    Use old person private key if relayable author migrated away
    We only store signatures for relayables if the author is external, but
    if the author becomes external through a migration, the signature is
    missing. Lets just use the old persons private key to still be able to
    generate a signature for the export.

--- spec/serializers/export/others_data_serializer_spec.rb
+++ spec/serializers/export/others_data_serializer_spec.rb
@@ -13,9 +13,14 @@ describe Export::OthersDataSerializer do
     serializer.associations
   end
 
-  context "with user's activity" do
-    before do
-      DataGenerator.new(user).activity
+  it "uses old local user private key if the author was migrated away from the pod" do
+    post = DataGenerator.new(user).status_message_with_activity
+
+    old_comment_author = post.comments.first.author
+    AccountMigration.create!(old_person: old_comment_author, new_person: FactoryBot.create(:person)).perform!
+
+    serializer.associations[:relayables].select {|r| r[:entity_type] == "comment" }.each do |comment|
+      expect(comment[:entity_data][:author]).to eq(old_comment_author.diaspora_handle)
     end
   end
 end
