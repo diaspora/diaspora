96493b4a5c19072763ce8cb58f3a18ae0fa89d4c
Benjamin Neff <benjamin@coding4coffee.ch>
2021-11-09 02:56:44 +0100

    Refactory archive concurrency so the same logic can be reused

--- app/workers/import_user.rb
+++ app/workers/import_user.rb
@@ -1,36 +1,12 @@
 # frozen_string_literal: true
 
 module Workers
-  class ImportUser < Base
-    sidekiq_options queue: :low
-
-    include Diaspora::Logging
-
-    def perform(user_id)
-      if currently_running_exports >= AppConfig.settings.export_concurrency.to_i
-
-        logger.info "Already the maximum number of parallel user imports running, " \
-                    "scheduling import for User:#{user_id} in 5 minutes."
-        self.class.perform_in(5.minutes + rand(30), user_id)
-      else
-        import_user(user_id)
-      end
-    end
-
+  class ImportUser < ArchiveBase
     private
 
-    def import_user(user_id)
+    def perform_archive_job(user_id)
       user = User.find(user_id)
       ImportService.new.import_by_user(user)
-    end
-
-    def currently_running_exports
-      return 0 if AppConfig.environment.single_process_mode?
-
-      Sidekiq::Workers.new.count do |process_id, thread_id, work|
-        !(Process.pid.to_s == process_id.split(":")[1] && Thread.current.object_id.to_s(36) == thread_id) &&
-          work["payload"]["class"] == self.class.to_s
-      end
     end
   end
 end
