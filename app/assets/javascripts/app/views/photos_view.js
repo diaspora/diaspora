35c254c88c086bd457b642b9e517625174026548
flaburgan <flaburgan@geexxx.fr>
2022-10-31 11:21:08 +0100

    Do not recreate blueimp each time you're scrolling in the photos page, fixes #8118

--- app/assets/javascripts/app/views/photos_view.js
+++ app/assets/javascripts/app/views/photos_view.js
@@ -2,23 +2,16 @@
 
 app.views.Photos = app.views.InfScroll.extend({
   className: "clearfix row",
-
   postClass : app.views.Photo,
 
   initialize : function() {
     this.stream = this.model;
     this.collection = this.stream.items;
+    new app.views.Gallery({el: this.$el});
 
     // viable for extraction
     this.stream.fetch();
     this.setupInfiniteScroll();
-  },
-
-  postRenderTemplate: function(){
-    var photoAttachments = $("#main-stream > div");
-    if(photoAttachments.length > 0) {
-      new app.views.Gallery({ el: photoAttachments });
-    }
   }
 });
 // @license-end
