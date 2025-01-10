a9b2c36a81c3df0fe976cf051a6449f66c556820
Thorsten Claus <thorstenclaus@web.de>
2025-01-10 23:33:27 +0100

    Fix JS issue

--- app/assets/javascripts/archive-uploader.js
+++ app/assets/javascripts/archive-uploader.js
@@ -7,18 +7,15 @@ const checkProfileUploadButton = function() {
     $("#upload_profile_files").removeAttr("disabled");
   }
 };
-const profileFileButton = document.getElementById("profile-file-btn");
-const profileFileChosen = document.getElementById("profile-file-chosen");
+const profileFileChosen = $("#profile-file-chosen");
+const photosFileChosen = $("#photos-file-chosen");
 
-const photosFileButton = document.getElementById("photos-file-btn");
-const photosFileChosen = document.getElementById("photos-file-chosen");
-
-profileFileButton.addEventListener("change", function() {
+$("#profile-file-btn").on("change", function() {
   profileFileChosen.textContent = this.files[0].name;
   checkProfileUploadButton();
 });
 
-photosFileButton.addEventListener("change", function() {
+$("#photos-file-btn").on("change", function() {
   photosFileChosen.textContent = this.files[0].name;
   checkProfileUploadButton();
 });
