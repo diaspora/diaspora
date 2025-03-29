2fbd0b1cb38716c614feb1f105d9fe91e8bc2bd1
Thorsten Claus <thorstenclaus@web.de>
2025-03-29 13:43:23 +0100

    Fixing syntax error in mogration

--- db/migrate/20250327181326_add_importing_data_field_to_user.rb
+++ db/migrate/20250327181326_add_importing_data_field_to_user.rb
@@ -3,8 +3,8 @@
 class AddImportingDataFieldToUser < ActiveRecord::Migration[6.1]
   def change
     change_table :users, bulk: true do |t|
-      t.column :users, :importing, :boolean, default: false, null: false
-      t.column :users, :importing_photos, :boolean, default: false, null: false
+      t.column :importing, :boolean, default: false, null: false
+      t.column :importing_photos, :boolean, default: false, null: false
     end
   end
 end
